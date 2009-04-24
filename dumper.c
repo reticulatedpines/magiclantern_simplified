26002921f9c9e6ba41a080ae5d0fb2dd05baa90f
Trammell Hudson <hudson@rotomotion.com>
2009-04-24 14:11:27 -0400

    Rudimentary loging facility

--- dumper.c
+++ dumper.c
@@ -22,6 +22,13 @@ extern void shutdown(void);
 #define INVALID_HANDLE	((void *)0xFFFFFFFF)
 #define NULL			((void *)0)
 
+#define LOG() \
+	do { \
+		uint32_t error = __LINE__; \
+		if( logfile != INVALID_HANDLE ) \
+			write( logfile, &error, sizeof(error) ); \
+	} while(0)
+
 int main( void )
 {
 	void * const	rom_start	= (void *) ROM0_ADDRESS;
@@ -32,9 +39,10 @@ int main( void )
 	char fname[12];
 
 	fname[0] = 'A'; fname[1] = ':'; fname[2] = '/';
-	fname[3] = 'R'; fname[4] = 'O'; fname[5] = 'M';
-	fname[6] = '0'; fname[7] = '.'; fname[8] = 'b';
-	fname[9] = 'i'; fname[10] = 'n'; fname[11] = '\0';
+	fname[3] = 'R'; fname[4] = 'O'; fname[5] = 'M'; fname[6] = '0';
+	fname[7] = '.';
+	fname[8] = 'l'; fname[9] = 'o'; fname[10] = 'g';
+	fname[11] = '\0';
 	
 
 	const uint32_t model_id = *(uint32_t*) 0x800000;
@@ -43,6 +51,14 @@ int main( void )
 	if( model_id != 0x80000218 )
 		while(1);
 
+	void * logfile = open( fname, O_WRONLY | O_CREAT, 0666 );
+	if( logfile == INVALID_HANDLE )
+		logfile = creat( fname, O_WRONLY );
+	if( logfile == INVALID_HANDLE )
+		shutdown();
+	write( logfile, &model_id, sizeof(model_id) );
+
+	fname[8] = 'b'; fname[9] = 'i'; fname[10] = 'n'; fname[11] = '\0';
 #if 0
 		case 0x80000190:	// 40D
 			// 40D updater 1.1.1 params
@@ -90,32 +106,54 @@ int main( void )
 			while(1);
 	}
 #endif
+	uint32_t error = 0;
 
-	//void *f = open(fname, O_CREAT|O_WRONLY, 0644);
-	//if( INVALID_HANDLE == f )
-	void * f = creat( fname, O_WRONLY );	// O_CREAT is not working in DRYOS
+	void *f = open(fname, O_CREAT|O_WRONLY, 0644);
 	if( INVALID_HANDLE == f )
+	{
+		LOG();
+		f = creat( fname, O_WRONLY );	// O_CREAT is not working in DRYOS
+	}
+	if( INVALID_HANDLE == f )
+	{
+		LOG();
 		shutdown();
+	}
 
 	write( f, rom_start, rom_size);
 	close( f );
 
 	if( rom1_size == 0 )
+	{
+		LOG();
 		shutdown();
+	}
 
 	fname[6] = '1';
-	//f = open(fname, O_CREAT|O_WRONLY, 0644);
-	//if( INVALID_HANDLE == f )
-	f = creat( fname, O_WRONLY );	// O_CREAT is not working in DRYOS
+	f = open(fname, O_CREAT|O_WRONLY, 0644);
 	if( INVALID_HANDLE == f )
+	{
+		LOG();
+		f = creat( fname, O_WRONLY );	// O_CREAT is not working in DRYOS
+	}
+	if( INVALID_HANDLE == f )
+	{
+		LOG();
 		shutdown();
+	}
 
+	LOG();
 	write(f, rom1_start, rom1_size);
 	close(f);
 
+	LOG();
+	close( logfile );
 	shutdown();
 
 	// If shutdown didn't work, we spin
+	LOG();
+	close( logfile );
+
 	while(1);
 	return 0;
 }
