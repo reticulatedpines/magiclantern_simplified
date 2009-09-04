c88b28fa7ee05f00fb5c4017c08d0b1c9b5e87c2
hudson <hudson@kremvax.wan>
2009-09-03 23:28:46 -0400

    More tests

--- test.py
+++ test.py
@@ -1,10 +1,21 @@
 # Testing the pymite interpreter dynamic loading
+collection = {
+	'first'		: "hello",
+	'second'	: "world",
+	'third'		: "from",
+}
+
 def run_test():
 	y = 30
 	x = 10
-	collection = ["hello","world","from","pymite","user script!"]
-	for item in collection:
-		main.bmp_puts( x, y, item )
+	#collection['fourth'] = 'pymite'
+	#collection['fifth'] = 'user script!'
+
+	#for k,v in collection.iteritems():
+	for k in collection.keys():
+		new_item = "X=" + `x` + ": " + collection[k]
+		#main.bmp_puts( x, y, new_item )
+		print new_item
 		y += 32
 		x += 28 * 2
 
@@ -13,6 +24,5 @@ def run_test():
 import main;
 
 main.bmp_fill( 3, 0, 30, 720, 300 )
-main.bmp_puts( 200, 40, "Hi there" )
 
 run_test()
