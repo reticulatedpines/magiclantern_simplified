807f969a66d792de10bb75cb89f90881e0b17b74
hudson <hudson@andromeda.local>
2009-09-12 17:13:22 -0400

    LTC clock frame recovery works on 16-bit au files

--- timecode.c
+++ timecode.c
@@ -42,13 +42,13 @@ int main( int argc, char ** argv )
 	);
 
 	if( hdr.magic != 0x2e736e64
-	||  hdr.encoding != 2 )
+	||  hdr.encoding != 3 )
 	{
 		fprintf( stderr, "Bad magic or unsupported format!\n" );
 		return -1;
 	}
 
-	int8_t sample;
+	uint16_t sample;
 	uint32_t last_transition = 0;
 
 	// Seek to the start of the data
@@ -58,68 +58,82 @@ int main( int argc, char ** argv )
 	// Now we are at the data
 	int bit = 0;
 	int one_count = 0;
-	int word = 0;
+	uint32_t word = 0;
+	uint32_t raw_word = 0;
 	int synced = 0;
-	int bit_count = 0;
+	int byte_count = 0;
 
 	// Reconstruct the clock?
 	while( offset < hdr.len )
 	{
-		ssize_t rc = read( fd, &sample, 1 );
+		ssize_t rc = read( fd, &sample, sizeof(sample) );
 		offset += rc;
 		if( rc < 1 )
 			break;
 
-		int old_bit = bit;
+		sample = ntohs( sample );
 
-		if( sample > 50 )
-			bit = 0;
-		else
-		if( sample < -50 )
-			bit = 1;
-
-		if( old_bit == bit )
-			continue;
-
-		// Transistion!
-		uint32_t delta = offset - last_transition;
-		last_transition = offset;
-		int new_bit = delta < 8 ? 1 : 0;
-
-		//printf( "%d\n", new_bit );
+		//printf( "%d\n", sample );
 		//continue;
 
-		// Look for a sync pulse of 12 ones in a row
+		// Use some hysteris to avoid zero crosing errors
+		if( sample > 40000 )
+			bit = 0;
+		else
+		if( sample < 24000 )
+			bit = 1;
+
+		//printf( "%d %d %d\n", offset, sample, bit );
+		//continue;
+
+		raw_word = (raw_word << 1) | bit;
+
+		// Wait until we have at least 16 bits worth of samples
+		if( (raw_word & 0xFFFF0000) == 0 )
+			continue;
+
+		// Check for a solid lock
+		uint32_t test_word = raw_word & 0xFFFF;
+		if( test_word == 0xFF00
+		||  test_word == 0x00FF )
+		{
+			//printf( "%d 1\n", (int) offset );
+			word = (word << 1) | 1;
+			raw_word = 0x1;
+		} else
+		if( test_word == 0xFFFF
+		||  test_word == 0x0000 )
+		{
+			//printf( "%d 0\n", (int) offset );
+			word = (word << 1) | 0;
+			raw_word = 0x1;
+		} else
+			continue;
+
+		//printf( "%04x\n", word & 0xFFFF );
+		//continue;
+
+		// Check for SMPTE sync signal
 		if( !synced )
 		{
-			if( new_bit == 0 )
+			if( (word & 0xFFFF) == 0x3FFD )
 			{
-				one_count = 0;
-				continue;
+				fprintf( stderr, "%x: Got sync!\n", offset );
+				printf( "\n%08x", offset );
+				synced = 1;
+				word = 1;
+				byte_count = 0;
 			}
-
-			if( ++one_count < 11 )
-				continue;
-
-			// Got it!
-			synced = 1;
-			word = 1; // Preset a bit
-			bit_count = 0;
-			printf( "\n" );
-			fprintf( stderr, "%08x: synced!\n", offset );
 			continue;
 		}
 
-		// Store it
-		word = (word << 1) | new_bit;
+		// We are locked; wait until we have 8 bits then write it
+		if( (word & 0x100) == 0 )
+			continue;
 
-		if( word & 0x100 )
-		{
-			printf( "%02x ", word & 0xFF );
-			word = 1;
-		}
-
-		if( ++bit_count > 64 )
+		printf( " %02x", word & 0xFF );
+		word = 1;
+		if( ++byte_count == 8 )
 			synced = 0;
 	}
 	return 0;
