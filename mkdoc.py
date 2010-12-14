d4938a4d684bd94c3d319efff9c7e6a0b46dbc11
a1ex <devnull@localhost>
2010-12-15 01:13:53 +0200

    mkdoc: also build PDF versions of install and user guide.

--- mkdoc.py
+++ mkdoc.py
@@ -22,3 +22,24 @@ for l in c:
 o.close()
 
 os.system("pandoc -f rst -t mediawiki -o userguide.wiki userguide.rst")
+
+
+o = open("userguide.rst", "w")
+print >> o, """Magic Lantern 0.2 for Canon 550D, Firmware 1.0.9 -- User's Guide
+===========================================================================
+
+"""
+for l in f:
+    o.write(l)
+for l in c:
+    o.write(l)
+o.close()
+
+os.system("pandoc -f rst -t latex -s -o userguide.tex userguide.rst")
+os.system("pdflatex userguide.tex")
+
+
+os.system("pandoc -f rst -t latex -s -o install.tex INSTALL.txt")
+os.system("pdflatex install.tex")
+
+
