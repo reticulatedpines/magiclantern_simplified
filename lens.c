2a5c76f695ee8e43ef3d1b14771878f3595a54d1
a1ex <devnull@localhost>
2011-02-03 13:41:45 +0100

    Fixed typo: MOV_1234.LOG -> MVI_1234.LOG

--- lens.c
+++ lens.c
@@ -462,7 +462,7 @@ mvr_create_logfile(
 
 	// Movie starting
 	char name[100];
-	snprintf(name, sizeof(name), "B:/DCIM/%03dCANON/MOV_%04d.LOG", folder_number, file_number);
+	snprintf(name, sizeof(name), "B:/DCIM/%03dCANON/MVI_%04d.LOG", folder_number, file_number);
 
 	FIO_RemoveFile(name);
 	mvr_logfile = FIO_CreateFile( name );
