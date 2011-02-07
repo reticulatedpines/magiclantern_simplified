82307ff2b13b5125031d21183edda0c03e56db8a
a1ex <devnull@localhost>
2011-02-07 16:02:19 +0100

    Changed doc template (switched from pandoc to rst2latex for PDF format); documented new features.

--- doc/mkdoc.py
+++ doc/mkdoc.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
-# Render the user's guide in wiki format
-# Output: userguide.wiki
+# -*- coding: utf-8 -*-
+# Render the user / install guide in pdf and wiki formats
+# Outputs: userguide.wiki, UserGuide.pdf, install.wiki, INSTALL.pdf
 
 # Author: Alex Dumitrache <broscutamaker@gmail.com>
 # License: GPL
 
-import os, re
+import os, re, time
+import urllib
 
 f = open("FEATURES.txt").readlines()[3:];
 c = open("CONFIG.txt").readlines()[2:];
@@ -43,22 +45,63 @@ def sub(file, fr, to):
     f.write(txt)
     f.close()
 
+def fixwikilinks(file):
+    txt = open(file).read()
+
+    while 1:
+        m = re.search(r"\[\[([^]|]+)([^]]*)\]\]", txt, re.MULTILINE)
+        if not m: break
+        origstr = "[[" + m.groups()[0] + m.groups()[1] + "]]"
+        print origstr
+        x = m.groups()[0]
+        txt = txt.replace(origstr, "`%s <http://magiclantern.wikia.com/wiki/%s>`_" % (x, urllib.quote(x)))
+        
+    #~ sub("INSTALL.rst", , ")
+
+    f = open(file,"w")
+    f.write(txt)
+    f.close()
+
+
+def labelhack(file): # bug in rst2latex? it forgets to place labels in tex source
+    txt = ""
+    for l in open(file).readlines():
+        txt += l
+        m = re.match(".. _(.*):", l)
+        if m:
+            label = m.groups()[0]
+            txt += r""".. raw:: latex
+    
+    \vspace{-10mm}\subsubsection*{}\label{%s}
+""" % label.lower().replace("/"," ").replace("   ", " ").replace("  ", " ").replace(" ", "-")
+    f = open(file,"w")
+    f.write(txt)
+    f.close()
+
 os.system("pandoc -f rst -t latex -o credits.tex CREDITS.txt")
 
-sub("userguide.rst", r"\[\[([^]|]+)([^]]*)\]\]", "`\\1 <http://magiclantern.wikia.com/wiki/\\1>`_")
-os.system("pandoc -f rst -t latex -o userguide-body.tex userguide.rst")
-os.system(r"sed -i 's/\\{\\{clr\\}\\}//g' userguide-body.tex")
+fixwikilinks("userguide.rst")
+labelhack("userguide.rst")
+#os.system("pandoc -f rst -t latex -o userguide-body.tex userguide.rst")
+os.system("rst2latex.py userguide.rst --output-encoding=utf8 --template=ug-template.tex > UserGuide.tex")
+os.system(r"sed -i 's/\\{\\{clr\\}\\}//g' UserGuide.tex")
+os.system(r"sed -i 's/⬜/$\\square$/g' UserGuide.tex")
+os.system(r"sed -i 's/\\addcontentsline{toc}{section}{Features}//g' UserGuide.tex")
 os.system("pdflatex UserGuide.tex")
 os.system("pdflatex UserGuide.tex")
+#os.system(r"sed -i 's/\\{\\{clr\\}\\}//g' userguide-body.tex")
+#os.system("pdflatex UserGuide.tex")
+#os.system("pdflatex UserGuide.tex")
 
 os.system("cp INSTALL.txt INSTALL.rst")
 os.system("pandoc -f rst -t mediawiki -s -o install.wiki INSTALL.rst")
 
 #sub("INSTALL.rst", r"\[\[Video:[^]]+\]\]", "`Video installation tutorial <http://vimeo.com/18035870>`_ by saw0media")
 
-sub("INSTALL.rst", r"\[\[([^]|]+)([^]]*)\]\]", "`\\1 <http://magiclantern.wikia.com/wiki/\\1>`_")
+fixwikilinks("INSTALL.rst")
 os.system("pandoc -f rst -t latex -o install-body.tex INSTALL.rst")
-os.system(r"sed -i 's/\\{\\{clr\\}\\}//g' install-body.tex")
+os.system("rst2latex.py INSTALL.rst --output-encoding=utf8 --template=ins-template.tex > INSTALL.tex")
+os.system(r"sed -i 's/\\{\\{clr\\}\\}//g' INSTALL.tex")
 os.system("pdflatex INSTALL.tex")
 os.system("pdflatex INSTALL.tex")
 
