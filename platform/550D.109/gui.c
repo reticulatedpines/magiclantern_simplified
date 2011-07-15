be5dcc513cad081a5dfb156e4238e505e075ff79
a1ex <devnull@localhost>
2011-07-15 22:19:09 +0200

    Fixed intervalometer

--- platform/550D.109/gui.c
+++ platform/550D.109/gui.c
@@ -275,7 +275,7 @@ static int handle_buttons(struct event * event)
 	}
 	
 	// stop intervalometer with MENU or PLAY
-	if (event->type == 0 && (event->param == BGMT_MENU || event->param == BGMT_PLAY) && !gui_menu_shown())
+	if (event != &fake_event && event->type == 0 && (event->param == BGMT_MENU || event->param == BGMT_PLAY) && !gui_menu_shown())
 		intervalometer_stop();
 		
 	
