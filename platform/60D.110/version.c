2dae7e1bbac02982d31e50f73244c96fd6f88deb
a1ex <devnull@localhost>
2011-07-30 23:45:39 +0300

    60D.110: initial port (fixed stubs). Magic Lantern boots and seems to work :)

--- platform/60D.110/version.c
+++
@@ -1,4 +0,0 @@
-const char build_version[] = "2011Jul28.60D.fw109.alex";
-const char build_id[] = "22cb9e48937b+ (unified) tip";
-const char build_date[] ="2011-07-28 15:04:36";
-const char build_user[] = "alex@karmic";
