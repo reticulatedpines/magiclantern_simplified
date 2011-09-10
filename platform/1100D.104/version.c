5404b44384c6837a6320d79e688dd06c27e0249d
a1ex <devnull@localhost>
2011-09-10 13:49:32 +0300

    Untrack version.c from 1100D

--- platform/1100D.104/version.c
+++
@@ -1,4 +0,0 @@
-const char build_version[] = "2011Sep10.1100D104.nanomad";
-const char build_id[] = "d9d35c016d85+ (unified) tip";
-const char build_date[] ="2011-09-10 11:24:46";
-const char build_user[] = "nanomad@CHIARA";
