6ba2dc523c225a84ba1f01fa617852abda0b820a
nanomad <devnull@localhost>
2011-09-11 02:24:30 +0200

    Added menu support to the code via Av shortpress. qtimer should be probably moved inside gui.c and refactored to use semaphores

--- platform/1100D.104/version.c
+++ platform/1100D.104/version.c
@@ -1,4 +1,4 @@
 const char build_version[] = "2011Sep11.1100D104.nanomad";
-const char build_id[] = "ee76c140af18+ (unified) tip";
-const char build_date[] ="2011-09-10 23:38:10";
+const char build_id[] = "04066f118926+ (unified) tip";
+const char build_date[] ="2011-09-11 00:20:24";
 const char build_user[] = "nanomad@CHIARA";
