c376b31f4eebc288204f4ebb01e38bcb53394318
a1ex <devnull@localhost>
2011-10-11 09:35:07 +0300

    Morse: support for digits and experimental menu navigation

--- src/morse.c
+++ src/morse.c
@@ -2,7 +2,7 @@
 // Original author: mrobbins@mit.edu
 // Ported to Magic Lantern by Alex Dumitrache <broscutamaker@gmail.com>
 
-#if 1
+#if 0
 #include "dryos.h"
 #include "bmp.h"
 #include "menu.h"
@@ -18,8 +18,8 @@ int morse_pressed()
     return 0;
 }
 
-uint8_t time_next_keypress() {
-  uint16_t counter;
+int time_next_keypress() {
+  int counter;
   
   // waits until the button is pressed
   // and then released. (OR timeout)
@@ -76,13 +76,13 @@ uint8_t time_next_keypress() {
 
 // defining the lookup table.
 // bits   7 6 5 4 3 2 1 0
-// 765 define the length (0 to 7)
-// 43210 define dits (0) and dahs (1)
-#define MORSE_SIZE 26
-#define MORSE(s, x)  ((s<<5) | x)
+// MSByte defines the length
+// LSByte defines dits (0) and dahs (1)
+#define MORSE_SIZE 26+10
+#define MORSE(s, x)  ((s<<8) | x)
 #define DIT(x) (x<<1)
 #define DAH(x) ((x<<1) | 1)
-unsigned char morse_coded[MORSE_SIZE] = 
+unsigned int morse_coded[MORSE_SIZE] = 
 {
   MORSE(2, DIT(DAH(0))),		//A
   MORSE(4, DAH(DIT(DIT(DIT(0))))),	//B
@@ -110,17 +110,27 @@ unsigned char morse_coded[MORSE_SIZE] =
   MORSE(4, DAH(DIT(DIT(DAH(0))))),	//X
   MORSE(4, DAH(DIT(DAH(DAH(0))))),	//Y
   MORSE(4, DAH(DAH(DIT(DIT(0))))),	//Z
+  MORSE(5, DIT(DAH(DAH(DAH(DAH(0)))))),	//1
+  MORSE(5, DIT(DIT(DAH(DAH(DAH(0)))))),	//2
+  MORSE(5, DIT(DIT(DIT(DAH(DAH(0)))))),	//3
+  MORSE(5, DIT(DIT(DIT(DIT(DAH(0)))))),	//4
+  MORSE(5, DIT(DIT(DIT(DIT(DIT(0)))))),	//5
+  MORSE(5, DAH(DIT(DIT(DIT(DIT(0)))))),	//6
+  MORSE(5, DAH(DAH(DIT(DIT(DIT(0)))))),	//7
+  MORSE(5, DAH(DAH(DAH(DIT(DIT(0)))))),	//8
+  MORSE(5, DAH(DAH(DAH(DAH(DIT(0)))))),	//9
+  MORSE(5, DAH(DAH(DAH(DAH(DAH(0)))))),	//0
 };
 unsigned char morse_alpha[MORSE_SIZE] =
 { 
-  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'
 };
 
-unsigned char morse_lookup(unsigned char in) {
+unsigned morse_lookup(unsigned in) {
   // linearly go through the table (in program memory)
   // and find the matching one
-  uint8_t i;
-  unsigned char tmp;
+  unsigned i;
+  unsigned tmp;
   
   for(i=0; i<MORSE_SIZE; i++) {
     tmp = morse_coded[i];
@@ -133,14 +143,14 @@ unsigned char morse_lookup(unsigned char in) {
   return '?';
 }
 
-unsigned char bitwise_reverse(unsigned char in, uint8_t max) {
+unsigned bitwise_reverse(unsigned in, unsigned max) {
   // maps bits backwards
   // i.e. for max=5
   // in  XXX43210
   // out YYY01234
-  unsigned char b = 0;
+  unsigned b = 0;
   
-  uint8_t i;
+  unsigned i;
   for(i=0; i<max; i++) {
     if(in & (1<<i))
     b |= (1<< (max-1-i) );
@@ -234,7 +244,50 @@ void morse_task(void* unused)
         curchar = morse_lookup(curchar);
         
         // print it
-        STR_APPEND(morse_string, "%s", &curchar);
+        //~ STR_APPEND(morse_string, "%s", &curchar);
+        snprintf(morse_string, sizeof(morse_string), "%s", &curchar);
+        
+        switch (curchar)
+        {
+            case 'L': 
+                fake_simple_button(BGMT_PRESS_LEFT); 
+                fake_simple_button(BGMT_UNPRESS_LEFT); 
+                break;
+            case 'R': 
+                fake_simple_button(BGMT_PRESS_RIGHT);
+                fake_simple_button(BGMT_UNPRESS_RIGHT);
+                break;
+            case 'U': 
+                fake_simple_button(BGMT_PRESS_UP);
+                fake_simple_button(BGMT_UNPRESS_UP);
+                break;
+            case 'D': 
+                fake_simple_button(BGMT_PRESS_DOWN);
+                fake_simple_button(BGMT_UNPRESS_DOWN);
+                break;
+            case 'M': 
+                fake_simple_button(BGMT_MENU);
+                break;
+            case 'Z': 
+                fake_simple_button(BGMT_TRASH);
+                break;
+            case 'V': 
+                fake_simple_button(BGMT_LV);
+                break;
+            case 'Q': 
+                fake_simple_button(BGMT_Q);
+                break;
+            case 'P': 
+                fake_simple_button(BGMT_PLAY);
+                break;
+            case 'S': 
+                fake_simple_button(BGMT_PRESS_SET);
+                fake_simple_button(BGMT_UNPRESS_SET);
+                break;
+            case 'X': 
+                lens_take_picture(64);
+                break;
+        }
 
         chars++;
         lastchar = curchar;
