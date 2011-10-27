6a94b87af3780f4bd0ea6a4f0583c184c315f761
a1ex <devnull@localhost>
2011-10-27 10:05:12 +0300

    Added 500D to user guide headers

--- doc/wikidoc.py
+++ doc/wikidoc.py
@@ -41,9 +41,9 @@ os.system("pandoc -f html -t mediawiki -o userguide.wiki userguide.html")
 sub("userguide.wiki", "= dummy =", "")
 sub("userguide.wiki", "= Magic Lantern 0.2.1 =", """[[Image:Logo.png|140px]]
 
-... for Canon 550D, 60D, 600D and 500D
+...  for Canon 550D, 60D, 600D, 500D and 50D
 
-<big>'''Magic Lantern 0.2.1 -- User's Guide'''</big>
+<big>'''Magic Lantern pre0.2.2 -- User's Guide'''</big>
 
 
 '''[[Unified/UserGuide|English]] | [[Unified/UserGuide/CZ|Česky]] | [[Unified/UserGuide/DE|Deutsch]] | [[Unified/UserGuide/NL|Dutch]] | [[Unified/UserGuide/ES|Español]] | [[Unified/UserGuide/FR|Français]] | [[Unified/UserGuide/IT|Italiano]] | [[Unified/UserGuide/RO|Română]] | [[Unified/UserGuide/RU|Russian]] | [[Unified/UserGuide/CHS|简体中文]] | [[Unified/UserGuide/JA|日本語]]'''
