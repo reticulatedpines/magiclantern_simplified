ee8a006ec0886de5396022b81e6e45c021e909e7
a1ex <devnull@localhost>
2011-11-21 11:34:11 +0200

    ExpSim warning in ML menu should not be displayed outside LiveView

--- src/menu.h
+++ src/menu.h
@@ -149,6 +149,6 @@ void menu_draw_icon(int x, int y, int type, intptr_t arg);
 #define MNI_BOOL(x) ((x) ? MNI_ON : MNI_OFF)
 #define MNI_BOOL_AUTO(x) ((x) == 1 ? MNI_ON : (x) == 0 ? MNI_OFF : MNI_AUTO)
 #define MNI_BOOL_GDR(x) ((x) ? ( get_global_draw() ? MNI_ON : MNI_WARNING ) : MNI_OFF), (intptr_t) "GlobalDraw is OFF"
-#define MNI_BOOL_GDR_EXPSIM(x) ((x) ? ( get_global_draw() && expsim ? MNI_ON : MNI_WARNING ) : MNI_OFF), (intptr_t)( !get_global_draw() ? "GlobalDraw is OFF" : "ExpSim is OFF" )
+#define MNI_BOOL_GDR_EXPSIM(x) ((x) ? ( get_global_draw() && (expsim || !lv) ? MNI_ON : MNI_WARNING ) : MNI_OFF), (intptr_t)( !get_global_draw() ? "GlobalDraw is OFF" : "ExpSim is OFF" )
 #define MNI_BOOL_LV(x) ((x) ? ( lv ? MNI_ON : MNI_WARNING ) : MNI_OFF), (intptr_t) "This option works only in LiveView" 
 #endif
