97c873ab52cb555411746682ab884b935abbb019
a1ex <devnull@localhost>
2011-11-22 19:30:05 +0200

    Audio meter tweak

--- src/audio.c
+++ src/audio.c
@@ -383,7 +383,7 @@ int audio_meters_are_drawn()
 {
 	return 
     (
-     is_movie_mode() && cfg_draw_meters && do_draw_meters && get_global_draw() && !gui_menu_shown() && lv_dispsize == 1
+     is_movie_mode() && cfg_draw_meters && do_draw_meters && zebra_should_run() && !gui_menu_shown() && lv_dispsize == 1
      )
     ||
     (
