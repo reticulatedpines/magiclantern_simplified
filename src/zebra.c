2eedf6a9731a655a15f757e3ab76425cc5748a42
a1ex <devnull@localhost>
2011-12-07 13:07:33 +0200

    Histogram summary in menu

--- src/zebra.c
+++ src/zebra.c
@@ -1632,8 +1632,10 @@ hist_print( void * priv, int x, int y, int selected )
 	bmp_printf(
 		selected ? MENU_FONT_SEL : MENU_FONT,
 		x, y,
-		"Histogram   : %s",
-		hist_draw == 0 ? "OFF" : hist_colorspace == 0 ? "Luma" : "RGB"
+		"Histogram   : %s, %s%s",
+		hist_draw == 0 ? "OFF" : hist_colorspace == 0 ? "Luma" : "RGB",
+		hist_log ? "Log" : "Lin",
+		hist_warn ? ", clip" : ""
 	);
 	menu_draw_icon(x, y, MNI_BOOL_GDR_EXPSIM(hist_draw));
 }
