9ac200f7023e29f5aecf65b1f3d6239e11ba0b70
Rob Kramer <rob@solution-space.com>
2011-12-17 14:20:19 +0800

    Undefine macros before redefining them.

--- src/disable-this-module.h
+++ src/disable-this-module.h
@@ -1,5 +1,22 @@
 // some defines which disable PROP_HANDLER's, TASK_CREATE's, TASK_OVERRIDE's and INIT_FUNC's
 
+#ifdef PROP_HANDLER
+#undef PROP_HANDLER
+#endif
+
+#ifdef TASK_CREATE
+#undef TASK_CREATE
+#endif
+
+#ifdef TASK_OVERRIDE
+#undef TASK_OVERRIDE
+#endif
+
+#ifdef INIT_FUNC
+#undef INIT_FUNC
+#endif
+
+
 #define PROP_HANDLER(id) \
 static void * _prop_handler_##id( \
 	unsigned		property, \
