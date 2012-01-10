ac493ad0c02b34b6819ea67deb2c6f2c0163c24c
a1ex <devnull@localhost>
2012-01-10 13:26:23 +0200

    Bitrate: clear hint that actual bitrate may be lower

--- src/bitrate.c
+++ src/bitrate.c
@@ -131,7 +131,7 @@ bitrate_print(
     else if (bitrate_mode == 1)
     {
         if (bitrate_factor > 10) fnt = FONT(fnt, bitrate_factor > 14 ? COLOR_RED : COLOR_YELLOW, FONT_BG(fnt));
-        bmp_printf( fnt, x, y, "Bit Rate (CBR): %d.%dx%s", bitrate_factor/10, bitrate_factor%10, bitrate_dirty || bitrate_factor != 10 ? "" : " (FW default)");
+        bmp_printf( fnt, x, y, "Bit Rate (CBR): %s%d.%dx%s", bitrate_factor>10 ? "up to " : "", bitrate_factor/10, bitrate_factor%10, bitrate_dirty || bitrate_factor != 10 ? "" : " (FW default)");
         menu_draw_icon(x, y, bitrate_dirty || bitrate_factor != 10 ? MNI_PERCENT : MNI_OFF, bitrate_factor * 100 / 30);
     }
     else if (bitrate_mode == 2)
