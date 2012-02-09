9eb9101df4185ac8b018a392c08d1640f6903085
a1ex <devnull@localhost>
2012-02-09 09:32:06 +0200

    5D2 exposure override: fixed shutter speed being restored to round value by Canon firmware

--- src/lens.c
+++ src/lens.c
@@ -1329,7 +1329,9 @@ PROP_HANDLER( PROP_SHUTTER )
 {
     if (!CONTROL_BV) lensinfo_set_shutter(buf[0]);
     #ifndef CONFIG_500D
-    else if (buf[0] && !gui_menu_shown())
+    else if (buf[0] && !gui_menu_shown()
+        && ABS(buf[0] - lens_info.raw_shutter) > 4 // some cameras may attempt to round shutter value to 1/2 or 1/3 stops
+        )
     {
         bv_set_rawshutter(buf[0]);
         bv_auto_needed_by_shutter = 0;
