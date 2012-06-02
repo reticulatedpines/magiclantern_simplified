4c6ac9c8eb863c4ecfe3cfef29696ca0a2a0190a
alex <alex@thinkpad>
2012-06-02 19:42:13 +0300

    Notify box: safeguard for buffer overflows

--- src/notify_box.c
+++ src/notify_box.c
@@ -87,7 +87,8 @@ void NotifyBox(int timeout, char* fmt, ...)
         goto end; // same message: do not redraw, just increase the timeout
 
     // new message
-    memcpy(notify_box_msg, notify_box_msg_tmp, sizeof(notify_box_msg));
+    my_memcpy(notify_box_msg, notify_box_msg_tmp, sizeof(notify_box_msg));
+    notify_box_msg[sizeof(notify_box_msg)-1] = '\0';
     notify_box_timeout = MAX(timeout, 100);
     if (notify_box_timeout) notify_box_dirty = 1; // ask for a redraw, message changed
 
