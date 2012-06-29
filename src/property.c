31b3b8c8ce632eb1c7a5faf896c9aa998731a7ac
alex <alex@thinkpad>
2012-06-29 19:05:10 +0300

    Experimental backend change: disabled token handler. If this is stable on 5Dc, it's probably the way to go.

--- src/property.c
+++ src/property.c
@@ -100,8 +100,8 @@ prop_init( void* unused )
         property_list,
         actual_num_properties,
         global_property_handler,
-        &global_token,
-        global_token_handler
+        0,
+        0
     );
 }
 
