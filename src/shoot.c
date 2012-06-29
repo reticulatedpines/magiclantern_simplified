b6143d353213e4787f47614576b468bc73d6e855
alex <alex@thinkpad>
2012-06-29 18:10:04 +0300

    Minor tweak

--- src/shoot.c
+++ src/shoot.c
@@ -5706,7 +5706,7 @@ shoot_task( void* unused )
                 //TODO: maybe get the spot yuv of the target box
                 get_spot_yuv(100, &y, &u, &v);
                 aev = y / 2;
-                if (K > 20) bmp_printf(FONT_MED, 0, 100, "Average exposure: %3d    New exposure: %3d   ", old_ae_avg/100, aev);
+                if (K > 20) bmp_printf(FONT_MED, 0, 80, "Average exposure: %3d    New exposure: %3d   ", old_ae_avg/100, aev);
                 if (K > 20 && ABS(old_ae_avg/100 - aev) >= (int)motion_detect_level)
                 {
                     lens_take_picture(64,1);
@@ -5718,7 +5718,7 @@ shoot_task( void* unused )
             else if (motion_detect_trigger == 1)
             {
                 int d = get_spot_motion(100, get_global_draw());
-                if (K > 20) bmp_printf(FONT_MED, 0, 100, "Motion level: %d   ", d);
+                if (K > 20) bmp_printf(FONT_MED, 0, 80, "Motion level: %d   ", d);
                 if (K > 20 && d >= (int)motion_detect_level)
                 {
                     lens_take_picture(64,1);
