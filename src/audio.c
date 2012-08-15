a9b4d2a67896576c4720ed05666c1c64e6068a69
miyake_t <kotyatokino@gmail.com>
2012-08-15 09:51:46 +0900

    delete duplicated lineout_onoff().

--- src/audio.c
+++ src/audio.c
@@ -1310,6 +1310,7 @@ audio_ic_set_lineout_vol(){
 
 static void
 audio_ic_set_lineout_onoff(){
+    if(cfg_override_audio == 0) return;
     //PDF p38
     if(audio_monitoring && AUDIO_MONITORING_HEADPHONES_CONNECTED){
 
@@ -1346,7 +1347,6 @@ void
 call_audio_ic_set_lineout_onoff(){
     audio_ic_set_input();
     audio_monitoring_update(); //call audio_monitoring_force_display()
-    audio_ic_set_lineout_onoff();
 }
 
 static void
@@ -2188,9 +2188,6 @@ static void
     audio_monitoring_toggle( void * priv, int delta )
 {
         audio_monitoring = !audio_monitoring;
-#ifdef CONFIG_600D
-        audio_ic_set_lineout_onoff();
-#endif
         audio_monitoring_update(); //call audio_monitoring_force_display()
 
 }
