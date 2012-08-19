2bf569dd1eb33390e4e0d2bcd96970659e96c69b
Nanomad <condellog@gmail.com>
2012-08-19 15:19:07 +0200

    1100D: P mode starts with no options highlighted

--- platform/1100D.105/consts.h
+++ platform/1100D.105/consts.h
@@ -42,12 +42,16 @@
 
 #define BGMT_AV_MOVIE (event->type == 0 && event->param == 0x61 && (is_movie_mode() && event->arg == 0xa))
 #define INT_EV_OBJ (*(int*)(event->obj))
+
 #define BGMT_PRESS_AV (BGMT_AV && ( \
 		(is_movie_mode() && INT_EV_OBJ == 0x3010040) || \
+		(shooting_mode == SHOOTMODE_P && INT_EV_OBJ == 0x3010040) || \
 		(shooting_mode == SHOOTMODE_M && INT_EV_OBJ == 0x1010006) || \
 		(INT_EV_OBJ == (0x1010040+2*shooting_mode)) ))
+
 #define BGMT_UNPRESS_AV (BGMT_AV && ( \
 		(is_movie_mode() && INT_EV_OBJ == 0x1010040) || \
+		(shooting_mode == SHOOTMODE_P && INT_EV_OBJ == 0x1010040) || \
 		(shooting_mode == SHOOTMODE_M && INT_EV_OBJ == 0x3010006) || \
 		(INT_EV_OBJ == (0x3010040+2*shooting_mode)) ))
 
