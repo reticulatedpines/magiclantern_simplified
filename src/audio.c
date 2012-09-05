c9f4a9bebb11e4334c0631232902ecaea06566c3
miyake_t <kotyatokino@gmail.com>
2012-09-06 00:02:31 +0900

    version string update

--- src/audio.c
+++ src/audio.c
@@ -447,7 +447,7 @@ meter_task( void* unused )
 
 #ifdef CONFIG_AUDIO_600D_DEBUG
 //will delete this when we finish debugging
-    NotifyBox(3000, "    ML2.3 TEST release:    \n      600D audio 0.10      ");
+    NotifyBox(3000, "    ML2.3 TEST release:    \n      600D audio 0.11      ");
     msleep(4000);
     NotifyBox( 250, " FOR TESTING PURPOSE ONLY. ");
     msleep(500);
