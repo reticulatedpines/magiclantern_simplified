08d1bcabd2c96e9f580beb02acc87bcda91d93a8
miyake_t <kotyatokino@gmail.com>
2012-09-20 01:59:02 +0900

    delete splash screen

--- src/audio-common.c
+++ src/audio-common.c
@@ -388,29 +388,6 @@ static void
 meter_task( void* unused )
 {
 
-#ifdef CONFIG_AUDIO_600D_DEBUG_____
-    //will delete this when we finish debugging
-    NotifyBox(3000, "    ML2.3 TEST release:    \n      600D audio 0.11      ");
-    msleep(4000);
-    NotifyBox( 250, " FOR TESTING PURPOSE ONLY. ");
-    msleep(500);
-    NotifyBox( 250, " FOR TESTING PURPOSE ONLY. ");
-    msleep(500);
-    NotifyBox( 250, " FOR TESTING PURPOSE ONLY. ");
-    msleep(500);
-    NotifyBox( 250, " FOR TESTING PURPOSE ONLY. ");
-    msleep(500);
-    NotifyBox( 250, " FOR TESTING PURPOSE ONLY. ");
-    msleep(500);
-    NotifyBox( 250, " FOR TESTING PURPOSE ONLY. ");
-    msleep(500);
-    NotifyBox( 250, " FOR TESTING PURPOSE ONLY. ");
-    msleep(500);
-    NotifyBox(4000, "   If you find problems    \n   please report them to   \n    www.magiclantern.fm    ");
-    msleep(5000);
-    NotifyBox(4000, "and use the stable release.\n Last Stable release: 2.3  ");
-#endif
-
 #ifdef CONFIG_600D
     //initialize audio config for 600D
     audio_configure(1);    
