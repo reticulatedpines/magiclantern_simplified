eb5ca8f9bb3a4455b482bbda9c3ebdf9e33c25b2
g3gg0 <devnull@localhost>
2012-11-15 01:31:29 +0100

    all: abort compilation with error message if compiling with unknown platform 7D: added neccessary includes

--- src/consts.h
+++ src/consts.h
@@ -1,47 +1,72 @@
+
+/* will get undefined if camera model is known */
+#define CONFIG_ERROR
+
 #ifdef CONFIG_550D
 #include "../platform/550D.109/consts.h"
+#undef CONFIG_ERROR
 #endif
 
 #ifdef CONFIG_60D
 #include "../platform/60D.111/consts.h"
+#undef CONFIG_ERROR
 #endif
 
 #ifdef CONFIG_600D
 #include "../platform/600D.102/consts.h"
+#undef CONFIG_ERROR
 #endif
 
 #ifdef CONFIG_50D
 #include "../platform/50D.109/consts.h"
+#undef CONFIG_ERROR
 #endif
 
 #ifdef CONFIG_500D
 #include "../platform/500D.111/consts.h"
+#undef CONFIG_ERROR
 #endif
 
 #ifdef CONFIG_1100D
 #include "../platform/1100D.105/consts.h"
+#undef CONFIG_ERROR
 #endif
 
 #ifdef CONFIG_5D2
 #include "../platform/5D2.212/consts.h"
+#undef CONFIG_ERROR
 #endif
 
 #ifdef CONFIG_5D3
 #include "../platform/5D3.113/consts.h"
+#undef CONFIG_ERROR
 #endif
 
 #ifdef CONFIG_5DC
 #include "../platform/5DC.111/consts.h"
+#undef CONFIG_ERROR
+#endif
+
+#ifdef CONFIG_7D_MASTER
+#include "../platform/7D_MASTER.203/consts.h"
+#undef CONFIG_ERROR
 #endif
 
 #ifdef CONFIG_7D
 #include "../platform/7D.203/consts.h"
+#undef CONFIG_ERROR
 #endif
 
 #ifdef CONFIG_40D
 #include "../platform/40D.111/consts.h"
+#undef CONFIG_ERROR
 #endif
 
 #ifdef CONFIG_EOSM
 #include "../platform/EOSM.100/consts.h"
+#undef CONFIG_ERROR
+#endif
+
+#ifdef CONFIG_ERROR
+#error This file does not contain this camera model. Please add it.
 #endif
--- src/gui.h
+++ src/gui.h
@@ -1,52 +1,76 @@
 #ifndef _big_gui_h_
 #define _big_gui_h_
 
+/* will get undefined if camera model is known */
+#define CONFIG_ERROR
+
 #ifdef CONFIG_550D
 #include "../platform/550D.109/gui.h"
+#undef CONFIG_ERROR
 #endif
 
 #ifdef CONFIG_60D
 #include "../platform/60D.111/gui.h"
+#undef CONFIG_ERROR
 #endif
 
 #ifdef CONFIG_600D
 #include "../platform/600D.102/gui.h"
+#undef CONFIG_ERROR
 #endif
 
 #ifdef CONFIG_50D
 #include "../platform/50D.109/gui.h"
+#undef CONFIG_ERROR
 #endif
 
 #ifdef CONFIG_500D
 #include "../platform/500D.111/gui.h"
+#undef CONFIG_ERROR
 #endif
 
 #ifdef CONFIG_1100D
 #include "../platform/1100D.105/gui.h"
+#undef CONFIG_ERROR
 #endif
 
 #ifdef CONFIG_5D2
 #include "../platform/5D2.212/gui.h"
+#undef CONFIG_ERROR
 #endif
 
 #ifdef CONFIG_5D3
 #include "../platform/5D3.113/gui.h"
+#undef CONFIG_ERROR
 #endif
 
 #ifdef CONFIG_5DC
 #include "../platform/5DC.111/gui.h"
+#undef CONFIG_ERROR
 #endif
 
 #ifdef CONFIG_7D
 #include "../platform/7D.203/gui.h"
+#undef CONFIG_ERROR
+#endif
+
+#ifdef CONFIG_7D_MASTER
+#include "../platform/7D_MASTER.203/gui.h"
+#undef CONFIG_ERROR
 #endif
 
 #ifdef CONFIG_40D
 #include "../platform/40D.111/gui.h"
+#undef CONFIG_ERROR
 #endif
 
 #ifdef CONFIG_EOSM
 #include "../platform/EOSM.100/gui.h"
+#undef CONFIG_ERROR
+#endif
+
+#ifdef CONFIG_ERROR
+#error This file does not contain this camera model. Please add it.
 #endif
 
 #define FAKE_BTN -123456
