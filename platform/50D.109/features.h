10ab58adf43bc7bec25394d3c235a7aa75f89ac3
alex <alex@thinkpad>
2012-11-24 10:16:57 +0200

    Forgot to add internals.h and features.h for 500D and 50D

---
+++ platform/50D.109/features.h
@@ -0,0 +1,13 @@
+#include "all_features.h"
+
+// 50D movie hacks
+#define FEATURE_MOVIE_RECORDING_50D
+#define FEATURE_MOVIE_RECORDING_50D_SHUTTER_HACK
+#define FEATURE_LVAE_EXPO_LOCK
+
+// no audio at all
+#undef FEATURE_AUDIO_METERS
+#undef FEATURE_BEEP
+#undef FEATURE_WAV_RECORDING
+#undef FEATURE_VOICE_TAGS
+#undef FEATURE_AUDIO_REMOTE_SHOT
