d068955704a00106b400ce7be2d09926bfea9e86
jplxpto <joao_pedro_lx@hotmail.com>
2012-11-25 00:48:16 +0000

    40D: Enable some overlays features.

--- platform/40D.111/features.h
+++ platform/40D.111/features.h
@@ -1,6 +1,26 @@
 // Small feature set, we'll just define each one 
 // look in all_features.h
 
+/** Overlay menu **/
+
+	#define FEATURE_GLOBAL_DRAW
+#define FEATURE_ZEBRA
+	#define FEATURE_ZEBRA_FAST
+    //~ #define FEATURE_FOCUS_PEAK
+    //~ #define FEATURE_FOCUS_PEAK_DISP_FILTER // too slow
+    //~ #define FEATURE_MAGIC_ZOOM
+    //~ #define FEATURE_CROPMARKS
+    //~ #define FEATURE_GHOST_IMAGE
+#define FEATURE_SPOTMETER
+    //~ #define FEATURE_FALSE_COLOR
+#define FEATURE_HISTOGRAM
+#define FEATURE_WAVEFORM
+#define FEATURE_VECTORSCOPE
+    
+#define FEATURE_OVERLAYS_IN_PLAYBACK_MODE
+    
+/** Focus menu **/
+
 #define FEATURE_AF_PATTERNS
 
 // anything else working? no idea
