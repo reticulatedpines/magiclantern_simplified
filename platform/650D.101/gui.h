96a19de23c64f45bfe8b2e95806a0c61cedc9f83
nanomad <devnull@localhost>
2012-12-13 15:51:54 +0100

    650D: Update consts

--- platform/650D.101/gui.h
+++ platform/650D.101/gui.h
@@ -2,9 +2,9 @@
 #define _cameraspecific_gui_h_
 
     // touch events
-    #define TOUCH_1_FINGER 0x6f
-    #define UNTOUCH_1_FINGER 0x70
-    #define TOUCH_2_FINGER 0x76
+#define TOUCH_1_FINGER 0x6b
+    #define UNTOUCH_1_FINGER 0x6c
+#define TOUCH_2_FINGER 0x72
     #define UNTOUCH_2_FINGER 0x77
 
     // used for knowing when canon's lv overlays are showing
@@ -16,39 +16,39 @@
     // need to print those on screen
     #define BGMT_WHEEL_UP 0
     #define BGMT_WHEEL_DOWN 1
-    #define BGMT_WHEEL_LEFT 2
-    #define BGMT_WHEEL_RIGHT 3
-    #define BGMT_PRESS_SET 4
-    #define BGMT_UNPRESS_SET 5
+#define BGMT_WHEEL_LEFT 2
+#define BGMT_WHEEL_RIGHT 3
+#define BGMT_PRESS_SET 4
+#define BGMT_UNPRESS_SET 5
     #define BGMT_MENU 6
-    #define BGMT_INFO 7
+#define BGMT_INFO 7
     #define BGMT_PRESS_DISP 8 // new, old value for BGMT_Q
     #define BGMT_UNPRESS_DISP 9 // new, old value for BGMT_PLAY
     #define BGMT_PLAY 0xB
-    #define BGMT_TRASH 0xD
+#define BGMT_TRASH 0xD
 
     #define BGMT_REC 0x1E
 
     #define BGMT_LV 0x1E
-    #define BGMT_Q 0x1D
+#define BGMT_Q 0x1D
     //~ #define BGMT_Q_ALT 0x67
 
     //~ #define BGMT_FUNC 0x12
     #define BGMT_PICSTYLE 0x13
     //~ #define BGMT_JOY_CENTER (lv ? 0x1e : 0x3b)
 
-    #define BGMT_PRESS_UP 0x2a          //~ unpress = 0x2b
-    #define BGMT_UNPRESS_UP 0x2b
+#define BGMT_PRESS_UP 0x28          //~ unpress = 0x2b
+#define BGMT_UNPRESS_UP 0x29
     #define BGMT_PRESS_UP_RIGHT 0x17
     #define BGMT_PRESS_UP_LEFT 0x18
-    #define BGMT_PRESS_RIGHT 0x26       //~ unpress = 0x27
-    #define BGMT_UNPRESS_RIGHT 0x27
-    #define BGMT_PRESS_LEFT 0x28        //~ unpress = 0x29
-    #define BGMT_UNPRESS_LEFT 0x29
+#define BGMT_PRESS_LEFT 0x26       //~ unpress = 0x27
+#define BGMT_UNPRESS_LEFT 0x27
+#define BGMT_PRESS_RIGHT 0x24      //~ unpress = 0x29
+#define BGMT_UNPRESS_RIGHT 0x25
     #define BGMT_PRESS_DOWN_RIGHT 0x1B
     #define BGMT_PRESS_DOWN_LEFT 0x1C
-    #define BGMT_PRESS_DOWN 0x2c        //~ unpress = 0x2d
-    #define BGMT_UNPRESS_DOWN 0x2d
+#define BGMT_PRESS_DOWN 0x2a       //~ unpress = 0x2d
+#define BGMT_UNPRESS_DOWN 0x2b
 
     #define BGMT_PRESS_HALFSHUTTER 0x50
     #define BGMT_UNPRESS_HALFSHUTTER 0x51
