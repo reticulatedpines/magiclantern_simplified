79a197add40819660fa143d42f903931bbc20cef
Coutts <andrewjohncoutts@gmail.com>
2012-12-14 02:06:02 -0500

    6D: firmware dumped, so added 6D skeleton to get ready for printing "hello world".

---
+++ platform/6D.112/internals.h
@@ -0,0 +1,100 @@
+/**
+ * Camera internals for 60D 1.1.1
+ */
+
+/** Properties are persistent (saved in NVRAM) => a mistake can cause permanent damage. Undefine this for new ports. */
+/** The 5D3 port is young, so we don't enable these for now. **/
+//~ #define CONFIG_PROP_REQUEST_CHANGE
+
+/** 
+ * State object hooks are pieces of code that run in Canon tasks (state objects). See state-object.c . 
+ * They might slow down Canon code, so here you can disable all of them (useful for debugging or early ports) 
+ */
+#define CONFIG_STATE_OBJECT_HOOKS
+
+/** This camera runs DryOS **/
+//~ #define CONFIG_VXWORKS
+
+/** This camera has a full-frame sensor */
+#define CONFIG_FULLFRAME
+
+/** This camera has LiveView and can record video **/
+#define CONFIG_LIVEVIEW
+#define CONFIG_MOVIE
+
+/** This camera has a 3:2 screen, 720x480 **/
+#define CONFIG_3_2_SCREEN
+
+/** We only have a single red LED **/
+//~ #define CONFIG_BLUE_LED
+
+/** There is no LCD sensor that turns the display off **/
+//~ #define CONFIG_LCD_SENSOR
+
+/** This camera has a mirror lockup feature **/
+#define CONFIG_MLU
+
+/** This camera reports focus info in LiveView **/
+#define CONFIG_LV_FOCUS_INFO
+
+/** Reports roll and pitch angle, but it's unstable, leave it off **/
+//~ #define CONFIG_ELECTRONIC_LEVEL
+
+/** Define this if the camera has an ambient light sensor used for auto brightness **/
+#define CONFIG_AUTO_BRIGHTNESS
+
+/** There is a Q menu in Play mode, with image protect, rate etc **/
+/** But it's a bit different from the other cameras, so let's say it doesn't have **/
+//~ #define CONFIG_Q_MENU_PLAYBACK
+
+/** No flip-out display **/
+//~ #define CONFIG_VARIANGLE_DISPLAY
+
+/** It has a 5D2-like battery which reports exact percentage **/
+#define CONFIG_BATTERY_INFO
+
+/** We can do bulb exposures **/
+#define CONFIG_BULB
+
+/** There is a separate bulb mode on the mode dial (other cameras have BULB = M + shutter speed beyond 30s) **/
+#define CONFIG_SEPARATE_BULB_MODE
+
+/** We can't control audio settings from ML **/
+//~ #define CONFIG_AUDIO_CONTROLS
+
+/** Zoom button can be used while recording (for Magic Zoom) **/
+//~ #define CONFIG_ZOOM_BTN_NOT_WORKING_WHILE_RECORDING
+
+/** We can redirect the display buffer to some arbitrary address, just by changing YUV422_LV_BUFFER_DISPLAY_ADDR **/
+#define CONFIG_CAN_REDIRECT_DISPLAY_BUFFER_EASILY
+#define CONFIG_CAN_REDIRECT_DISPLAY_BUFFER
+
+/** Therefore, we can implement display filters (features that alter the LiveView image in real-time) **/
+#define CONFIG_DISPLAY_FILTERS
+
+/** We can override ISO on a per-frame basis, by changing FRAME_ISO (e.g. for HDR video or gradual exposure) **/
+#define CONFIG_FRAME_ISO_OVERRIDE
+
+/** But we can't override the digital ISO component via FRAME_ISO **/
+#define CONFIG_FRAME_ISO_OVERRIDE_ANALOG_ONLY
+
+/** We can change ExpSim from ML **/
+#define CONFIG_EXPSIM
+
+/** We can playback sounds via ASIF DMA **/
+#define CONFIG_BEEP
+
+/** This camera has no trouble saving Kelvin and/or WBShift in movie mode **/
+//~ #define CONFIG_WB_WORKAROUND
+
+/** We can't restore ML files after formatting the card in the camera **/
+//~ #define CONFIG_RESTORE_AFTER_FORMAT
+
+/** We don't know how to use DMA_MEMCPY (yet) **/
+//~ #define CONFIG_DMA_MEMCPY
+
+/** We shouldn't warn the user if movie exposure is Auto **/
+//~ #define CONFIG_MOVIE_AE_WARNING
+
+/** We can display some extra info in photo mode (not LiveView) **/
+#define CONFIG_PHOTO_MODE_INFO_DISPLAY
