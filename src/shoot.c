62f05820c01a20233bc99fa1e4242470f88a84fb
scrax <scrizza@gmail.com>
2013-01-01 23:12:40 +0100

    fixed HTP on shown

--- src/shoot.c
+++ src/shoot.c
@@ -3274,12 +3274,6 @@ void hdr_display_status(int fnt)
         }
     #else
         int bg = bmp_getpixel(HTP_STATUS_POS_X,HTP_STATUS_POS_Y);
-        if (get_htp())
-        {
-            bmp_fill(bg,HTP_STATUS_POS_X,HTP_STATUS_POS_Y,60,46); // HIDE ALO ICON
-            bmp_printf(FONT(FONT_LARGE, COLOR_FG_NONLV, bg), HTP_STATUS_POS_X, HTP_STATUS_POS_Y, "HTP");
-            bmp_printf(FONT(FONT_MED, COLOR_FG_NONLV, bg), HTP_STATUS_POS_X+40, HTP_STATUS_POS_Y+30, "ON");
-        }
         
         bg = bmp_getpixel(HDR_STATUS_POS_X-30,HDR_STATUS_POS_Y);
         //fnt = FONT(FONT_LARGE, COLOR_YELLOW, bg);
@@ -3292,6 +3286,14 @@ void hdr_display_status(int fnt)
     }
     #endif
     #endif
+#ifdef CONFIG_PHOTO_MODE_INFO_DISPLAY
+    if (get_htp())
+    {
+        bmp_fill(bg,HTP_STATUS_POS_X,HTP_STATUS_POS_Y,60,46); // HIDE ALO ICON
+        bmp_printf(FONT(FONT_LARGE, COLOR_FG_NONLV, bg), HTP_STATUS_POS_X, HTP_STATUS_POS_Y, "HTP");
+        bmp_printf(FONT(FONT_MED, COLOR_FG_NONLV, bg), HTP_STATUS_POS_X+40, HTP_STATUS_POS_Y+30, "ON");
+    }
+#endif
 }
 
 #ifdef FEATURE_HDR_BRACKETING
