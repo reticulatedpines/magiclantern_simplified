d10907cca51351128c4f85bbf63097e2b6c6482a
alex <alex@thinkpad>
2013-01-08 10:38:49 +0200

    Typo (meant 6D, not 60D)

--- platform/60D.111/features.h
+++ platform/60D.111/features.h
@@ -3,5 +3,3 @@
 #define FEATURE_SWAP_MENU_ERASE
 #define FEATURE_EYEFI_TRICKS
 #define FEATURE_REC_NOTIFY_BEEP
-
-#undef FEATURE_MOVIE_RESTART // legal uncertainity (the only purpose would be bypassing the 30-minute EU limit)
--- platform/6D.112/features.h
+++ platform/6D.112/features.h
@@ -3,3 +3,5 @@
 #undef FEATURE_QUICK_ZOOM
 #undef FEATURE_VOICE_TAGS
 #undef FEATURE_AF_PATTERNS
+
+#undef FEATURE_MOVIE_RESTART // legal uncertainity (the only purpose would be bypassing the 30-minute EU limit)
