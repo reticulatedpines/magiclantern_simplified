63cd094eedf3acffea3eeea16f3581d37bd76456
alex <alex@thinkpad>
2013-01-17 23:10:58 +0200

    Added CONFIG_TSKMON => 8K more free RAM on 60D and 6D. Disabled some debug things for 6D. Not tested.

--- platform/6D.112/features.h
+++ platform/6D.112/features.h
@@ -5,3 +5,15 @@
 #undef FEATURE_AF_PATTERNS
 
 #undef FEATURE_MOVIE_RESTART // legal uncertainity (the only purpose would be bypassing the 30-minute EU limit)
+
+// the 6D has very little RAM; disable some debug things
+
+#undef FEATURE_SHOW_TASKS
+#undef FEATURE_SHOW_CPU_USAGE
+#undef FEATURE_SHOW_GUI_EVENTS
+#undef FEATURE_SNAP_SIM
+
+#undef CONFIG_STRESS_TEST
+#undef CONFIG_BENCHMARKS
+
+#undef CONFIG_TSKMON
