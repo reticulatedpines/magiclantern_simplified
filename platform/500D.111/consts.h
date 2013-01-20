e5e8a9c7f21709aed35178168f0dc021bb1b2135
alex <alex@thinkpad>
2013-01-20 17:31:58 +0200

    Fixed BULB_MIN_EXPOSURE for 500D, 650D and M

--- platform/500D.111/consts.h
+++ platform/500D.111/consts.h
@@ -188,7 +188,7 @@
 #define DIALOG_MnCardFormatBegin (0x242AC+4) // ret_CreateDialogBox(...DlgMnCardFormatBegin_handler...) is stored there
 #define DIALOG_MnCardFormatExecute (0x24398+4) // similar
 
-#define BULB_MIN_EXPOSURE 500
+#define BULB_MIN_EXPOSURE 1000
 
 // http://magiclantern.wikia.com/wiki/Fonts
 #define BFNT_CHAR_CODES    0xFF61E250
--- platform/650D.101/consts.h
+++ platform/650D.101/consts.h
@@ -144,7 +144,7 @@
 #define DIALOG_MnCardFormatBegin (0x44C38) // ret_CreateDialogBox(...DlgMnCardFormatBegin_handler...) is stored there
 #define DIALOG_MnCardFormatExecute (0x48CFC) // similar
 
-    #define BULB_MIN_EXPOSURE 500
+    #define BULB_MIN_EXPOSURE 1000
 
 // http://magiclantern.wikia.com/wiki/Fonts
 #define BFNT_CHAR_CODES    0xffcca8a8
