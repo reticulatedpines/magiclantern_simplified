a5af4a49b27fdaf65f5cd8d2d12786be7ab32e31
nanomad <devnull@localhost>
2013-01-22 21:58:36 +0100

    650D: Disable more audio/wav features

--- platform/650D.101/features.h
+++ platform/650D.101/features.h
@@ -3,9 +3,14 @@
 #define FEATURE_LV_3RD_PARTY_FLASH
 #define FEATURE_EYEFI_TRICKS
 
+// Disable all audio stuff
+#undef FEATURE_WAV_RECORDING
+#undef FEATURE_FPS_WAV_RECORD
+#undef FEATURE_BEEP
 #undef FEATURE_VOICE_TAGS
 #undef FEATURE_AUDIO_REMOTE_SHOT
 #undef FEATURE_AUDIO_METERS
+
 #undef FEATURE_AUTO_BURST_PICQ // maybe not working on 650D
 #undef FEATURE_OVERLAYS_IN_PLAYBACK_MODE // no DISP button
 #undef FEATURE_IMAGE_EFFECTS // none working
