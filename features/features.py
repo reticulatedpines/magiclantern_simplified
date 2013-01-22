59742ef272f8f5b3f21d894a866b2f2425f4b24f
nanomad <devnull@localhost>
2013-01-22 17:45:33 +0100

    Feature matrix: generate an html page using mako+bootstrap+jquery

--- features/features.py
+++ features/features.py
@@ -2,6 +2,21 @@
 # make a table with what features are enabled on what camera
 import os, sys, string, re
 import commands
+from mako.template import Template
+
+class Bunch(dict):
+    def __init__(self, d):
+        dict.__init__(self, d)
+        self.__dict__.update(d)
+
+def to_bunch(d):
+    r = {}
+    for k, v in d.items():
+        if isinstance(v, dict):
+            v = to_bunch(v)
+        r[k] = v
+    return Bunch(r)
+
 
 def run(cmd):
     return commands.getstatusoutput(cmd)[1]
@@ -10,7 +25,7 @@ cams = []
 
 for c in os.listdir("../platform"):
     if os.path.isdir(os.path.join("../platform", c)):
-        if "_" not in c and "all" not in c and "MASTER" not in c:
+        if "_" not in c and "all" not in c and "7D" not in c:
             cams.append(c)
 
 cams = sorted(cams)
@@ -24,7 +39,7 @@ for c in cams:
     F = run(cmd)
     for f in F.split('\n'):
         f = f.replace("#define", "").strip()
-        #~ print c,f
+        #print c,f
         FD[c,f] = True
         AF.append(f)
 
@@ -35,16 +50,25 @@ def cam_shortname(c):
     c = c.split(".")[0]
     return c.center(5)
 
-print "%30s" % "",
-for c in cams:
-    print "%3s" % cam_shortname(c),
-print ""
+#print "%30s" % "",
+#for c in cams:
+#    print "%3s" % cam_shortname(c),
+#print ""
 
-for f in AF:
-    print "%30s" % f[8:38],
-    for c in cams:
-        if FD.get((c,f)):
-            print "  *  ",
-        else:
-            print "     ",
-    print ""
+#for f in AF:
+#    print "%30s" % f[8:38],
+#    for c in cams:
+#        if FD.get((c,f)):
+#            print "  *  ",
+#        else:
+#            print "     ",
+#    print ""
+
+shortnames = {}
+for c in cams:
+    shortnames[c]=cam_shortname(c)
+
+data = {'FD':FD, 'AF':AF, 'cams':cams, 'shortnames':shortnames}
+mytemplate = Template(filename='features.tmpl')
+print mytemplate.render(**data)
+
