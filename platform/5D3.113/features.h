fcb4a5461895905a3fc5de86d89b379b4d065a19
alex <alex@thinkpad>
2013-01-31 12:02:53 +0200

    5D3: disable ISR hooks (doesn't compile)

--- platform/5D3.113/features.h
+++ platform/5D3.113/features.h
@@ -11,7 +11,7 @@
 #undef FEATURE_SHUTTER_LOCK // Canon has a dedicated button for it
 #undef FEATURE_FLASH_TWEAKS // no built-in flash
 
-#define FEATURE_ISR_HOOKS
+//~ #define FEATURE_ISR_HOOKS // doesn't compile
 #define FEATURE_KEN_ROCKWELL_ZOOM_5D3
 #define FEATURE_ZOOM_TRICK_5D3 // not reliable
 //~ #define FEATURE_REMEMBER_LAST_ZOOM_POS_5D3 // too many conflicts with other features
