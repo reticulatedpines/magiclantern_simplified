4fa0897c34a22d8269f5cfda86087fe1e3c2b89a
nanomad <devnull@localhost>
2013-02-02 18:30:52 +0100

    EOSM: Remove old code

--- platform/EOSM.106/misc.c
+++ platform/EOSM.106/misc.c
@@ -7,33 +7,6 @@
 #include <consts.h>
 #include <lens.h>
 
-void display_shooting_info() // called from debug task
-{
-	if (lv) return;
-	
-	uint32_t fnt = SHADOW_FONT(FONT_MED);
-
-	if (lens_info.wb_mode == WB_KELVIN)
-	{
-		bmp_printf(fnt, 152, 280, "%5dK", lens_info.kelvin);
-	}
-	if (lens_info.wbs_gm || lens_info.wbs_ba)
-	{
-		int ba = lens_info.wbs_ba;
-		if (ba) bmp_printf(fnt, 270 + 2 * font_med.width, 280, "%s%d", ba > 0 ? "A" : "B", ABS(ba));
-		else    bmp_printf(fnt, 270 + 2 * font_med.width, 280, "  ");
-
-		int gm = lens_info.wbs_gm;
-		if (gm) bmp_printf(fnt, 270, 280, "%s%d", gm > 0 ? "G" : "M", ABS(gm));
-		else    bmp_printf(fnt, 270, 280, "  ");
-	}
-	
-	hdr_display_status(fnt);
-
-	bmp_printf(fnt, 40, 460, get_mlu() ? "MLU" : "   ");
-
-	display_trap_focus_info();
-}
 
 int new_LiveViewApp_handler = 0xff123456;
 
