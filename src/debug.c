086f22089bb044833a6cea28d25802591b89d3a0
alex <alex@thinkpad>
2013-02-03 20:53:45 +0200

    BSOD (thanks SDX)

--- src/debug.c
+++ src/debug.c
@@ -574,8 +574,41 @@ FILE * movfile;
 int record_uncomp = 0;
 #endif
 
+void bsod()
+{
+    msleep(rand() % 20000 + 2000);
+    
+    do {
+        SetGUIRequestMode(1);
+        msleep(1000);
+    } while (CURRENT_DIALOG_MAYBE != 1);
+    
+    bmp_fill(COLOR_BLUE, 0, 0, 720, 480);
+    int fnt = SHADOW_FONT(FONT_MED);
+    int h = font_med.height;
+    int y = 50;
+    bmp_printf(fnt, 0, y+=h, "   A problem has been detected and Magic Lantern has been"   );
+    bmp_printf(fnt, 0, y+=h, "   shut down to prevent damage to your camera."              );
+    y += h;
+    bmp_printf(fnt, 0, y+=h, "   If this is the first time you've seen this Stop error"    );
+    bmp_printf(fnt, 0, y+=h, "   screen, restart your camera. If this screen appears"      );
+    bmp_printf(fnt, 0, y+=h, "   again, follow these steps:"                               );
+    y += h;
+    bmp_printf(fnt, 0, y+=h, "   Don't click things you don't know what they do."          );
+    y += h;
+    bmp_printf(fnt, 0, y+=h, "   Technical information:");
+    bmp_printf(fnt, 0, y+=h, "   *** STOP 0x000000aa (0x1000af22, 0xdeadbeef, 0xffff)"     );
+    y += h;
+    bmp_printf(fnt, 0, y+=h, "   Beginning dump of physical memory"                        );
+    bmp_printf(fnt, 0, y+=h, "   Physical memory dump complete. Your camera is bricked."   );
+    y += h;
+    bmp_printf(fnt, 0, y+=h, "   Contact the Magic Lantern guys at www.magiclantern.fm"    );
+    bmp_printf(fnt, 0, y+=h, "   for further assistance and information."                  );
+}
+
 void run_test()
 {
+    bsod();
     call("dumpf");
 }
 
