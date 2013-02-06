0abf9501ea910b9709569951cd2de666422289ff
alex <alex@thinkpad>
2013-02-06 16:24:29 +0200

    Submenu spacing tweaks

--- src/menu.c
+++ src/menu.c
@@ -1471,7 +1471,7 @@ entry_print(
     if (customize_mode)
     {
         // reserve space for icons
-        if (entry != &customize_menu[0] && !is_menu_active(MY_MENU_NAME))
+        if (entry != &customize_menu[0] && !is_menu_active(MY_MENU_NAME) && !in_submenu)
             xc = x_end - 70;
         else
             xc = x_end;
@@ -1958,7 +1958,9 @@ submenu_display(struct menu * submenu)
     int count = 0;
     struct menu_entry * child = submenu->children;
     while (child) { if (IS_VISIBLE(child)) count++; child = child->next; }
-    int h = submenu->submenu_height ? submenu->submenu_height : (int)MIN((count + 3) * font_large.height, 420);
+    int h = submenu->submenu_height ? submenu->submenu_height : count * font_large.height + 40 + 50;
+    if (count > 5) h -= font_large.height;
+    if (h > 420) h = 420;
     int w = submenu->submenu_width  ? submenu->submenu_width : 600;
     g_submenu_width = w;
     int bx = (720 - w)/2;
@@ -1990,7 +1992,7 @@ submenu_display(struct menu * submenu)
             draw_line(xl, yl, xl+i, yl+7, COLOR_CYAN);
     }
 
-    menu_display(submenu,  bx + SUBMENU_OFFSET,  by + 40 + 25 - (h > 400 ? 10 : 0), 0);
+    menu_display(submenu,  bx + SUBMENU_OFFSET,  by + 40 + (count > 5 ? 10 : 25), 0);
     show_hidden_items(submenu, 1);
 }
 
