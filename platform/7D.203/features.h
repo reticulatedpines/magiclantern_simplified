35f83956356b4a2c3ef23fe73fba0b4833b565c7
g3gg0 <devnull@localhost>
2013-03-03 12:53:48 +0100

    7D: enable AFMA, disable non-working features

--- platform/7D.203/features.h
+++ platform/7D.203/features.h
@@ -5,8 +5,11 @@
 #undef FEATURE_FPS_OVERRIDE
 #undef FEATURE_FPS_WAV_RECORD
 #undef FEATURE_FPS_RAMPING
-#undef FEATURE_EXPO_ISO_DIGIC // not working, no idea why
+#undef FEATURE_EXPO_ISO_DIGIC // not working, no idea why -> test on master?
 #undef FEATURE_SHUTTER_LOCK // Canon has it
+#undef FEATURE_IMAGE_EFFECTS
+#undef FEATURE_FPS_OVERRIDE
 
 #define FEATURE_VIDEO_HACKS
 #define FEATURE_ISR_HOOKS
+#define FEATURE_AFMA_TUNING
