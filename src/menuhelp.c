1002c19f89e2ca3c674e39c772aa9fc1c96a5240
Giovanni Condello <condellog@gmail.com>
2013-03-17 14:33:03 +0100

    650D welcome page

--- src/menuhelp.c
+++ src/menuhelp.c
@@ -41,13 +41,12 @@ draw_beta_warning()
 {
     bmp_fill(COLOR_BLACK, 0, 0, 720, 480);
 
-    bfnt_puts("Magic Lantern", 242, 53, COLOR_WHITE, COLOR_BLACK);
+    bfnt_puts("Magic Lantern for 650D/T4i - Alpha 1", 50, 50, COLOR_WHITE, COLOR_BLACK);
 
-    bmp_printf(FONT_MED, 50, 150, "This is a development snapshot for testing purposes.");
+    bmp_printf(FONT_MED, 50, 150, "   This is a version with limited functionality,    ");
+    bmp_printf(FONT_MED, 50, 180, "   for testing purposes, not for production work.   ");
 
-    bmp_printf(FONT_MED, 50, 200, "   Please report all bugs at www.magiclantern.fm.   ");
-
-    bmp_printf(FONT_MED, 50, 250, "      Be careful using it for production work.      ");
+    bmp_printf(FONT_MED, 50, 250, "     Please report bugs at www.magiclantern.fm.     ");
 
     bmp_printf(FONT_MED, 50, 300, "                       Enjoy!                       ");
 
