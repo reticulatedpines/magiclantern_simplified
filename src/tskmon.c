d57faabba848b220ec1afcce2a4a50276e50825c
alex <alex@thinkpad>
2013-04-05 08:55:05 +0300

    1100D: ignoring the FileMgr bug

--- src/tskmon.c
+++ src/tskmon.c
@@ -222,7 +222,7 @@ static void null_pointer_check()
                 return;
             #endif
 
-            #if defined(CONFIG_550D) || defined(CONFIG_500D)
+            #if defined(CONFIG_550D) || defined(CONFIG_500D) || defined(CONFIG_1100D)
             if (streq(task_name, "FileMgr"))
                 return;
             #endif
