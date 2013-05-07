b9136ed63ba5f7e6b4d9f704fdc105edc05e0be6
alex <alex@thinkpad>
2013-05-07 19:49:40 +0300

    Memory benchmark: run every test for around 1 second

--- src/debug.c
+++ src/debug.c
@@ -954,18 +954,28 @@ typedef void (*mem_bench_fun)(
 );
 
 
-static void mem_benchmark_run(char* msg, int* y, int times, int bufsize, mem_bench_fun bench_fun, int arg0, int arg1, int arg2, int arg3)
+static void mem_benchmark_run(char* msg, int* y, int bufsize, mem_bench_fun bench_fun, int arg0, int arg1, int arg2, int arg3)
 {
     bmp_printf(FONT_LARGE, 0, 0, "%s...", msg);
 
+    int times = 0;
     int t0 = get_ms_clock_value();
-    for (int i = 0; i < times; i++)
+    for (int i = 0; i < 1000; i++)
     {
         bench_fun(arg0, arg1, arg2, arg3);
         if (i%2) info_led_off(); else info_led_on();
+        
+        /* run the benchmark for roughly 1 second */
+        if (get_ms_clock_value_fast() - t0 > 1000)
+        {
+            times = i + 1;
+            break;
+        }
     }
     int t1 = get_ms_clock_value();
     int dt = t1 - t0;
+    
+    info_led_off();
     
     /* units: KB/s */
     int speed = bufsize * times / dt;
@@ -1040,26 +1050,26 @@ static void mem_benchmark_task()
     defish_draw_lv_color();
     void defish_draw_lv_color_loop(uint64_t* src_buf, uint64_t* dst_buf, int* ind);
     if (defish_ind)
-    mem_benchmark_run("defish_draw_lv_color", &y, 50, 720*os.y_ex, (mem_bench_fun)defish_draw_lv_color_loop, (intptr_t)UNCACHEABLE(buf1), (intptr_t)UNCACHEABLE(buf2), defish_ind, 0);
+    mem_benchmark_run("defish_draw_lv_color", &y, 720*os.y_ex, (mem_bench_fun)defish_draw_lv_color_loop, (intptr_t)UNCACHEABLE(buf1), (intptr_t)UNCACHEABLE(buf2), defish_ind, 0);
 #endif
 
-    mem_benchmark_run("memcpy cacheable    ", &y, 50, bufsize, (mem_bench_fun)memcpy,     (intptr_t)CACHEABLE(buf1),   (intptr_t)CACHEABLE(buf2),   bufsize, 0);
-    mem_benchmark_run("memcpy uncacheable  ", &y, 50, bufsize, (mem_bench_fun)memcpy,     (intptr_t)UNCACHEABLE(buf1), (intptr_t)UNCACHEABLE(buf2), bufsize, 0);
-    mem_benchmark_run("memcpy64 cacheable  ", &y, 50, bufsize, (mem_bench_fun)memcpy64,   (intptr_t)CACHEABLE(buf1),   (intptr_t)CACHEABLE(buf2),   bufsize, 0);
-    mem_benchmark_run("memcpy64 uncacheable", &y, 50, bufsize, (mem_bench_fun)memcpy64,   (intptr_t)UNCACHEABLE(buf1), (intptr_t)UNCACHEABLE(buf2), bufsize, 0);
+    mem_benchmark_run("memcpy cacheable    ", &y, bufsize, (mem_bench_fun)memcpy,     (intptr_t)CACHEABLE(buf1),   (intptr_t)CACHEABLE(buf2),   bufsize, 0);
+    mem_benchmark_run("memcpy uncacheable  ", &y, bufsize, (mem_bench_fun)memcpy,     (intptr_t)UNCACHEABLE(buf1), (intptr_t)UNCACHEABLE(buf2), bufsize, 0);
+    mem_benchmark_run("memcpy64 cacheable  ", &y, bufsize, (mem_bench_fun)memcpy64,   (intptr_t)CACHEABLE(buf1),   (intptr_t)CACHEABLE(buf2),   bufsize, 0);
+    mem_benchmark_run("memcpy64 uncacheable", &y, bufsize, (mem_bench_fun)memcpy64,   (intptr_t)UNCACHEABLE(buf1), (intptr_t)UNCACHEABLE(buf2), bufsize, 0);
     #ifdef CONFIG_DMA_MEMCPY
-    mem_benchmark_run("dma_memcpy cacheable", &y, 50, bufsize, (mem_bench_fun)dma_memcpy, (intptr_t)CACHEABLE(buf1),   (intptr_t)CACHEABLE(buf2),   bufsize, 0);
-    mem_benchmark_run("dma_memcpy uncacheab", &y, 50, bufsize, (mem_bench_fun)dma_memcpy, (intptr_t)UNCACHEABLE(buf1), (intptr_t)UNCACHEABLE(buf2), bufsize, 0);
+    mem_benchmark_run("dma_memcpy cacheable", &y, bufsize, (mem_bench_fun)dma_memcpy, (intptr_t)CACHEABLE(buf1),   (intptr_t)CACHEABLE(buf2),   bufsize, 0);
+    mem_benchmark_run("dma_memcpy uncacheab", &y, bufsize, (mem_bench_fun)dma_memcpy, (intptr_t)UNCACHEABLE(buf1), (intptr_t)UNCACHEABLE(buf2), bufsize, 0);
     #endif
-    mem_benchmark_run("memset cacheable    ", &y, 50, bufsize, (mem_bench_fun)memset,     (intptr_t)CACHEABLE(buf1),   0,                           bufsize, 0);
-    mem_benchmark_run("memset uncacheable  ", &y, 50, bufsize, (mem_bench_fun)memset,     (intptr_t)UNCACHEABLE(buf1), 0,                           bufsize, 0);
-    mem_benchmark_run("memset64 cacheable  ", &y, 50, bufsize, (mem_bench_fun)memset64,   (intptr_t)CACHEABLE(buf1),   0,                           bufsize, 0);
-    mem_benchmark_run("memset64 uncacheable", &y, 50, bufsize, (mem_bench_fun)memset64,   (intptr_t)UNCACHEABLE(buf1), 0,                           bufsize, 0);
-    mem_benchmark_run("read32 cacheable    ", &y, 50, bufsize, (mem_bench_fun)mem_test_read32, (intptr_t)CACHEABLE(buf1),   bufsize, 0, 0);
-    mem_benchmark_run("read32 uncacheable  ", &y, 50, bufsize, (mem_bench_fun)mem_test_read32, (intptr_t)UNCACHEABLE(buf1), bufsize, 0, 0);
-    mem_benchmark_run("read64 cacheable    ", &y, 50, bufsize, (mem_bench_fun)mem_test_read64, (intptr_t)CACHEABLE(buf1),   bufsize, 0, 0);
-    mem_benchmark_run("read64 uncacheable  ", &y, 50, bufsize, (mem_bench_fun)mem_test_read64, (intptr_t)UNCACHEABLE(buf1), bufsize, 0, 0);
-    mem_benchmark_run("bmp_fill to idle buf", &y, 50, 720*480, (mem_bench_fun)mem_test_bmp_fill, 0, 0, 720, 480);
+    mem_benchmark_run("memset cacheable    ", &y, bufsize, (mem_bench_fun)memset,     (intptr_t)CACHEABLE(buf1),   0,                           bufsize, 0);
+    mem_benchmark_run("memset uncacheable  ", &y, bufsize, (mem_bench_fun)memset,     (intptr_t)UNCACHEABLE(buf1), 0,                           bufsize, 0);
+    mem_benchmark_run("memset64 cacheable  ", &y, bufsize, (mem_bench_fun)memset64,   (intptr_t)CACHEABLE(buf1),   0,                           bufsize, 0);
+    mem_benchmark_run("memset64 uncacheable", &y, bufsize, (mem_bench_fun)memset64,   (intptr_t)UNCACHEABLE(buf1), 0,                           bufsize, 0);
+    mem_benchmark_run("read32 cacheable    ", &y, bufsize, (mem_bench_fun)mem_test_read32, (intptr_t)CACHEABLE(buf1),   bufsize, 0, 0);
+    mem_benchmark_run("read32 uncacheable  ", &y, bufsize, (mem_bench_fun)mem_test_read32, (intptr_t)UNCACHEABLE(buf1), bufsize, 0, 0);
+    mem_benchmark_run("read64 cacheable    ", &y, bufsize, (mem_bench_fun)mem_test_read64, (intptr_t)CACHEABLE(buf1),   bufsize, 0, 0);
+    mem_benchmark_run("read64 uncacheable  ", &y, bufsize, (mem_bench_fun)mem_test_read64, (intptr_t)UNCACHEABLE(buf1), bufsize, 0, 0);
+    mem_benchmark_run("bmp_fill to idle buf", &y, 720*480, (mem_bench_fun)mem_test_bmp_fill, 0, 0, 720, 480);
     call("dispcheck");
     msleep(3000);
     canon_gui_enable_front_buffer(0);
