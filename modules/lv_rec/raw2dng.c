16ed15f86ece331aef75d9ea31a14d45248bbedb
alex <alex@thinkpad>
2013-05-12 15:37:27 +0300

    raw2dng: fixed segfault on certain files

--- modules/lv_rec/raw2dng.c
+++ modules/lv_rec/raw2dng.c
@@ -56,6 +56,7 @@ int main(int argc, char** argv)
     if (lv_rec_footer.xRes != raw_info.width)
     {
         raw_info.width = lv_rec_footer.xRes;
+        raw_info.pitch = raw_info.width * 14/8;
         raw_info.active_area.x1 = 0;
         raw_info.active_area.x2 = raw_info.width;
         raw_info.jpeg.x = 0;
