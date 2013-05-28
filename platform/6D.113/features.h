429b79940f37f5e6daceed7528fd4c3732367c7d
Coutts <andrewjohncoutts@gmail.com>
2013-05-28 10:44:17 -0400

    6D: finish merging /platform/ directory from 1%

--- platform/6D.113/features.h
+++ platform/6D.113/features.h
@@ -1,15 +1,16 @@
 #include "all_features.h"
 
-//Not Working Or Unnecessary
+
+/*Not Working Or Unnecessary*/
 #undef FEATURE_QUICK_ZOOM    //Canon Has it
 #undef FEATURE_AF_PATTERNS   // Not working will disable auto af points
-#undef FEATURE_IMAGE_EFFECTS // none working
-//~ #undef FEATURE_FLASH_TWEAKS // They work. More to come.
+#undef FEATURE_IMAGE_EFFECTS // Missing Zero Sharpness :( Regs have changed
 #undef FEATURE_IMAGE_REVIEW_PLAY // not needed, one can press Zoom right away
-#undef FEATURE_MLU_HANDHELD // not needed, Canon's silent mode is much better
+#undef CONFIG_STRESS_TEST // We don't need this so much.
+#undef CONFIG_MEMPATCH_CHECK // Reports 0 for total.
 
-//Work Relatively well
-//#define CONFIG_KILL_FLICKER // Kill canon popups
+
+/*Work Relatively well*/
 #define CONFIG_FPS_AGGRESSIVE_UPDATE
 #define FEATURE_MOVIE_AUTOSTOP_RECORDING
 #define FEATURE_REC_ON_RESUME
@@ -18,23 +19,24 @@
 #define FEATURE_KEN_ROCKWELL_ZOOM_5D3 // Play From Image Review Mode - Did not bring up play
 #define FEATURE_AFMA_TUNING
 #define CONFIG_AFMA_EXTENDED
-#define FEATURE_PREFIX //Named HDR Bracketing B01, B02, etc
 #define FEATURE_NOHELP //No one can help you now
-#undef CONFIG_MEMPATCH_CHECK // Reports 0 for total.
-#define FEATURE_AUDIO_METERS
+#define FEATURE_PREFIX		
+/* Development */
 
-
-// Development
-//#define FEATURE_SILENT_PIC_JPG // Does not work that way anymore.
-//#define FEATURE_HDR_EXTENDED // Broken By new menu change, please wait :(
-//~ #define FEATURE_SHOW_SIGNATURE
-//~ #define CONFIG_DUMPROM Lawyers will kill your camera.. and you.
-//~ #define FEATURE_SILENT_PIC_HIRES //Needs check of alignment.
-#undef FEATURE_VOICE_TAGS // Asif stop is broken^HHHH ... Fixed
-
+/* Debugging Stuff */
 #define FEATURE_PROP_DISPLAY
 #define CONFIG_HEXDUMP
 #define CONFIG_DIGIC_POKE
+#define FEATURE_DEBUG_INTERCEPT
+//~ #define FEATURE_SHOW_SIGNATURE
+//~ #define CONFIG_DUMPROM Lawyers will kill your camera.. and you.
 
-#undef CONFIG_TSKMON
 
+/* Audio Features */
+#define FEATURE_HEADPHONE_MONITORING
+#define FEATURE_NITRATE_WAV_RECORD
+#define FEATURE_WAV_RECORDING
+#define FEATURE_AUDIO_METERS
+
+//~ #define FEATURE_HEADPHONE_OUTPUT_VOLUME
+//#define CONFIG_AUDIO_CONTROLS
