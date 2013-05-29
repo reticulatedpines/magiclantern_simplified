ff558d4d47771caf2b4e9bdcfa760696898d65cb
Gr3g01 <devnull@localhost>
2013-05-29 20:27:51 +0200

    500D flex info

--- platform/500D.111/misc.c
+++ platform/500D.111/misc.c
@@ -7,40 +7,5 @@
 #include <consts.h>
 #include <lens.h>
 
-void display_shooting_info() // called from debug task
-{
-	if (lv) return;
-	
-	int bg = bmp_getpixel(314, 260);
-	uint32_t fnt = FONT(FONT_MED, COLOR_FG_NONLV, bg);
-
-	if (lens_info.wb_mode == WB_KELVIN)
-	{
-		bmp_printf(fnt, 315, 238, "      \n%5dK\n      ", lens_info.kelvin);
-	}
-	if (lens_info.wbs_gm || lens_info.wbs_ba)
-	{
-		fnt = FONT(FONT_LARGE, COLOR_FG_NONLV, bg);
-
-		int ba = lens_info.wbs_ba;
-		if (ba) bmp_printf(fnt, 402, 238, "%s%d ", ba > 0 ? "A" : "B", ABS(ba));
-		//else bmp_printf(fnt, 402, 238, "   "); // not needed, camera redraws the place itself
-
-		int gm = lens_info.wbs_gm;
-		if (gm) bmp_printf(fnt, 402, 268, "%s%d ", gm > 0 ? "G" : "M", ABS(gm));
-		//else bmp_printf(fnt, 402, 268, "   "); // not needed, camera redraws the place itself
-	}
-
-	iso_refresh_display();
-
-	bg = bmp_getpixel(15, 430);
-	fnt = FONT(FONT_MED, COLOR_FG_NONLV, bg);
-	
-	bmp_printf(fnt, 40, 460, get_mlu() ? "MLU" : "   ");
-
-	display_lcd_remote_icon(480, 0);
-	display_trap_focus_info();
-}
-
 // dummy stub
 int new_LiveViewApp_handler = 0xff123456;
