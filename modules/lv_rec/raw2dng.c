5c599ff290e4ae299262f0128ce6080c4bcbe13b
alex <alex@thinkpad>
2013-05-30 11:10:23 +0300

    raw2dng: fix for vertical stripes/banding in 5D Mark III and maybe others

--- modules/lv_rec/raw2dng.c
+++ modules/lv_rec/raw2dng.c
@@ -11,6 +11,8 @@ struct raw_info raw_info;
 #define FAIL(fmt,...) { fprintf(stderr, "Error: "); fprintf(stderr, fmt, ## __VA_ARGS__); fprintf(stderr, "\n"); exit(1); }
 #define CHECK(ok, fmt,...) { if (!ok) FAIL(fmt, ## __VA_ARGS__); }
 
+static void fix_vertical_stripes();
+
 int main(int argc, char** argv)
 {
     if (argc < 2)
@@ -88,6 +90,7 @@ int main(int argc, char** argv)
         
         char fn[100];
         snprintf(fn, sizeof(fn), "%s%06d.dng", prefix, i);
+        fix_vertical_stripes();
         save_dng(fn);
     }
     fclose(fi);
@@ -114,4 +117,162 @@ int raw_get_pixel(int x, int y) {
     return p->a;
 }
 
+/**
+ * Fix vertical stripes (banding) from 5D Mark III (and maybe others).
+ * 
+ * These stripes are periodic, they repeat every 8 pixels.
+ * It looks like some columns have different luma amplification;
+ * correction factors are somewhere around 0.98 - 1.02, maybe camera-specific, maybe depends on
+ * certain settings, I have no idea. So, this fix compares luma values within one pixel block,
+ * computes the correction factors (using median to reject outliers) and decides
+ * whether to apply the correction or not.
+ * 
+ * For speed reasons:
+ * - Correction factors are computed from the first frame only.
+ * - Only channels with error greater than 0.5% are corrected.
+ */
 
+static int stripes_coeffs[8];
+static int stripes_correction_needed = 0;
+
+#define MIN(a,b) \
+   ({ typeof ((a)+(b)) _a = (a); \
+      typeof ((a)+(b)) _b = (b); \
+     _a < _b ? _a : _b; })
+
+#define MIN_DUMB(a,b) ((a) < (b) ? (a) : (b))
+
+#define MAX(a,b) \
+   ({ typeof ((a)+(b)) _a = (a); \
+       typeof ((a)+(b)) _b = (b); \
+     _a > _b ? _a : _b; })
+
+#define COERCE(x,lo,hi) MAX(MIN((x),(hi)),(lo))
+
+#define PA ((int)(p->a))
+#define PB ((int)(p->b_lo | (p->b_hi << 12)))
+#define PC ((int)(p->c_lo | (p->c_hi << 10)))
+#define PD ((int)(p->d_lo | (p->d_hi << 8)))
+#define PE ((int)(p->e_lo | (p->e_hi << 6)))
+#define PF ((int)(p->f_lo | (p->f_hi << 4)))
+#define PG ((int)(p->g_lo | (p->g_hi << 2)))
+#define PH ((int)(p->h))
+
+#define SET_PA(x) { int v = (x); p->a = v; }
+#define SET_PB(x) { int v = (x); p->b_lo = v; p->b_hi = v >> 12; }
+#define SET_PC(x) { int v = (x); p->c_lo = v; p->c_hi = v >> 10; }
+#define SET_PD(x) { int v = (x); p->d_lo = v; p->d_hi = v >> 8; }
+#define SET_PE(x) { int v = (x); p->e_lo = v; p->e_hi = v >> 6; }
+#define SET_PF(x) { int v = (x); p->f_lo = v; p->f_hi = v >> 4; }
+#define SET_PG(x) { int v = (x); p->g_lo = v; p->g_hi = v >> 2; }
+#define SET_PH(x) { int v = (x); p->h = v; }
+
+#define RAW_MUL(p, x) ((((int)(p) - raw_info.black_level) * (int)(x) / 1024) + raw_info.black_level)
+#define FACTOR(a,b) ({ int A = ((int)(a) - raw_info.black_level); int B = ((int)(b) - raw_info.black_level); B > 4 ? (A * 1024 / B) : 1024; })
+
+#define F2H(x) COERCE(x, 0, 2047)
+#define H2F(x) (x)
+
+static void detect_vertical_stripes_coeffs()
+{
+    /* could be a little more memory efficient if we limit coefficient range to something like 0.8 - 1.2 */
+    static int hist[8][2048];
+    
+    memset(hist, 0, sizeof(hist));
+
+    /* compute 8 little histograms */
+    int n = 0;
+    struct raw_pixblock * row;
+    for (row = raw_info.buffer; (void*)row < (void*)raw_info.buffer + raw_info.frame_size; row += raw_info.pitch / sizeof(struct raw_pixblock))
+    {
+        struct raw_pixblock * p;
+        for (p = row; (void*)p < (void*)row + raw_info.pitch; p++)
+        {
+            n++;
+            hist[0][F2H(FACTOR(1,  1))]++;
+            hist[1][F2H(FACTOR(1,  1))]++;
+            hist[2][F2H(FACTOR(PA, PC))]++;
+            hist[3][F2H(FACTOR(PB, PD))]++;
+            hist[4][F2H(FACTOR(PA, PE))]++;
+            hist[5][F2H(FACTOR(PB, PF))]++;
+            hist[6][F2H(FACTOR(PA, PG))]++;
+            hist[7][F2H(FACTOR(PB, PH))]++;
+        }
+    }
+    
+    /* compute the median correction factor (this will reject outliers) */
+    int j,k;
+    for (j = 0; j < 8; j++)
+    {
+        int t = 0;
+        for (k = 0; k < 2048; k++)
+        {
+            t += hist[j][k];
+            if (t >= n/2)
+            {
+                int c = H2F(k);
+                /* do we really need stripe correction, or it won't be noticeable? or maybe it's just computation error? */
+                if (abs(c - 1024) < 5)
+                {
+                    stripes_coeffs[j] = 0;
+                }
+                else
+                {
+                    stripes_coeffs[j] = c;
+                    stripes_correction_needed = 1;
+                }
+                break;
+            }
+        }
+    }
+    
+    if (stripes_correction_needed)
+    {
+        printf("\nVertical stripes correction:\n");
+        for (j = 0; j < 8; j++)
+        {
+            if (stripes_coeffs[j])
+                printf("  %.3f", (double)stripes_coeffs[j] / 1024.0);
+            else
+                printf("    1  ");
+        }
+        printf("\n");
+    }
+}
+
+static void apply_vertical_stripes_correction()
+{
+    struct raw_pixblock * row;
+    for (row = raw_info.buffer; (void*)row < (void*)raw_info.buffer + raw_info.frame_size; row += raw_info.pitch / sizeof(struct raw_pixblock))
+    {
+        struct raw_pixblock * p;
+        for (p = row; (void*)p < (void*)row + raw_info.pitch; p++)
+        {
+            if (stripes_coeffs[0]) SET_PA(RAW_MUL(PA, stripes_coeffs[0]));
+            if (stripes_coeffs[1]) SET_PB(RAW_MUL(PB, stripes_coeffs[1]));
+            if (stripes_coeffs[2]) SET_PC(RAW_MUL(PC, stripes_coeffs[2]));
+            if (stripes_coeffs[3]) SET_PD(RAW_MUL(PD, stripes_coeffs[3]));
+            if (stripes_coeffs[4]) SET_PE(RAW_MUL(PE, stripes_coeffs[4]));
+            if (stripes_coeffs[5]) SET_PF(RAW_MUL(PF, stripes_coeffs[5]));
+            if (stripes_coeffs[6]) SET_PG(RAW_MUL(PG, stripes_coeffs[6]));
+            if (stripes_coeffs[7]) SET_PH(RAW_MUL(PH, stripes_coeffs[7]));
+        }
+    }
+}
+
+static void fix_vertical_stripes()
+{
+    /* for speed: only detect correction factors from the first frame */
+    static int first_time = 1;
+    if (first_time)
+    {
+        detect_vertical_stripes_coeffs();
+        first_time = 0;
+    }
+    
+    /* only apply stripe correction if we need it, since it takes a little CPU time */
+    if (stripes_correction_needed)
+    {
+        apply_vertical_stripes_correction();
+    }
+}
