0b06f62c92ce6540d0da0f91e5dd06e5897f97d3
GregoryO <go@gregoryo.com>
2013-06-07 11:49:05 -0400

    clean up features for merge

--- platform/50D.109/features.h
+++ platform/50D.109/features.h
@@ -4,12 +4,9 @@
 #define FEATURE_MOVIE_RECORDING_50D
 #define FEATURE_MOVIE_RECORDING_50D_SHUTTER_HACK
 
-// silent pic 
-#define FEATURE_SILENT_PIC_RAW_BURST // compiles and works for single pics burst seems to have issues
-//#define FEATURE_SILENT_PIC_RAW  ---> Set Burst or single not both! single gives undefined reference to `silent_pic_raw_vsync'
-//~ #define FEATURE_LVAE_EXPO_LOCK // unreliable, and we have full manual controls now
 
-#define FEATURE_LV_FOCUS_BOX_SNAP_TO_X5_RAW
+//#define FEATURE_SILENT_PIC_RAW_BURST // compiles and works for single pics burst seems to have issues
+//#define FEATURE_LV_FOCUS_BOX_SNAP_TO_X5_RAW
 
 // no audio at all
 #undef FEATURE_AUDIO_METERS
