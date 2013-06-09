21d7bf07a6d49fcdf5200a0d4ba343bbb4fa9ade
g3gg0 <devnull@localhost>
2013-06-09 02:12:57 +0200

    adtg_log: print nrzi decoded pendant

--- modules/adtg_log/parse_bin.py
+++ modules/adtg_log/parse_bin.py
@@ -4,35 +4,59 @@ import sys,getopt
 
 filename = None
 
+def nrzi_decode( in_val ):
+	val = 0
+	if (in_val & 0x8000):
+		val |= 0x8000
+	for num in range(0,31):
+		old_bit = (val & 1<<(30-num+1)) >> 1
+		val |= old_bit ^ (in_val & 1<<(30-num))
+	return val
+
 opts,args = getopt.getopt(sys.argv[1:],'f:b:')
 for o,a in opts:
-    if o == '-f':
-	filename = a
+	if o == '-f':
+		filename = a
 
 with open(filename,"rb") as f:
-    while 1:
-        dst_data = f.read(4)
-        dat_data = f.read(4)
+	while 1:
+		dst_data = f.read(4)
+		dat_data = f.read(4)
 
-	if len(dst_data) < 4:
-	    break
-	str = ""
-	dst = 0
-	dat = 0
-        for ch in dst_data:
-    	    dst = dst >> 8
-	    dst += ord(ch) << 24
-        for ch in dat_data:
-    	    dat = dat >> 8
-	    dat += ord(ch) << 24
+		if len(dst_data) < 4:
+			break
+		
+		str = ""
+		dst = 0
+		dat = 0
+	
+		for ch in dst_data:
+			dst = dst >> 8
+			dst += ord(ch) << 24
+		for ch in dat_data:
+			dat = dat >> 8
+			dat += ord(ch) << 24
 
-	if dst == 0xFF000000:
-	    str += "[CMOS16]  Reg:    %01X  Data:  %03X  (%4d)" % (dat >> 12, dat & 0x0FFF, dat & 0x0FFF)
-	elif dst == 0x00FF0000:
-	    str += "[CMOS]    Reg:    %01X  Data:  %03X  (%4d)" % (dat >> 12, dat & 0x0FFF, dat & 0x0FFF)
-	elif dst == 0xFFFFFFFF:
-    	    str += "[----------- VSYNC ------------]   "
-	else:
-	    str += "[ADTG%1d]   Reg: %04X  Data: %04X (%5d)" % (dst & 0x0F, dat >> 16, dat & 0xFFFF, dat & 0xFFFF)
-
-	print str
+		if dst == 0xFF000000:
+			reg = dat >> 12
+			val = dat & 0x0FFF
+			str += "[CMOS16]  Reg:    %01X  Data:  %03X  (%4d)  " % (reg, val, dat & 0x0FFF)
+			val = nrzi_decode(dat & 0x0FFF)
+			str += "[CMOS16]  Reg:    %01X  Data:  %03X  (%4d)" % (reg, val, dat & 0x0FFF)
+		elif dst == 0x00FF0000:
+			reg = dat >> 12
+			val = dat & 0x0FFF
+			str += "[CMOS]    Reg:    %01X  Data:  %03X  (%4d)  " % (reg, val, dat & 0x0FFF)
+			val = nrzi_decode(dat & 0x0FFF)
+			str += "[CMOS]    Reg:    %01X  Data:  %03X  (%4d)" % (reg, val, dat & 0x0FFF)
+		elif dst == 0xFFFFFFFF:
+			str += "[----------- VSYNC ------------]   "
+		else:
+			reg = dat >> 16
+			val = dat & 0xFFFF
+			str += "[ADTG%1d]   Reg: %04X  Data: %04X (%5d)  " % (dst & 0x0F, reg, val, val)
+			reg = nrzi_decode(dat >> 16)
+			val = nrzi_decode(dat & 0xFFFF)
+			str += "[ADTG%1d]   Reg: %04X  Data: %04X (%5d)" % (dst & 0x0F, reg, val, val)
+	
+		print str
