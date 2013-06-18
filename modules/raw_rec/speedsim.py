2bfb6d553e5fb790ccc87df4b5d6a4dc40f43224
alex <alex@thinkpad>
2013-06-18 17:38:45 +0300

    speedsim.py: added a mathematical model for speed loss at small buffer sizes

--- modules/raw_rec/speedsim.py
+++ modules/raw_rec/speedsim.py
@@ -7,18 +7,29 @@ import os, sys, math, random
 
 # buffers = [32*1024*1024] * 4                              # 5D3 1x
 # buffers = [32*1024*1024] * 3 + [22 * 1024 * 1024]         # 5D3 zoom
-buffers = [32*1024*1024] * 8 + [23 * 1024 * 1024]         # 60D (wow!)
+# buffers = [32*1024*1024] * 8 + [8 * 1024 * 1024]          # 60D (wow!)
+buffers = [32*1024*1024] * 2 + [8 * 1024 * 1024]            # 550D
 
-x = 1728;
-y = 736;
-fps = 24;
-write_speed = 19.2*1024*1024;
+x = 1152;
+y = 464;
+fps = 23.976;
+write_speed = 20.0*1024*1024;
 timing_variation = 0.01;
 
 framesize = x * y * 14/8;
 
 print "Frame size: %.2f MB" % (framesize / 1024 / 1024)
 print "Write speed: %.1f MB/s; for continuous recording, needs %.1f MB/s. " % (write_speed / 1024 / 1024, framesize * fps / 1024 / 1024)
+
+def speed_model(nominal_speed, buffer_size):
+    # model fitted from benchmarks from Hoodman 1000x.log - http://www.magiclantern.fm/forum/index.php?topic=5471
+    # and confirmed with small tendency of underestimation on 550D SanDisk Extreme 32GB 45MBs.log
+    pfit = [-9.1988e-01, 6.5760e-09, 9.4763e-02, -1.1569e-04]
+    speed_factor = pfit[0] + pfit[1] * buffer_size + pfit[2] * math.log(buffer_size, 2) + pfit[3] * math.sqrt(buffer_size)
+    if speed_factor < 0: speed_factor = 0
+    if speed_factor > 1: speed_factor = 1
+    return nominal_speed * speed_factor
+
 
 buffers = [math.floor(b / framesize) for b in buffers]
 total = sum(buffers)
@@ -41,9 +52,11 @@ while used < total and f < 10000:
             k = (k + 1) % len(buffers)
         else:
             wt = t
-        dt = framesize * buffers[k] / write_speed + random.uniform(-timing_variation, timing_variation);
+        adjusted_speed = speed_model(write_speed, framesize * buffers[k])
+        dt = framesize * buffers[k] / adjusted_speed + random.uniform(-timing_variation, timing_variation);
         wt += dt
-        print "[%.2f] saving chunk, f=%d, dt=%.2f, will finish at %.02f" % (t, f, dt, wt)
+        asmb = adjusted_speed/1024/1024;
+        print "[%.2f] saving chunk, f=%d, s=%.2f, dt=%.2f, will finish at %.02f" % (t, f, asmb, dt, wt)
 
     A.append(used)
     T.append(t)
