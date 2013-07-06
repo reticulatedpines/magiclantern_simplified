7a01ab35a65afa54d9970c4189f29bad0bb40cf4
nanomad <devnull@localhost>
2013-07-06 13:37:22 +0200

    650D: Fix building with CONFIG_PICOC = y. Fixes issue #1566

--- platform/650D.101/gui.h
+++ platform/650D.101/gui.h
@@ -50,6 +50,8 @@
 #define BGMT_PRESS_HALFSHUTTER 0x4e
 #define BGMT_UNPRESS_HALFSHUTTER 0x4f
 
+    #define BGMT_PRESS_FULLSHUTTER 0xdeadbeef // Not found ??
+
 #define GMT_GUICMD_PRESS_BUTTON_SOMETHING 0x52 // unhandled buttons?
 
 #define GMT_OLC_INFO_CHANGED 0x67 // backtrace copyOlcDataToStorage call in gui_massive_event_loop
