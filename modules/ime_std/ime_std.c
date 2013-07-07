b48cdae7e332e68a07ed46d68703fd96e16575a3
g3gg0 <devnull@localhost>
2013-07-07 11:17:52 +0200

    ime_std: added improved filename handling

--- modules/ime_std/ime_std.c
+++ modules/ime_std/ime_std.c
@@ -8,6 +8,7 @@
 #include <bmp.h>
 #include <menu.h>
 #include <config.h>
+#include <string.h>
 
 #include "../ime_base/ime_base.h"
 
@@ -17,9 +18,7 @@
 
 extern int menu_redraw_blocked;
 
-unsigned char *ime_std_string = NULL;
-unsigned char *ime_std_caption = NULL;
-
+/* appearance options */
 int ime_std_char_x = 30;
 int ime_std_char_y = 200;
 int ime_std_char_w = 650;
@@ -32,19 +31,12 @@ int ime_std_caption_y = 20;
 
 int ime_std_text_fg = COLOR_WHITE;
 int ime_std_text_bg = COLOR_BLACK;    
+unsigned int ime_std_font = FONT_LARGE;
 
-
-/* currently selected character */
-unsigned int ime_std_selection = 0;
-/* which string position to change */
-unsigned int ime_std_caret_pos = 0;
-
-unsigned int ime_std_max_length = 0;
-unsigned int ime_std_active = 0;
-unsigned int ime_std_valid = 0;
-unsigned int ime_std_current_sel = 0;
-unsigned int ime_std_charset_type = 0;
-
+/* thats the charset that is used for various use cases */
+#define IME_STD_VAR_CHARSET           3
+#define IME_STD_VAR_CHARSET_DEF       ime_std_charset_punctuation
+#define IME_STD_VAR_CHARSET_DEF_TYPE  IME_CHARSET_PUNCTUATION
 
 /* should that be located in files? */
 unsigned char ime_std_charset_alpha_upper[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
@@ -52,27 +44,54 @@ unsigned char ime_std_charset_alpha_lower[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g'
 unsigned char ime_std_charset_numeric[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 unsigned char ime_std_charset_punctuation[] = {'.', ',', ':', ';', '!', '?', '`', '\'', '\"'};
 unsigned char ime_std_charset_math[] = { '+', '-', '*', '/', '^' };
+unsigned char ime_std_charset_file[] = { '.', ' ', '-', '_' };
 unsigned char ime_std_charset_mail[] = { '@', '.', '-', '_', '!', '#', '$', '%', '&', '\'', '*', '+', '-', '/', '=', '?', '^', '_', '`', '{', '|', '}' };
 unsigned char ime_std_charset_func[] = { CHAR_DEL, CHAR_OK, CHAR_CANCEL };
 
-/* number of characters in current set, currently set 0 */
-unsigned int ime_std_charsetnum = 0;
-unsigned int ime_std_charcount = sizeof(ime_std_charset_alpha_upper);
-unsigned char *ime_std_charset = ime_std_charset_alpha_upper;
-
 /* number of characters in specific sets */
-unsigned int ime_std_charset_types[] = { IME_CHARSET_ALPHA | IME_CHARSET_FILENAME, IME_CHARSET_ALPHA, IME_CHARSET_NUMERIC, IME_CHARSET_PUNCTUATION, IME_CHARSET_MATH, IME_CHARSET_MAIL, IME_CHARSET_ANY };
+unsigned int ime_std_charset_types[] = { IME_CHARSET_ALPHA | IME_CHARSET_FILENAME, IME_CHARSET_ALPHA, IME_CHARSET_NUMERIC | IME_CHARSET_FILENAME, IME_CHARSET_PUNCTUATION, IME_CHARSET_MATH, IME_CHARSET_MAIL, IME_CHARSET_ANY };
 unsigned char *ime_std_charsets[] = { ime_std_charset_alpha_upper, ime_std_charset_alpha_lower, ime_std_charset_numeric, ime_std_charset_punctuation, ime_std_charset_math, ime_std_charset_mail, ime_std_charset_func };
 int ime_std_charcounts[] = { sizeof(ime_std_charset_alpha_upper), sizeof(ime_std_charset_alpha_lower), sizeof(ime_std_charset_numeric), sizeof(ime_std_charset_punctuation), sizeof(ime_std_charset_math), sizeof(ime_std_charset_mail), sizeof(ime_std_charset_func) };
-unsigned char *ime_std_charsetnames[] = { (unsigned char *)"upper", (unsigned char *)"lower", (unsigned char *)"num", (unsigned char *)"punct", (unsigned char *)"math", (unsigned char *)"mail" };
+unsigned char *ime_std_charsetnames[] = { (unsigned char *)"upper", (unsigned char *)"lower", (unsigned char *)"num", (unsigned char *)"punct", (unsigned char *)"math", (unsigned char *)"var" };
 
-unsigned int ime_std_font = FONT_LARGE;
-  
-unsigned int ime_std_ret = IME_ERR_UNKNOWN;
-t_ime_update_cbr ime_std_update_cbr = NULL;
-t_ime_done_cbr ime_std_done_cbr = NULL;
 
-static void ime_std_draw_charset(unsigned int charset, unsigned int font, int charnum, int selected, int x, int y, int w, int h)
+typedef struct
+{
+    unsigned int active;
+    unsigned char *string;
+    unsigned int max_length;
+    unsigned int selection;
+    unsigned char *caption;
+    t_ime_update_cbr update_cbr;
+    t_ime_done_cbr done_cbr;
+    unsigned int caret_pos;
+    unsigned int valid;
+    
+    /* infos about current selected charset */
+    unsigned int charset_type;
+    unsigned char *charset;
+    unsigned int charset_charcount;
+    unsigned int charsetnum;
+    unsigned int returncode;
+} ime_std_ctx_t;
+
+ime_std_ctx_t *ime_std_current_ctx = NULL;
+
+
+static void ime_std_update(ime_std_ctx_t *ctx)
+{
+    /* check update function return code */
+    if(ctx->update_cbr)
+    {
+        ctx->valid = (ctx->update_cbr(ctx, ctx->string, ctx->caret_pos, 0) == IME_OK);
+    }
+    else
+    {
+        ctx->valid = 1;
+    }
+}
+
+static void ime_std_draw_charset(ime_std_ctx_t *ctx, unsigned int charset, unsigned int font, int charnum, int selected, int x, int y, int w, int h)
 {
     /* total border width */
     int border = 2;
@@ -146,12 +165,12 @@ static void ime_std_draw_charset(unsigned int charset, unsigned int font, int ch
     }
 }
 
-static void ime_std_draw()
+static void ime_std_draw(ime_std_ctx_t *ctx)
 {
     int color_bg = COLOR_GRAY(10);
     
     /* if text isnt valid, print red */
-    if(ime_std_valid)
+    if(ctx->valid)
     {
         ime_std_text_fg = COLOR_WHITE;
     }
@@ -177,9 +196,9 @@ static void ime_std_draw()
         }
         
         /* print title text */
-        if(ime_std_caption)
+        if(ctx->caption)
         {
-            bfnt_printf(ime_std_caption_x, ime_std_caption_y, COLOR_WHITE, color_bg, "%s", ime_std_caption);
+            bfnt_printf(ime_std_caption_x, ime_std_caption_y, COLOR_WHITE, color_bg, "%s", ctx->caption);
             draw_line(ime_std_caption_x, ime_std_caption_y + 40, ime_std_str_w + ime_std_str_x, ime_std_caption_y + 40, COLOR_ORANGE);
             draw_line(ime_std_caption_x + 5, ime_std_caption_y + 40 + 3, ime_std_str_w + ime_std_str_x + 5, ime_std_caption_y + 40 + 3, COLOR_ORANGE);
         }
@@ -191,24 +210,24 @@ static void ime_std_draw()
         bmp_draw_rect(COLOR_ORANGE, ime_std_str_x, ime_std_str_y, ime_std_str_w, font_large.height + 6);
         
         /* now the text and right after the caret */
-        bmp_printf(FONT(FONT_LARGE,ime_std_text_fg, ime_std_text_bg), ime_std_str_x + 3, ime_std_str_y + 3, "%s", ime_std_string);
-        bmp_printf(SHADOW_FONT(FONT(FONT_LARGE,COLOR_BLACK, COLOR_ORANGE)), ime_std_str_x + 3 + font_large.width * ime_std_caret_pos, ime_std_str_y + 3, "_");
+        bmp_printf(FONT(FONT_LARGE,ime_std_text_fg, ime_std_text_bg), ime_std_str_x + 3, ime_std_str_y + 3, "%s", ctx->string);
+        bmp_printf(SHADOW_FONT(FONT(FONT_LARGE,COLOR_BLACK, COLOR_ORANGE)), ime_std_str_x + 3 + font_large.width * ctx->caret_pos, ime_std_str_y + 3, "_");
 
         /* orange rectangle around that dark characters box background */
         bmp_fill(COLOR_GRAY(20), ime_std_char_x - 1, ime_std_char_y - 1, ime_std_char_w + 2, fontspec_font(ime_std_font)->height * COUNT(ime_std_charcounts) + 4);
         bmp_draw_rect(COLOR_ORANGE, ime_std_char_x - 1, ime_std_char_y - 1, ime_std_char_w + 2, fontspec_font(ime_std_font)->height * COUNT(ime_std_charcounts) + 4);
         
         /* print charsets that are selected */
-        for(int set = 0; set < COUNT(ime_std_charcounts); set++)
+        for(unsigned int set = 0; set < COUNT(ime_std_charcounts); set++)
         {
             int offset_y = fontspec_font(ime_std_font)->height * set;
-            if(ime_std_charset_types[set] & ime_std_charset_type)
+            if(ime_std_charset_types[set] & ctx->charset_type)
             {
-                ime_std_draw_charset(set, ime_std_font, ime_std_selection, set == ime_std_charsetnum, ime_std_char_x + 2, ime_std_char_y + offset_y, ime_std_char_w - 1, ime_std_char_h - offset_y);
+                ime_std_draw_charset(ctx, set, ime_std_font, ctx->selection, set == ctx->charsetnum, ime_std_char_x + 2, ime_std_char_y + offset_y, ime_std_char_w - 1, ime_std_char_h - offset_y);
             }
             else
             {
-                ime_std_draw_charset(set, ime_std_font, ime_std_selection, 2, ime_std_char_x + 2, ime_std_char_y + offset_y, ime_std_char_w - 1, ime_std_char_h - offset_y);
+                ime_std_draw_charset(ctx, set, ime_std_font, ctx->selection, 2, ime_std_char_x + 2, ime_std_char_y + offset_y, ime_std_char_w - 1, ime_std_char_h - offset_y);
             }
         }
         
@@ -217,17 +236,18 @@ static void ime_std_draw()
     )
 }
 
-static int ime_std_select_charset(int charset)
+static int ime_std_select_charset(ime_std_ctx_t *ctx, int charset)
 {
-    if(charset < COUNT(ime_std_charcounts) && (ime_std_charset_types[charset] & ime_std_charset_type))
+    /* only select charset if it matches the charset type patter specified by dialog creator */
+    if(charset < COUNT(ime_std_charcounts) && (ime_std_charset_types[charset] & ctx->charset_type))
     {
-        ime_std_charset = ime_std_charsets[charset];
-        ime_std_charcount = ime_std_charcounts[charset];
+        ctx->charset = ime_std_charsets[charset];
+        ctx->charset_charcount = ime_std_charcounts[charset];
         
         /* make sure that the currently selected character position is available */
-        if(ime_std_selection >= ime_std_charcount)
+        if(ctx->selection >= ctx->charset_charcount)
         {
-            ime_std_selection = ime_std_charcount - 1;
+            ctx->selection = ctx->charset_charcount - 1;
         }
         
         return 1;
@@ -238,52 +258,54 @@ static int ime_std_select_charset(int charset)
 
 static unsigned int ime_std_keypress_cbr(unsigned int key)
 {
-    if (!ime_std_active)
+    ime_std_ctx_t *ctx = ime_std_current_ctx;
+    
+    if (!ctx || !ctx->active)
         return 1;
     
     switch (key)
     {
         case MODULE_KEY_WHEEL_UP:
-            if(ime_std_caret_pos > 0)
+            if(ctx->caret_pos > 0)
             {
-                ime_std_caret_pos--;
+                ctx->caret_pos--;
             }
             break;
             
         case MODULE_KEY_WHEEL_DOWN:
-            if(ime_std_caret_pos < strlen(ime_std_string))
+            if(ctx->caret_pos < strlen((const char*)ctx->string))
             {
-                ime_std_caret_pos++;
+                ctx->caret_pos++;
             }
             break;
             
         case MODULE_KEY_WHEEL_LEFT:
         case MODULE_KEY_PRESS_LEFT:
-            if(ime_std_selection > 0)
+            if(ctx->selection > 0)
             {
-                ime_std_selection--;
+                ctx->selection--;
             }
             break;
             
         case MODULE_KEY_WHEEL_RIGHT:
         case MODULE_KEY_PRESS_RIGHT:
-            if(ime_std_selection < ime_std_charcount - 1)
+            if(ctx->selection < ctx->charset_charcount - 1)
             {
-                ime_std_selection++;
+                ctx->selection++;
             }
             break;
             
         case MODULE_KEY_PRESS_DOWN:
-            if(ime_std_charsetnum < COUNT(ime_std_charcounts))
+            if(ctx->charsetnum < COUNT(ime_std_charcounts))
             {
-                int set = ime_std_charsetnum + 1;
+                int set = ctx->charsetnum + 1;
                 
                 while(set < COUNT(ime_std_charcounts))
                 {
                     /* check the next possible charset */
-                    if(ime_std_select_charset(set))
+                    if(ime_std_select_charset(ctx, set))
                     {
-                        ime_std_charsetnum = set;
+                        ctx->charsetnum = set;
                         break;
                     }
                     
@@ -293,16 +315,16 @@ static unsigned int ime_std_keypress_cbr(unsigned int key)
             break;
             
         case MODULE_KEY_PRESS_UP:
-            if(ime_std_charsetnum > 0)
+            if(ctx->charsetnum > 0)
             {
-                int set = ime_std_charsetnum - 1;
+                int set = ctx->charsetnum - 1;
                 
                 while(set >= 0)
                 {
                     /* check the next possible charset */
-                    if(ime_std_select_charset(set))
+                    if(ime_std_select_charset(ctx, set))
                     {
-                        ime_std_charsetnum = set;
+                        ctx->charsetnum = set;
                         break;
                     }
                     set--;
@@ -312,34 +334,34 @@ static unsigned int ime_std_keypress_cbr(unsigned int key)
             
         case MODULE_KEY_JOY_CENTER:
         case MODULE_KEY_PRESS_SET:
-            if(ime_std_selection < ime_std_charcount)
+            if(ctx->selection < ctx->charset_charcount)
             {
-                unsigned char selected_char = ime_std_charset[ime_std_selection];
+                unsigned char selected_char = ctx->charset[ctx->selection];
                 
                 if(selected_char == CHAR_DEL)
                 {
                     /* backspace/del was pressed */
-                    //ime_std_caret_pos--;
-                    strncpy(&ime_std_string[ime_std_caret_pos], &ime_std_string[ime_std_caret_pos+1], ime_std_max_length - ime_std_caret_pos);
+                    //ctx->caret_pos--;
+                    strncpy((char*)&ctx->string[ctx->caret_pos], (char*)&ctx->string[ctx->caret_pos+1], ctx->max_length - ctx->caret_pos);
                 }
                 else if(selected_char == CHAR_OK)
                 {
                     /* ok was pressed, set return code and return */
-                    ime_std_ret = IME_OK;
-                    ime_std_active = 0;
+                    ctx->returncode = IME_OK;
+                    ctx->active = 0;
                 }
                 else if(selected_char == CHAR_CANCEL)
                 {
                     /* cancel was pressed, set return code and return */
-                    ime_std_ret = IME_CANCEL;
-                    ime_std_active = 0;
+                    ctx->returncode = IME_CANCEL;
+                    ctx->active = 0;
                 }
                 else
                 {
-                    if(ime_std_caret_pos < ime_std_max_length)
+                    if(ctx->caret_pos < ctx->max_length)
                     {
-                        ime_std_string[ime_std_caret_pos] = selected_char;
-                        ime_std_caret_pos++;
+                        ctx->string[ctx->caret_pos] = selected_char;
+                        ctx->caret_pos++;
                     }
                 }
             }
@@ -349,17 +371,9 @@ static unsigned int ime_std_keypress_cbr(unsigned int key)
             return 0;
     }
     
-    /* check update function return code */
-    if(ime_std_update_cbr)
-    {
-        ime_std_valid = (ime_std_update_cbr(ime_std_string, ime_std_caret_pos, 0) == IME_OK);
-    }
-    else
-    {
-        ime_std_valid = 1;
-    }
+    ime_std_update(ctx);
     
-    ime_std_draw();
+    ime_std_draw(ctx);
     return 0;
 }
 
@@ -367,64 +381,136 @@ static void ime_std_config()
 {
 }
 
-static void ime_std_input(unsigned int unused)
+
+IME_UPDATE_FUNC(ime_std_update_cbr_file)
 {
+    if(strlen((char*)text) > 12)
+    {
+        return IME_ERR_UNKNOWN;
+    }
+    
+    int dots = 0;
+    for(int pos = 0; pos < strlen((char*)text); pos++)
+    {
+        if(text[pos] == '.')
+        {
+            dots++;
+        }
+    }
+    
+    if(dots != 1)
+    {
+        return IME_ERR_UNKNOWN;
+    }
+
+    char *dot_pos = strchr((char*)text, '.');
+    if(strlen(dot_pos + 1) > 3)
+    {
+        return IME_ERR_UNKNOWN;
+    }
+    
+    if((int)dot_pos - (int)text > 8)
+    {
+        return IME_ERR_UNKNOWN;
+    }
+    
+    return IME_OK;
+}
+
+
+static void ime_std_input(unsigned int parm)
+{
+    ime_std_ctx_t *ctx = (ime_std_ctx_t *)parm;
+    
     /* stop menu painting */
     menu_redraw_blocked = 1;
     
-    /* redraw periodically */
-    while(ime_std_active)
+    /* select appropriate punctuation for filenames */
+    if(ctx->charset_type & IME_CHARSET_FILENAME)
     {
-        ime_std_draw();
+        ctx->max_length = 12;
+        ime_std_charsets[IME_STD_VAR_CHARSET] = ime_std_charset_file;
+        ime_std_charset_types[IME_STD_VAR_CHARSET] = IME_CHARSET_FILENAME;
+        ime_std_charcounts[IME_STD_VAR_CHARSET] = sizeof(ime_std_charset_file);
+        if(!ctx->update_cbr)
+        {
+            ctx->update_cbr = &ime_std_update_cbr_file;
+        }
+    }
+    else
+    {
+        ime_std_charsets[IME_STD_VAR_CHARSET] = IME_STD_VAR_CHARSET_DEF;
+        ime_std_charset_types[IME_STD_VAR_CHARSET] = IME_STD_VAR_CHARSET_DEF_TYPE;
+        ime_std_charcounts[IME_STD_VAR_CHARSET] = sizeof(IME_STD_VAR_CHARSET_DEF);
+    }
+        
+    /* redraw periodically */
+    while(ctx->active)
+    {
+        ime_std_update(ctx);
+        ime_std_draw(ctx);
         msleep(250);
     }
     
     /* re-enable menu painting */
     menu_redraw_blocked = 0;
-    ime_std_done_cbr(ime_std_ret, ime_std_string);
+    ctx->done_cbr(ctx, ctx->returncode, ctx->string);
+    
+    free(ctx);
 }
 
-static unsigned int ime_std_start(char *caption, char *text, int max_length, int codepage, int charset, t_ime_update_cbr update_cbr, t_ime_done_cbr done_cbr, int x, int y, int w, int h)
+static void *ime_std_start(unsigned char *caption, unsigned char *text, int max_length, int codepage, int charset, t_ime_update_cbr update_cbr, t_ime_done_cbr done_cbr, int x, int y, int w, int h)
 {
-    ime_std_string = text;
-    ime_std_caption = caption;
-    ime_std_caret_pos = strlen(text);
-    ime_std_update_cbr = update_cbr;
-    ime_std_done_cbr = done_cbr;
-    ime_std_active = 1;
-    ime_std_valid = 1;
-    ime_std_max_length = max_length;
-    ime_std_charset_type = charset;
+    ime_std_ctx_t *ctx = malloc(sizeof(ime_std_ctx_t));
     
-    for(int pos = strlen(ime_std_string); pos < max_length; pos++)
+    /* set parameters */
+    ctx->string = text;
+    ctx->caption = caption;
+    ctx->caret_pos = strlen((char*)text);
+    ctx->max_length = max_length;
+    ctx->charset_type = charset;
+
+    /* store callback routines */
+    ctx->update_cbr = update_cbr;
+    ctx->done_cbr = done_cbr;
+    
+    ctx->selection = 0;
+    ctx->active = 1;
+    ctx->valid = 1;
+    ctx->returncode = IME_CANCEL;
+
+    
+    /* fill remaining space with zeros just to make sure */
+    for(int pos = strlen((char*)ctx->string); pos < max_length; pos++)
     {
-        ime_std_string[pos] = '\000';
+        ctx->string[pos] = '\000';
     }
     
     /* select first charset */
-    ime_std_charsetnum = 0;    
+    ctx->charsetnum = 0;    
 
-    while(ime_std_charsetnum < COUNT(ime_std_charcounts))
+    while(ctx->charsetnum < COUNT(ime_std_charcounts))
     {
         /* check the next possible charset */
-        if(ime_std_select_charset(ime_std_charsetnum))
+        if(ime_std_select_charset(ctx, ctx->charsetnum))
         {
             break;
         }
         
-        ime_std_charsetnum++;
+        ctx->charsetnum++;
     }
     
     /* no valid charset found */
-    if(ime_std_charsetnum >= COUNT(ime_std_charcounts))
+    if(ctx->charsetnum >= COUNT(ime_std_charcounts))
     {
-        return IME_ERR_UNAVAIL;
+        return NULL;
     }
     
-    /* start input system */
-    task_create("ime_std_input", 0x1c, 0x1000, ime_std_input, (void*)0);
+    /* start input system - ToDo: add a lock to make sure only one thread starts the IME */
+    ime_std_current_ctx = ctx;
+    task_create("ime_std_input", 0x1c, 0x1000, ime_std_input, ctx);
     
-    return IME_OK;
+    return ctx;
 }
 
 
