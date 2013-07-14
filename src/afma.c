8440d67e9998308c4ad986334dc908703bea67d4
YMP <nospam@boteanu.com>
2013-07-14 04:48:35 +0000

    Added new scan mode that detects the AFMA range and uses high precision even if adjustment is outside the -20..+20 range. Configurable number of passes.

--- src/afma.c
+++ src/afma.c
@@ -111,8 +111,12 @@ void set_afma_mode(int mode)
  * Dot-Tune AFMA
  */
 
+#define AFMA_SCAN_PASSES_MAX 10
 static int afma_mode = AFMA_MODE_AUTODETECT;
 static int afma_mode_index = 0;
+static int afma_scan_range_index = 0;
+static int afma_scan_passes = 4;
+static int afma0;
 
 static void afma_print_status(int8_t* score, int range_expand_factor)
 {
@@ -145,6 +149,46 @@ static void afma_print_status(int8_t* score, int range_expand_factor)
     bmp_printf(SHADOW_FONT(FONT_SMALL), xc + 8 - strlen(msg)*font_small.width/2, 4, msg);
 }
 
+static void afma_print_status_extended(int8_t* score, int range_display_min, int range_display_max)
+{
+    int max = 4;
+    for (int i = -AFMA_MAX; i <= AFMA_MAX; i++)
+        max = MAX(max, score[i+AFMA_MAX]);
+
+    //fill 0,0 -> 720,20 with color 45
+    bmp_fill(45, 0, 0, 720, 20);
+
+    char msg[5];
+    snprintf(msg, sizeof(msg), "%d", range_display_min);
+    bmp_printf(SHADOW_FONT(FONT_SMALL), 3, 4, msg);
+    snprintf(msg, sizeof(msg), "+%d", range_display_max);
+    bmp_printf(SHADOW_FONT(FONT_SMALL), 695, 4, msg);
+
+    int start_x = 33;
+    int end_x = 689;
+    int nb_cells = range_display_max - range_display_min +1 ;
+    int cell_size =    (end_x - start_x) / nb_cells;
+    
+    //center the histogram 
+    int delta = (end_x - (cell_size * nb_cells) - start_x) / 2;
+    start_x += delta;
+
+    for (int i = range_display_min; i <= range_display_max; i++)
+    {
+        int x = start_x + (i-range_display_min)*cell_size;
+        int s = score[i+AFMA_MAX];
+        int h = s * 16 / max;
+        if (s) h = MAX(h, 2);
+        bmp_fill(COLOR_BLACK, x, 2, cell_size-2, 16-h);
+        bmp_fill(COLOR_WHITE, x, 2 + 16-h, cell_size-2, h);
+    }
+    
+    int afma = get_afma(afma_mode);
+    int xc = start_x + (COERCE(afma, range_display_min, range_display_max)-range_display_min) * cell_size;
+    bmp_fill(COLOR_RED, xc, 2, cell_size-2, 16);
+    snprintf(msg, sizeof(msg), "%d", afma);
+    bmp_printf(SHADOW_FONT(FONT_SMALL), xc + 8 - strlen(msg)*font_small.width/2, 4, msg);
+}
 
 static int wait_for_focus_confirmation_val(int maxTimeToWaitMs, int valToWaitFor)
 {
@@ -161,38 +205,243 @@ static int wait_for_focus_confirmation_val(int maxTimeToWaitMs, int valToWaitFor
     }
 }
 
-static void afma_auto_tune(int range_expand_factor)
+static void afma_auto_tune_automatic()
 {
-    if (AFMA_MODE == AFMA_MODE_DISABLED) return;
-    
-    int afma0 = get_afma(afma_mode);
-    
-    msleep(1000);
+    int8_t score[AFMA_MAX*2 +1];
+    int8_t scanned[AFMA_MAX*2 +1];
 
-    if (is_movie_mode()) { NotifyBox(5000, "Switch to photo mode and try again."); beep(); return; };
-    
-    if (lv) { fake_simple_button(BGMT_LV); msleep(1000); }
-    
-    for (int i = 0; i < 5; i++)
+    for (int i = -AFMA_MAX; i <= AFMA_MAX; i++) 
     {
-        if (!DISPLAY_IS_ON || !display_idle())
+            score[i+AFMA_MAX] = 0;
+            scanned[i+AFMA_MAX] = 0;
+    }
+    int range_display_min = -20;
+    int range_display_max = 20;
+
+    afma_print_status_extended(score, range_display_min, range_display_max);
+    msleep(1000);
+    
+    set_afma(0, afma_mode);
+    assign_af_button_to_halfshutter();
+    msleep(100);
+
+    SW1(1,100);
+
+    // Phase 1: rapid edge detection
+    int found_focus=0;
+    int found_focus_min_value=0;
+    int found_focus_max_value=0;
+    int found_min_edge=0;
+    int found_max_edge=0;
+    int scanned_min=0;
+    int scanned_max=0;
+    int last_scanned_direction = 0;
+    int skipped=0;
+
+    while (!found_min_edge || !found_max_edge)
+    {
+        int candidate;
+        if (last_scanned_direction == 0)
         {
-            fake_simple_button(BGMT_INFO);
-            msleep(500);
+            candidate = 0;
+            last_scanned_direction = 1;
+            scanned_min = candidate;
+            scanned_max = candidate;
         }
+        else
+        {            
+            if (found_max_edge)
+            {
+                candidate = scanned_min - 1;
+            }
+            else if (found_min_edge)
+            {
+                candidate = scanned_max + 1;
+            }
+            else
+            {
+                int step =     found_focus ? 1 : 5;
+                last_scanned_direction *= -1;
+                if (last_scanned_direction == 1)
+                {
+                    candidate = scanned_max + step;
+                }
+                else
+                {
+                    candidate = scanned_min - step;
+                }
+            }
+        }
+
+        // scan candidate
+        if (candidate < -AFMA_MAX ||
+            candidate > AFMA_MAX) 
+        {
+            if (skipped)
+            {
+                break;
+            }
+            else
+            {
+                skipped++;
+                continue;
+            }
+        };
+
+        skipped=0;
+        scanned_min = MIN(scanned_min, candidate);
+        scanned_max = MAX(scanned_max, candidate);
+
+        set_afma(candidate, afma_mode);
+        msleep(100);
+            
+        int fc;
+        // initial focus must occur within 200ms
+        fc = wait_for_focus_confirmation_val(200, 1);
+           
+        if (fc)
+        {
+            // weak or strong confirmation? use a higher score if strong
+            // focus must sustain for 500ms to be considered strong
+            if (!wait_for_focus_confirmation_val(500, 0))
+            {
+                // focus sustained
+                fc = 3;
+            }
+        }  
+
+        if (fc)
+        {
+            if (found_focus)
+            {
+                found_focus_min_value = MIN(found_focus_min_value, candidate);
+                found_focus_max_value = MAX(found_focus_max_value, candidate);
+            }
+            else
+            {
+                found_focus = 1;
+                found_focus_min_value = candidate;
+                found_focus_max_value = candidate;
+            }
+        }
+        else
+        {
+            if (found_focus &&
+                candidate <= found_focus_min_value - 5)
+            {
+                found_min_edge = 1;
+            }
+
+            if (found_focus &&
+                candidate >= found_focus_max_value + 5)
+            {
+                found_max_edge = 1;
+            }
+        }
+
+        scanned[candidate+AFMA_MAX]++;
+        score[candidate+AFMA_MAX] += fc;
+        range_display_min = MIN(range_display_min, candidate);
+        range_display_max = MAX(range_display_max, candidate);
+        afma_print_status_extended(score, range_display_min, range_display_max);
+
+        if (!HALFSHUTTER_PRESSED)
+        {
+            beep();
+            set_afma(afma0, afma_mode);
+            NotifyBox(2000, "Canceled by user.");
+            return;
+        }
+
+    }
+    
+    if (!found_focus)
+    {
+        NotifyBox(5000, "OOF, check focus and contrast.");
+        set_afma(afma0, afma_mode);
+        return 0;
     }
 
-    if (lv) { NotifyBox(5000, "Turn off LiveView and try again."); beep(); return; }
-    if (!DISPLAY_IS_ON || !display_idle()) { NotifyBox(5000, "Press " INFO_BTN_NAME " and try again."); beep(); return; }
-    if (!is_manual_focus()) { NotifyBox(5000, "Switch to MF and try again."); beep(); return; }
-    if (!lens_info.name[0]) { NotifyBox(5000, "Attach a chipped lens and try again."); beep(); return; }
-    NotifyBox(5000, "You should have perfect focus.");
-    msleep(2000);
-    NotifyBox(5000, "Leave the camera still...");
-    msleep(2000);
-    NotifyBoxHide();
-    msleep(100);
+    // Phase 2: scan range edge to edge
+    for (int pass = 1 ; pass <= afma_scan_passes; pass++)
+    {
+
+        for (int i = MAX(-AFMA_MAX,found_focus_min_value - 10) ; i <= MIN(AFMA_MAX,found_focus_max_value + 10) ; i++) 
+        {
+
+            // skip values that were already scaned during Phase 1
+            if (pass == 1 &&
+                scanned[i+AFMA_MAX] > 0)
+                continue; 
+
+            scanned[i+AFMA_MAX]++;
+            range_display_min = MIN(range_display_min, i);
+            range_display_max = MAX(range_display_max, i);
+            
+            set_afma(i, afma_mode);
+            msleep(100);
+            
+            int fc;
+            // initial focus must occur within 200ms
+            fc = wait_for_focus_confirmation_val(200, 1);
+           
+            if (fc)
+            {
+                // weak or strong confirmation? use a higher score if strong
+                // focus must sustain for 500ms to be considered strong
+                if (!wait_for_focus_confirmation_val(500, 0))
+                    // focus sustained
+                    fc = 3;
+            }  
+            
+            score[i+AFMA_MAX] += fc;
+            afma_print_status_extended(score, range_display_min, range_display_max);
+            
+            if (!HALFSHUTTER_PRESSED)
+            {
+                beep();
+                set_afma(afma0, afma_mode);
+                NotifyBox(2000, "Canceled by user.");
+                return;
+            }
+        }
+
+    }
+
+    SW1(0,100);
     
+    beep();
+
+    restore_af_button_assignment();
+
+    int s = 0;
+    int w = 0;
+    for (int i = -AFMA_MAX; i <= AFMA_MAX; i++)
+    {
+        // values confirmed multiple times, and/or strongly confirmed, are weighted a lot more
+        int wi = score[i+AFMA_MAX];
+        s += (wi * i);
+        w += wi;
+    }
+    if (w < afma_scan_passes*3)
+    {
+        NotifyBox(5000, "OOF, check focus and contrast.");
+        set_afma(afma0, afma_mode);
+    }
+    else
+    {
+        int afma = s / w;
+        NotifyBox(10000, "New AFMA: %d", afma);
+        set_afma(afma, afma_mode);
+        msleep(300);
+        afma_print_status_extended(score, range_display_min, range_display_max);
+        //~ call("dispcheck"); // screenshot
+        NotifyBox(5000, "Double-check the focus target.");
+    }
+}
+
+static void afma_auto_tune_linear(int range_expand_factor)
+{
     int8_t score[41];
     for (int i = -20; i <= 20; i++) score[i+20] = 0;
     afma_print_status(score, range_expand_factor);
@@ -204,10 +453,11 @@ static void afma_auto_tune(int range_expand_factor)
 
     SW1(1,100);
 
-    for (int k = 0; k < 2; k++)
+    for (int k = 0; k <= (afma_scan_passes-1)/2; k++)
     {
         for (int c = 0; c <= 80; c++) // -20 ... 20 and back to -20
         {
+            if (c>40 && (k*2+1 >= afma_scan_passes)) break;
             int i = (c <= 40) ? c-20 : 60-c;
             set_afma(i * range_expand_factor, afma_mode);
             msleep(100);
@@ -278,11 +528,48 @@ static void afma_auto_tune(int range_expand_factor)
                 NotifyBox(5000, "Double-check the focus target.");
         }
     }
-}
+}    
 
-static void afma_auto_tune_std() { afma_auto_tune(1); }   // -20...20
-static void afma_auto_tune_ext() { afma_auto_tune(2); }   // -40...40
-static void afma_auto_tune_ultra() { afma_auto_tune(5); } // -100...100
+static void afma_auto_tune()
+{
+    if (AFMA_MODE == AFMA_MODE_DISABLED) return;
+    
+    afma0 = get_afma(afma_mode);
+    
+    msleep(1000);
+
+    if (is_movie_mode()) { NotifyBox(5000, "Switch to photo mode and try again."); beep(); return; };
+    
+    if (lv) { fake_simple_button(BGMT_LV); msleep(1000); }
+    
+    for (int i = 0; i < 5; i++)
+    {
+        if (!DISPLAY_IS_ON || !display_idle())
+        {
+            fake_simple_button(BGMT_INFO);
+            msleep(500);
+        }
+    }
+
+    if (lv) { NotifyBox(5000, "Turn off LiveView and try again."); beep(); return; }
+    if (!DISPLAY_IS_ON || !display_idle()) { NotifyBox(5000, "Press " INFO_BTN_NAME " and try again."); beep(); return; }
+    if (!is_manual_focus()) { NotifyBox(5000, "Switch to MF and try again."); beep(); return; }
+    if (!lens_info.name[0]) { NotifyBox(5000, "Attach a chipped lens and try again."); beep(); return; }
+    NotifyBox(5000, "You should have perfect focus.");
+    msleep(2000);
+    NotifyBox(5000, "Leave the camera still...");
+    msleep(2000);
+    NotifyBoxHide();
+    msleep(100);
+    
+    if (afma_scan_range_index == 0)    { afma_auto_tune_automatic(); }
+    else switch( afma_scan_range_index )
+    {
+        case 1:    afma_auto_tune_linear(1); // -20 .. +20
+        case 2:    afma_auto_tune_linear(2); // -40 .. +40
+        case 3:    afma_auto_tune_linear(5); // -100 .. +100
+    }
+}
 
 // for toggling
 static int afma_mode_to_index(int mode)
@@ -335,6 +622,39 @@ static MENU_UPDATE_FUNC(afma_generic_update)
 {
     if (!afma_mode)
         MENU_SET_WARNING(MENU_WARN_NOT_WORKING, "AFMA is disabled.");
+}
+
+static MENU_SELECT_FUNC(afma_scan_range_toggle)
+{
+    #ifdef CONFIG_AFMA_EXTENDED
+    int afma_index_max = 3;
+    #else
+    int afma_index_max = 1;
+    #endif
+    
+    if (!lens_info.name[0])
+        return;
+
+    menu_numeric_toggle(&afma_scan_range_index, delta, 0, afma_index_max);
+}
+
+static MENU_UPDATE_FUNC(afma_scan_passes_display)
+{
+    MENU_SET_VALUE(
+        "%d", 
+        afma_scan_passes
+    );
+            
+    MENU_SET_ICON(MNI_PERCENT, afma_scan_passes * 100 / AFMA_SCAN_PASSES_MAX);
+    afma_generic_update(entry, info);
+}
+
+static MENU_SELECT_FUNC(afma_scan_passes_toggle)
+{
+    if (afma_mode == AFMA_MODE_DISABLED)
+        return;
+    
+    menu_numeric_toggle(&afma_scan_passes, delta, 1, AFMA_SCAN_PASSES_MAX);
 }
 
 static MENU_UPDATE_FUNC(afma_display)
@@ -410,34 +730,41 @@ static struct menu_entry afma_menu[] = {
         .submenu_width = 700,
         .children =  (struct menu_entry[]) {
             {
-                .name = "Scan default range  (-20...+20)",
-                .priv = afma_auto_tune_std,
+                .name = "Start Scan",
+                .priv = afma_auto_tune,
                 .select = (void (*)(void*,int))run_in_separate_task,
                 .update = afma_generic_update,
                 .help  = "Step 1: achieve critical focus in LiveView with 10x zoom.",
                 .help2 = "Step 2: run this and leave the camera still for ~2 minutes.",
                 .depends_on = DEP_MANUAL_FOCUS,
             },
-            #ifdef CONFIG_AFMA_EXTENDED
             {
-                .name = "Scan extended range (-40...+40)",
-                .priv = afma_auto_tune_ext,
-                .select = (void (*)(void*,int))run_in_separate_task,
-                .update = afma_generic_update,
-                .help  = "This uses AFMA values normally not available in Canon menu.",
-                .help2 = "Run if focus is confirmed at +/-20, or if normal scan fails.",
-                .depends_on = DEP_MANUAL_FOCUS,
+                .name = "Scan type",
+                .priv = &afma_scan_range_index,
+                .select = afma_scan_range_toggle,
+                .min = 0,
+                #ifdef CONFIG_AFMA_EXTENDED
+                .max = 3,
+                #else
+                .max = 1,
+                #endif
+                .choices = CHOICES(
+                    "Auto range detection", 
+                    "Linear -20 .. +20", 
+                    #ifdef CONFIG_AFMA_EXTENDED
+                    "Linear -40 .. +40", 
+                    "Linear -100 .. +100", 
+                    #endif
+                ),
+                .help  = "AFMA scan type and range",
             },
             {
-                .name = "Scan extended range (-100..+100)",
-                .priv = afma_auto_tune_ultra,
-                .select = (void (*)(void*,int))run_in_separate_task,
-                .update = afma_generic_update,
-                .help  = "This uses AFMA values normally not available in Canon menu.",
-                .help2 = "Less accurate. Run only if you have severe focus problems.",
-                .depends_on = DEP_MANUAL_FOCUS,
+                .name = "Scan passes",
+                .update = afma_scan_passes_display,
+                .select = afma_scan_passes_toggle,
+                .help  = "Number of time to check for focus confirmation in the scan range.",
+                .edit_mode = EM_MANY_VALUES,
             },
-            #endif
             {
                 .name = "AF microadjust",
                 .update = afma_display,
