178d71eb943dec2f4844053a9a1987caf98b0a96
Giovanni Condello <condellog@gmail.com>
2013-07-15 12:24:43 +0200

    misc.c: fix warning

--- platform/1100D.105/misc.c
+++ platform/1100D.105/misc.c
@@ -7,54 +7,8 @@
 #include <consts.h>
 #include <lens.h>
 
-void DISABLED_display_shooting_info() // called from debug task
-{
-	if (lv) return;
-	
-	int bg = bmp_getpixel(314, 260);
-	uint32_t fnt = FONT(FONT_MED, COLOR_FG_NONLV, bg);
 
-	if (lens_info.wb_mode == WB_KELVIN)
-	{
-		bmp_printf(fnt, 185, 250, "%5dK", lens_info.kelvin);
-	}
-	if (lens_info.wbs_gm || lens_info.wbs_ba)
-	{
-		bg = bmp_getpixel(380, 250);
-		fnt = FONT(FONT_MED, COLOR_FG_NONLV, bg);
-
-		int ba = lens_info.wbs_ba;
-		if (ba) bmp_printf(fnt, 380 + 2 * font_med.width, 250, "%s%d", ba > 0 ? "A" : "B", ABS(ba));
-		else    bmp_printf(fnt, 380 + 2 * font_med.width, 250, "  ");
-
-		int gm = lens_info.wbs_gm;
-		if (gm) bmp_printf(fnt, 380, 250, "%s%d", gm > 0 ? "G" : "M", ABS(gm));
-		else    bmp_printf(fnt, 380, 250, "  ");
-	}
-
-	iso_refresh_display();
-
-	bg = bmp_getpixel(15, 430);
-	fnt = FONT(FONT_MED, COLOR_FG_NONLV, bg);
-	
-	extern int hdr_steps, hdr_stepsize, hdr_enabled;
-	if (hdr_enabled)
-		bmp_printf(fnt, 190, 450, "HDR %dx%dEV", hdr_steps, hdr_stepsize/8);
-	else
-		bmp_printf(fnt, 190, 450, "         ");
-
-	//~ bmp_printf(fnt, 400, 450, "Flash:%s", 
-		//~ strobo_firing == 0 ? " ON" : 
-		//~ strobo_firing == 1 ? "OFF" : "Auto"
-		//~ strobo_firing < 2 && flash_and_no_flash ? "/T" : "  "
-		//~ );
-
-	bmp_printf(fnt, 40, 460, get_mlu() ? "MLU" : "   ");
-
-	//~ display_lcd_remote_info();
-	display_trap_focus_info();
-}
-
+extern void* AllocateMemory_do(void* memory_pool, size_t size);
 
 // some dummy stubs
 int lcd_release_running = 0;
@@ -64,7 +18,5 @@ void display_lcd_remote_icon(int x0, int y0) {}
 
 void* AllocateMemory(size_t size) // this won't throw ERR70
 {
-	return (void*) AllocateMemory_do(*(int*)0x2B48, size);
+	return (void*) AllocateMemory_do((void*)0x2B48, size);
 }
-
-//int new_LiveViewApp_handler = 0xff123456;
