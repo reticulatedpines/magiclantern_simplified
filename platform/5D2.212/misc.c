e08efb82c47e09350970b6ca60ef7858f05c6352
pravdomil <pravdomil.toman@seznam.cz>
2013-07-29 22:38:32 +0200

    code clean up

--- platform/5D2.212/misc.c
+++ platform/5D2.212/misc.c
@@ -13,36 +13,3 @@ int lcd_release_running = 0;
 void lcd_release_step() {};
 int get_lcd_sensor_shortcuts() { return 0; }
 void display_lcd_remote_icon(int x0, int y0) {}
-
-/* ported in flexinfo
-void display_shooting_info() // called from debug task
-{
-	if (lv) return;
-	
-	int bg = COLOR_BLACK;
-	uint32_t fnt = SHADOW_FONT(FONT_MED);
-
-	if (lens_info.wb_mode == WB_KELVIN)
-	{
-		bmp_printf(fnt, 490, 275, "%5dK", lens_info.kelvin);
-	}
-	if (lens_info.wbs_gm || lens_info.wbs_ba)
-	{
-		bg = bmp_getpixel(15, 460);
-		fnt = FONT(FONT_MED, COLOR_FG_NONLV, bg);
-
-		int ba = lens_info.wbs_ba;
-		if (ba) bmp_printf(fnt, 350 + 2 * font_med.width, 460, "%s%d", ba > 0 ? "A" : "B", ABS(ba));
-		else    bmp_printf(fnt, 350 + 2 * font_med.width, 460, "  ");
-
-		int gm = lens_info.wbs_gm;
-		if (gm) bmp_printf(fnt, 350, 460, "%s%d", gm > 0 ? "G" : "M", ABS(gm));
-		else    bmp_printf(fnt, 350, 460, "  ");
-	}
-
-	bmp_printf(fnt, 40, 460, get_mlu() ? "MLU" : "   ");
-
-	display_lcd_remote_icon(555, 460);
-	display_trap_focus_info();
-}
-*/
