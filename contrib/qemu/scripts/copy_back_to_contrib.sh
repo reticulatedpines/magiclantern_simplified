58b13625d1c1867aecee82e90891577640739fb5
Antony Pavlov <antonynpavlov@gmail.com>
2013-08-03 01:54:26 +0400

    contrib/qemu: parametrize ML path [2]
    see also this:
    
        changeset:   42ef0642be1b
        user:        Antony Pavlov <antonynpavlov@gmail.com>
        date:        Tue Jul 30 03:48:48 2013 +0400
        files:       contrib/qemu/install.sh
        description:
        contrib: qemu: parametrize ML path

--- contrib/qemu/scripts/copy_back_to_contrib.sh
+++ contrib/qemu/scripts/copy_back_to_contrib.sh
@@ -3,9 +3,10 @@
 # run this if you make changes to qemu and want to commit them back into ML tree
 
 QEMU_PATH=qemu-1.4.0
+ML=magic-lantern
 
-cp -v *.sh gdbopts ../magic-lantern/contrib/qemu/scripts
-cp -v $QEMU_PATH/hw/eos.c $QEMU_PATH/hw/eos.h ../magic-lantern/contrib/qemu/hw
+cp -v *.sh gdbopts ../$ML/contrib/qemu/scripts
+cp -v $QEMU_PATH/hw/eos.c $QEMU_PATH/hw/eos.h ../$ML/contrib/qemu/hw
 
-cd ../magic-lantern/contrib/qemu/
+cd ../$ML/contrib/qemu/
 hg diff .
