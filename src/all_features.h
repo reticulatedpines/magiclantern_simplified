a8d45289915158e713568f01195803f065f5a0cc
alex <alex@thinkpad>
2013-08-03 20:20:45 +0300

    Placed FEATURE_AFMA_TUNING and FEATURE_LV_FOCUS_BOX_SNAP_TO_X5_RAW in the correct menus (for the HTML script)

--- src/all_features.h
+++ src/all_features.h
@@ -201,6 +201,8 @@
     //~ #define FEATURE_MOVIE_AF // not reliable
 #endif
 
+    //~ FEATURE_AFMA_TUNING
+
 /** Display menu **/
     #define FEATURE_LV_BRIGHTNESS_CONTRAST
     #define FEATURE_LV_SATURATION
@@ -261,6 +263,7 @@
 
     #define FEATURE_LV_FOCUS_BOX_FAST
     #define FEATURE_LV_FOCUS_BOX_SNAP
+    //~ #define FEATURE_LV_FOCUS_BOX_SNAP_TO_X5_RAW
     #define FEATURE_LV_FOCUS_BOX_AUTOHIDE
 
     #define FEATURE_ARROW_SHORTCUTS
