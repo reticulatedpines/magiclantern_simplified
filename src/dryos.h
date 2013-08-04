6859b595a9071d995f3418c82c1b1a44b74e940d
alex <alex@thinkpad>
2013-08-04 11:56:38 +0300

    Force inline with -Os, thanks @frantony

--- src/dryos.h
+++ src/dryos.h
@@ -601,6 +601,7 @@ const char* get_dcim_dir();
 
 #define FAST __attribute__((optimize("-O3")))
 #define SMALL __attribute__((optimize("-Os")))
+#define inline inline __attribute__((always_inline))
 
 // for modules and other optional code
 #define WEAK_FUNC(name)  __attribute__((weak,alias(#name))) 
