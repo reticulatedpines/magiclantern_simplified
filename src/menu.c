1c2b8c70291070652ec58f7e2313cb0b8287b116
alex <alex@thinkpad>
2013-08-10 18:20:57 +0300

    Menu: allow Q to return to submenus from transparent LiveView mode

--- src/menu.c
+++ src/menu.c
@@ -3264,7 +3264,8 @@ menu_entry_select(
     }
     else if (mode == 2) // Q
     {
-        if (menu_lv_transparent_mode || edit_mode) { menu_lv_transparent_mode = edit_mode = submenu_mode = 0; }
+        if (menu_lv_transparent_mode) { menu_lv_transparent_mode = 0; }
+        else if (edit_mode) { edit_mode = submenu_mode = 0; }
         else if ( entry->select_Q ) entry->select_Q( entry->priv, 1);
         else menu_toggle_submenu();
 
