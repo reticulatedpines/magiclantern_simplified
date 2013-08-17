5e9f9d3c4e34102fc4e773b2eac54d1dbcab6ecf
TheUnkn0wn <gary.mathews.93@gmail.com>
2013-08-17 21:01:12 +0100

    700D: AE_VALUE checked

--- platform/700D.111/consts.h
+++ platform/700D.111/consts.h
@@ -87,7 +87,7 @@
     #define MOV_OPT_STEP 5
     #define MOV_GOP_OPT_STEP 5
 
-    #define EXPO_COMP (*(int16_t*)0x366D4) //CHECK THIS
+    #define EXPO_COMP (*(int16_t*)0x366D4)
     #define AE_VALUE (EXPO_COMP-1) * 8 / 2048
 
     #define DLG_PLAY 1
