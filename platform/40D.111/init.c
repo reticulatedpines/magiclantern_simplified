ec1603d539e09268eefdf6cf86350c286735491c
jplxpto <joao_pedro_lx@hotmail.com>
2013-08-22 23:45:56 +0400

    40D: fix whitespaces and formatting
    Signed-off-by: Antony Pavlov <antonynpavlov@gmail.com>

--- platform/40D.111/init.c
+++ platform/40D.111/init.c
@@ -63,9 +63,9 @@ static void call_init_funcs( void * priv )
 extern uint32_t ptp_register_handlers_0x9800(void);
 
 uint32_t ml_hijack_ptp_register_handlers()
-{	
+{
 	uint32_t ret = 0;
-	
+
 	ret = ptp_register_handlers_0x9800();
 
 	ptp_register_all_handlers();
@@ -80,14 +80,14 @@ uint32_t ml_hijack_ptp_register_handlers()
 void ml_big_init_task()
 {
 	bmp_vram_idle_ptr = malloc(360*240);
-	
+
     load_fonts();
 
     call("DisablePowerSave");
     menu_init();
     debug_init();
     call_init_funcs( 0 );
-    
+
     msleep(200); // leave some time for property handlers to run
 
     config_parse_file( CARD_DRIVE "ML/SETTINGS/magic.cfg" );
@@ -111,34 +111,34 @@ void ml_big_init_task()
                 //~ streq(task->name, "audio_level_task") ||
                 //~ streq(task->name, "bitrate_task") ||
                 //~ streq(task->name, "cartridge_task") ||
-                //~ 
+                //~
                 streq(task->name, "cls_task") ||
                 //~ streq(task->name, "console_task") ||
                 streq(task->name, "debug_task") ||
-                //~ 
+                //~
                 streq(task->name, "focus_task") ||
-                //~ 
+                //~
                 streq(task->name, "focus_misc_task") ||
                 //~ streq(task->name, "fps_task") ||
                 //~ streq(task->name, "iso_adj_task") ||
 					//~ streq(task->name, "joypress_task") ||
                 //~ streq(task->name, "light_sensor_task") ||
-                //~ 
+                //~
                 streq(task->name, "livev_hiprio_task") ||
-                //~ 
+                //~
                 streq(task->name, "livev_loprio_task") ||
-                //~ streq(task->name, "menu_task_minimal") ||                     
-                //~ 
-                streq(task->name, "menu_task") ||        
+                //~ streq(task->name, "menu_task_minimal") ||
+                //~
+                streq(task->name, "menu_task") ||
                 streq(task->name, "menu_redraw_task") ||
                 //~ streq(task->name, "morse_task") ||
                 //~ streq(task->name, "movtweak_task") ||
 					//~ streq(task->name, "ms100_clock_task") ||
-                //~ 
+                //~
                 streq(task->name, "notifybox_task") ||
-                //~ 
+                //~
                 streq(task->name, "clock_task") ||
-                //~ 
+                //~
                 streq(task->name, "shoot_task") ||
                 //~ streq(task->name, "tweak_task") ||
             0 )
@@ -157,20 +157,18 @@ void ml_big_init_task()
     //~ bmp_printf( FONT_MED, 0, 85,
         //~ "Magic Lantern is up and running... %d tasks started.",
         //~ ml_tasks
-    //~ );   
+    //~ );
     msleep(500);
     ml_started = 1;
 }
 
-
 void ml_init_task(void * p)
 {
 	msleep(100);
-	
-    ml_big_init_task();	
+
+    ml_big_init_task();
     ml_hijack_gui_main_task();
 }
-
 
 extern void create_task_cmd_shell(const char * name);
 
@@ -182,7 +180,6 @@ void ml_hijack_create_task_cmd_shell(const char * name)
 	// create ml_init_task to start ML
 	task_create("ml_init_task", 0x1f, 0x2000, ml_init_task, 0);
 }
-
 
 void disable_cache_clearing()
 {
@@ -196,32 +193,31 @@ void disable_cache_clearing()
     cache_fake(0xFFD65518, 0xE1A00000, TYPE_ICACHE);
 }
 
-
 void configure_cache_replaces()
 {
     /* reserve 512 KB or RAM for ML (original: MOV R1, 0xc00000; modified: MOV R1, 0xB80000) */
     cache_fake(0xFF811354, 0xE3A0172E, TYPE_ICACHE);
 
     /* replace create_task_cmd_shell with our modified version to start ml_init_task */
-    cache_fake(0xFF81147C, BL_INSTR(0xFF81147C, &ml_hijack_create_task_cmd_shell), TYPE_ICACHE);  
-    
-#ifdef CONFIG_PTP    
+    cache_fake(0xFF81147C, BL_INSTR(0xFF81147C, &ml_hijack_create_task_cmd_shell), TYPE_ICACHE);
+
+#ifdef CONFIG_PTP
     /* replace one ptp_register funtion call with ml_hijack_ptp_register_handlers */
     /* sub_FFBA12E0 - 0xFFBA12E4 : BL sub_FFB9D4E4 */
-    cache_fake(0xFFBA12E4, BL_INSTR(0xFFBA12E4, &ml_hijack_ptp_register_handlers), TYPE_ICACHE); // OK
+    cache_fake(0xFFBA12E4, BL_INSTR(0xFFBA12E4, &ml_hijack_ptp_register_handlers), TYPE_ICACHE); /* OK */
 #endif
 }
 
 /*
     // sub_FFBA12E0 - 0xFFBA12E4 : BL sub_FFB9D4E4
     cache_fake(0xFFBA12E4, BL_INSTR(0xFFBA12E4, &ml_hijack_ptp_register_handlers), TYPE_ICACHE); // OK
-    
+
     // sub_FFBA42E8 - 0xFFBA4304 : BL sub_FFBA1370
     //cache_fake(0xFFBA4304, BL_INSTR(0xFFBA4304, &ml_hijack_ptp_register_handlers_dummy), TYPE_ICACHE);
-    
+
     // sub_FFB8B4A4 - 0xFFB8B4A8 : BL sub_FFB8B6EC
     //cache_fake(0xFFB8B4A8, BL_INSTR(0xFFB8B4A8, &ml_hijack_ptp_register_handlers_dummy), TYPE_ICACHE);
-    
+
     // sub_FFBA4DD4 - FFBA4F18 : BL sub_FFBB16DC
     //cache_fake(0xFFB8B4A8, BL_INSTR(0xFFB8B4A8, &ml_hijack_ptp_register_handlers_dummy), TYPE_ICACHE); // CERES NOT OK
 */
@@ -231,23 +227,22 @@ void configure_cache_replaces()
 void copy_and_restart()
 {
     zero_bss();
-    
+
     /* lock down caches */
     cache_lock();
-    
+
     disable_cache_clearing();
-    
-    configure_cache_replaces(); 
+
+    configure_cache_replaces();
 
     /* now start main firmware */
     /* I don't know why, the firmware_entry() method don't work */
     void (*reset)(void) = (void*) ROMBASEADDR;
     reset();
-    
+
     // unreachable
     while(1) LEDBLUE = LEDON;
 }
-
 
 /**
  * Custom ML assert handler
@@ -258,10 +253,10 @@ const char* get_assert_msg() { return assert_msg; }
 
 void ml_assert_handler(char* msg, char* file, int line, const char* func)
 {
-    snprintf(assert_msg, sizeof(assert_msg), 
+    snprintf(assert_msg, sizeof(assert_msg),
         "ML ASSERT:\n%s\n"
         "at %s:%d (%s), task %s\n",
-        msg, 
+        msg,
         file, line, func, get_task_name_from_id((int)get_current_task())
     );
     request_crash_log(2);
