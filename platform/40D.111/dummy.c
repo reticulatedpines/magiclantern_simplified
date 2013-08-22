49abe034b932d25c96046107215cf6a3d4161f17
Antony Pavlov <antonynpavlov@gmail.com>
2013-08-23 00:08:44 +0400

    40D: fix LCD sensor
    See also this changeset
    
      changeset:   c6777c6c3574
      user:        Giovanni Condello <condellog@gmail.com>
      date:        Mon Aug 19 19:58:00 2013 +0200
      description:
      LCD Sensor: Make it portable, enable on 650D

--- platform/40D.111/dummy.c
+++ platform/40D.111/dummy.c
@@ -4,10 +4,6 @@
 uint32_t shamem_read(uint32_t addr) { return 0; } // or maybe return MEM(addr)
 void _EngDrvOut(int addr, int value) { MEM(addr) = value; }
 
-int lcd_release_running = 0;
-void lcd_release_step() {};
-int get_lcd_sensor_shortcuts() { return 0; }
-void display_lcd_remote_icon(int x0, int y0) {}
 int new_LiveViewApp_handler = 0xff123456;
 
 void free_space_show_photomode(){}
