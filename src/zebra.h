fe48d7b4e5c73a38bf6360bc2c0ef79e26794ebd
pravdomil <pravdomil.toman@seznam.cz>
2013-09-08 02:56:39 +0200

    movie_cropmarks doxy

--- src/zebra.h
+++ src/zebra.h
@@ -5,6 +5,17 @@
 int liveview_display_idle();
 int get_global_draw();
 void NotifyBox( int timeout, char* fmt, ...);
+/**
+ * @brief Set custom movie cropmark in LiveView
+ *
+ * @param x X coordinate
+ * @param y Y coordinate
+ * @param w Width
+ * @param h Height
+ */
 void set_movie_cropmarks(int x, int y, int w, int h);
+/**
+ * @brief Reset LiveView movie cropmark
+ */
 void reset_movie_cropmarks();
 #endif //_zebra_h_
