791f4124fb9dd2e42cb3e3170146749436816ebe
g3gg0 <devnull@localhost>
2013-09-12 00:02:56 +0200

    module: cleaned up dumper script for hg infos

--- modules/module_hginfo_dump.sh
+++ modules/module_hginfo_dump.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+TMP_FILE="tmp.bin"
+
+if [[ -z "$@" ]]; then
+    echo >&2 "You must supply a module file"
+    exit 1
+elif [[ ! -f "$@" ]]; then
+    echo >&2 "$@ is not a valid module file"
+    exit 1
+fi
+
 echo "#-------- reading general hg information --------"
-arm-linux-gnueabihf-objcopy -O binary -j .module_hginfo raw_rec.mo out.bin
-cat out.bin | gunzip
+arm-linux-gnueabihf-objcopy -O binary -j .module_hginfo $@ $TMP_FILE
+gunzip < $TMP_FILE
 
 echo "#-------- reading hg diff --------"
-arm-linux-gnueabihf-objcopy -O binary -j .module_hgdiff raw_rec.mo out.bin
-cat out.bin | gunzip
+arm-linux-gnueabihf-objcopy -O binary -j .module_hgdiff $@ $TMP_FILE
+gunzip < $TMP_FILE
 
-rm out.bin
+rm $TMP_FILE
