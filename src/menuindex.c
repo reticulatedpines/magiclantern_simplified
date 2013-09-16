8766139441b7467b9a5d1b4024fb0c8ea126063a
alex <alex@thinkpad>
2013-09-16 17:16:27 +0300

    Help: display the shortcut keys for zebras in playback mode and arrow key shortcuts, if these features are used

--- src/menuindex.c
+++ src/menuindex.c
@@ -97,6 +97,21 @@ static struct menu_entry help_menus[] = {
         .name = "Press MENU",
         .choices = CHOICES("Junkie mode"),
     },
+    #ifdef FEATURE_OVERLAYS_IN_PLAYBACK_MODE
+    /* if BTN_ZEBRAS_FOR_PLAYBACK_NAME is undefined, you must define it (or undefine FEATURE_OVERLAYS_IN_PLAYBACK_MODE) */
+    {
+        .select = menu_nav_help_open,
+        .name = "PLAY: press "BTN_ZEBRAS_FOR_PLAYBACK_NAME,
+        .choices = CHOICES("Overlays in PLAY mode"),
+    },
+    #endif
+    #ifdef ARROW_MODE_TOGGLE_KEY
+    {
+        .select = menu_nav_help_open,
+        .name = "LiveView: press "ARROW_MODE_TOGGLE_KEY,
+        .choices = CHOICES("Arrow Shortcuts"),
+    },
+    #endif
     {
         .name = "Key Shortcuts",
         .select = menu_help_go_to_label,
