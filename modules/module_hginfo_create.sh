1791be55b12d59c595e32f1dfbd042d864595ecf
g3gg0 <devnull@localhost>
2013-09-17 22:05:05 +0200

    module: cleaned up hginfo embedding code

--- modules/module_hginfo_create.sh
+++ modules/module_hginfo_create.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
 LANG=enUS
-echo -ne "\n\n"
-echo -ne "char __module_hgdiff[] MODULE_HGDIFF_SECTION = \""
-hg diff . | gzip | od -tx1 | sed -e 's/^[0-9]* //' -e '$d' -e 's/^/ /' -e 's/ /\\x/g' | tr -d '\n'
-echo -ne "\";\n"
-echo -ne "char __module_hginfo[] MODULE_HGINFO_SECTION = \""
-(hg paths; hg parent) | gzip | od -tx1 | sed -e 's/^[0-9]* //' -e '$d' -e 's/^/ /' -e 's/ /\\x/g' | tr -d '\n'
-echo -ne "\";\n"
-echo -ne "\n\n"
+
+# get the history of the current directory. if it is empty, the directory isn't part of the current repository
+history=`hg history .`
+
+# encode all information using gzip and hexencode it. the resulting string is formatted e.g. "xdexadxbexef"
+if [ ${#history} -gt 0 ]; then
+    diff_str=`(hg diff -g .) | gzip | od -tx1 | sed -e 's/^[0-9]* //' -e '$d' -e 's/^/ /' -e 's/ /x/g' | tr -d '\n'`
+else
+    diff_str=`(echo "# This directory is not in repository") | gzip | od -tx1 | sed -e 's/^[0-9]* //' -e '$d' -e 's/^/ /' -e 's/ /x/g' | tr -d '\n'`
+fi
+repo_str=`(echo "Repo paths:"; echo -ne "path = "; hg root; hg paths; echo "Log:"; hg log --limit 1) | gzip | od -tx1 | sed -e 's/^[0-9]* //' -e '$d' -e 's/^/ /' -e 's/ /x/g' | tr -d '\n'`
+
+# now print the string and replace all occurences of 'x' with '\x'
+echo -ne "\n"
+echo -ne 'char __module_hgdiff[] MODULE_HGDIFF_SECTION = "'${diff_str//x/\\\\x}'";\n'
+echo -ne 'char __module_hginfo[] MODULE_HGINFO_SECTION = "'${repo_str//x/\\\\x}'";\n'
+echo -ne "\n"
+
+# done.
--- modules/module_hginfo_dump.sh
+++ modules/module_hginfo_dump.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
+# please set them according to your setup in your environment variables.
+# if not set, the ones are the default values
+ARM_ABI=${ARM_ABI:-none-eabi}
+ARM_PATH=${ARM_PATH:-~/gcc-arm-none-eabi-4_7-2012q4}
+GCC_VERSION=${GCC_VERSION:-4.7.3}
+ARM_BINPATH=${ARM_BINPATH:-$ARM_PATH/bin}
+OBJCOPY=${OBJCOPY:-$ARM_BINPATH/arm-$ARM_ABI-objcopy}
+
 TMP_FILE="tmp.bin"
 
-if [[ -z "$@" ]]; then
+if [[ -z "$1" ]]; then
     echo >&2 "You must supply a module file"
     exit 1
-elif [[ ! -f "$@" ]]; then
-    echo >&2 "$@ is not a valid module file"
+elif [[ ! -f "$1" ]]; then
+    echo >&2 "$1 is not a valid module file"
     exit 1
 fi
 
 echo "#-------- reading general hg information --------"
-$OBJCOPY -O binary -j .module_hginfo $@ $TMP_FILE
+$OBJCOPY -O binary -j .module_hginfo $@ $TMP_FILE || exit 0
 gunzip < $TMP_FILE
+rm $TMP_FILE
 
 echo "#-------- reading hg diff --------"
-$OBJCOPY -O binary -j .module_hgdiff $@ $TMP_FILE
+$OBJCOPY -O binary -j .module_hgdiff $@ $TMP_FILE || exit 0
 gunzip < $TMP_FILE
-
 rm $TMP_FILE
+
+echo "#-------- DONE --------"
