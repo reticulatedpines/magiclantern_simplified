400535a9e4ba545f3f1bcf945dbfbda3af14dca9
g3gg0 <devnull@localhost>
2013-09-18 00:25:50 +0200

    module: use makefile and objcopy to embed hg information

--- modules/module_hginfo_dump.sh
+++ modules/module_hginfo_dump.sh
@@ -19,12 +19,12 @@ elif [[ ! -f "$1" ]]; then
 fi
 
 echo "#-------- reading general hg information --------"
-$OBJCOPY -O binary -j .module_hginfo $@ $TMP_FILE || exit 0
+$OBJCOPY -O binary -j .module_hginfo --set-section-flags .module_hginfo=load $@ $TMP_FILE || exit 0
 gunzip < $TMP_FILE
 rm $TMP_FILE
 
 echo "#-------- reading hg diff --------"
-$OBJCOPY -O binary -j .module_hgdiff $@ $TMP_FILE || exit 0
+$OBJCOPY -O binary -j .module_hgdiff --set-section-flags .module_hgdiff=load $@ $TMP_FILE || exit 0
 gunzip < $TMP_FILE
 rm $TMP_FILE
 
