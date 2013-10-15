f12d2336a03c712f194bfe0bbab1941d6a90cda8
alex <alex@thinkpad>
2013-10-15 13:16:22 +0300

    raw2dng: 2x2 chroma smoothing

--- modules/lv_rec/raw2dng.c
+++ modules/lv_rec/raw2dng.c
@@ -513,6 +513,7 @@ static void fix_vertical_stripes()
 }
 
 #ifdef CHROMA_SMOOTH
+
 static void chroma_smooth_3x3(unsigned short * inp, unsigned short * out, int* raw2ev, int* ev2raw)
 {
     int w = raw_info.width;
@@ -668,8 +669,174 @@ static void chroma_smooth_3x3(unsigned short * inp, unsigned short * out, int* r
             }
 
             /* replace red and blue pixels with filtered values, keep green pixels unchanged */
-            out[x   +     y * w] = ev2raw[COERCE(gr + dr, -10*EV_RESOLUTION, 14*EV_RESOLUTION)];
-            out[x+1 + (y+1) * w] = ev2raw[COERCE(gb + db, -10*EV_RESOLUTION, 14*EV_RESOLUTION)];
+            out[x   +     y * w] = ev2raw[COERCE(gr + dr, -10*EV_RESOLUTION, 14*EV_RESOLUTION-1)];
+            out[x+1 + (y+1) * w] = ev2raw[COERCE(gb + db, -10*EV_RESOLUTION, 14*EV_RESOLUTION-1)];
+        }
+    }
+}
+
+/* processes top, bottom, left and right neighbours */
+static void chroma_smooth_2x2(unsigned short * inp, unsigned short * out, int* raw2ev, int* ev2raw)
+{
+    int w = raw_info.width;
+    int h = raw_info.height;
+    int x,y;
+
+    for (y = 4; y < h-5; y += 2)
+    {
+        for (x = 4; x < w-4; x += 2)
+        {
+            /**
+             * for each red pixel, compute the median value of red minus interpolated green at the same location
+             * the median value is then considered the "true" difference between red and green
+             * same for blue vs green
+             * 
+             *
+             * each red pixel has 4 green neighbours, so we may interpolate as follows:
+             * - mean or median(t,b,l,r)
+             * - choose between mean(t,b) and mean(l,r) (idea from AHD)
+             * 
+             * same for blue; note that a RG/GB cell has 6 green pixels that we need to analyze
+             * 2 only for red, 2 only for blue, and 2 shared
+             *    g
+             *   gRg
+             *    gBg
+             *     g
+             *
+             * choosing the interpolation direction seems to give cleaner results
+             * the direction is choosen over the entire filtered area (so we do two passes, one for each direction, 
+             * and at the end choose the one for which total interpolation error is smaller)
+             * 
+             * error = sum(abs(t-b)) or sum(abs(l-r))
+             * 
+             * interpolation in EV space (rather than linear) seems to have less color artifacts in high-contrast areas
+             * 
+             * we can use this filter for 3x3 RG/GB cells or 5x5
+             */
+            int i,j;
+            int k = 0;
+            int med_r[5];
+            int med_b[5];
+            
+            /* first try to interpolate in horizontal direction */
+            int eh = 0;
+            for (i = -2; i <= 2; i += 2)
+            {
+                for (j = -2; j <= 2; j += 2)
+                {
+                    if (ABS(i) + ABS(j) == 4) continue;
+                    
+                    int r  = inp[x+i   +   (y+j) * w];
+                    int b  = inp[x+i+1 + (y+j+1) * w];
+                                                        /*  for R      for B      */
+                    int g1 = inp[x+i+1 +   (y+j) * w];  /*  Right      Top        */
+                    int g2 = inp[x+i   + (y+j+1) * w];  /*  Bottom     Left       */
+                    int g3 = inp[x+i-1 +   (y+j) * w];  /*  Left                  */ 
+                  //int g4 = inp[x+i   + (y+j-1) * w];  /*  Top                   */
+                    int g5 = inp[x+i+2 + (y+j+1) * w];  /*             Right      */
+                  //int g6 = inp[x+i+1 + (y+j+2) * w];  /*             Bottom     */
+                    
+                    g1 = raw2ev[g1];
+                    g2 = raw2ev[g2];
+                    g3 = raw2ev[g3];
+                  //g4 = raw2ev[g4];
+                    g5 = raw2ev[g5];
+                  //g6 = raw2ev[g6];
+                    
+                    int gr = (g1+g3)/2;
+                    int gb = (g2+g5)/2;
+                    eh += ABS(g1-g3) + ABS(g2-g5);
+                    med_r[k] = raw2ev[r] - gr;
+                    med_b[k] = raw2ev[b] - gb;
+                    k++;
+                }
+            }
+
+            /* difference from green, with horizontal interpolation */
+            int drh = opt_med5(med_r);
+            int dbh = opt_med5(med_b);
+            
+            /* next, try to interpolate in vertical direction */
+            int ev = 0;
+            k = 0;
+            for (i = -2; i <= 2; i += 2)
+            {
+                for (j = -2; j <= 2; j += 2)
+                {
+                    if (ABS(i) + ABS(j) == 4) continue;
+
+                    int r  = inp[x+i   +   (y+j) * w];
+                    int b  = inp[x+i+1 + (y+j+1) * w];
+                                                        /*  for R      for B      */
+                    int g1 = inp[x+i+1 +   (y+j) * w];  /*  Right      Top        */
+                    int g2 = inp[x+i   + (y+j+1) * w];  /*  Bottom     Left       */
+                  //int g3 = inp[x+i-1 +   (y+j) * w];  /*  Left                  */ 
+                    int g4 = inp[x+i   + (y+j-1) * w];  /*  Top                   */
+                  //int g5 = inp[x+i+2 + (y+j+1) * w];  /*             Right      */
+                    int g6 = inp[x+i+1 + (y+j+2) * w];  /*             Bottom     */
+                    
+                    g1 = raw2ev[g1];
+                    g2 = raw2ev[g2];
+                  //g3 = raw2ev[g3];
+                    g4 = raw2ev[g4];
+                  //g5 = raw2ev[g5];
+                    g6 = raw2ev[g6];
+                    
+                    int gr = (g2+g4)/2;
+                    int gb = (g1+g6)/2;
+                    ev += ABS(g2-g4) + ABS(g1-g6);
+                    med_r[k] = raw2ev[r] - gr;
+                    med_b[k] = raw2ev[b] - gb;
+                    k++;
+                }
+            }
+
+            /* difference from green, with vertical interpolation */
+            int drv = opt_med5(med_r);
+            int dbv = opt_med5(med_b);
+
+            /* back to our filtered pixels (RG/GB cell) */
+            int g1 = inp[x+1 +     y * w];
+            int g2 = inp[x   + (y+1) * w];
+            int g3 = inp[x-1 +   (y) * w];
+            int g4 = inp[x   + (y-1) * w];
+            int g5 = inp[x+2 + (y+1) * w];
+            int g6 = inp[x+1 + (y+2) * w];
+            
+            g1 = raw2ev[g1];
+            g2 = raw2ev[g2];
+            g3 = raw2ev[g3];
+            g4 = raw2ev[g4];
+            g5 = raw2ev[g5];
+            g6 = raw2ev[g6];
+
+            /* which of the two interpolations will we choose? */
+            int grv = (g2+g4)/2;
+            int grh = (g1+g3)/2;
+            int gbv = (g1+g6)/2;
+            int gbh = (g2+g5)/2;
+            int gr = ev < eh ? grv : grh;
+            int gb = ev < eh ? gbv : gbh;
+            int dr = ev < eh ? drv : drh;
+            int db = ev < eh ? dbv : dbh;
+            
+            int r0 = inp[x   +     y * w];
+            int b0 = inp[x+1 + (y+1) * w];
+
+            /* if we are close to the noise floor, use both directions, beacuse otherwise it will affect the noise structure and introduce false detail */
+            /* todo: smooth transition between the two methods? better thresholding condition? */
+            int thr = 64;
+            if (r0 < raw_info.black_level+thr || b0 < raw_info.black_level+thr || ABS(drv - drh) < thr || ABS(grv-grh) < thr || ABS(gbv-gbh) < thr)
+            {
+                dr = (drv+drh)/2;
+                db = (dbv+dbh)/2;
+                gr = (g1+g2+g3+g4)/4;
+                gb = (g1+g2+g5+g6)/4;
+            }
+
+            /* replace red and blue pixels with filtered values, keep green pixels unchanged */
+            out[x   +     y * w] = ev2raw[COERCE(gr + dr, -10*EV_RESOLUTION, 14*EV_RESOLUTION-1)];
+            out[x+1 + (y+1) * w] = ev2raw[COERCE(gb + db, -10*EV_RESOLUTION, 14*EV_RESOLUTION-1)];
         }
     }
 }
@@ -829,8 +996,8 @@ static void chroma_smooth_5x5(unsigned short * inp, unsigned short * out, int* r
             }
             
             /* replace red and blue pixels with filtered values, keep green pixels unchanged */
-            out[x   +     y * w] = ev2raw[COERCE(gr + dr, -10*EV_RESOLUTION, 14*EV_RESOLUTION)];
-            out[x+1 + (y+1) * w] = ev2raw[COERCE(gb + db, -10*EV_RESOLUTION, 14*EV_RESOLUTION)];
+            out[x   +     y * w] = ev2raw[COERCE(gr + dr, -10*EV_RESOLUTION, 14*EV_RESOLUTION-1)];
+            out[x+1 + (y+1) * w] = ev2raw[COERCE(gb + db, -10*EV_RESOLUTION, 14*EV_RESOLUTION-1)];
         }
     }
 }
@@ -864,7 +1031,7 @@ static void chroma_smooth()
         for (x = 0; x < w; x++)
             aux[x + y*w] = aux2[x + y*w] = raw_get_pixel(x, y);
     
-    chroma_smooth_3x3(aux, aux2, raw2ev, ev2raw);
+    chroma_smooth_2x2(aux, aux2, raw2ev, ev2raw);
     
     for (y = 0; y < h; y++)
         for (x = 0; x < w; x++)
