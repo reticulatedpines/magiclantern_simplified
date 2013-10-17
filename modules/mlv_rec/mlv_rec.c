e41730f86e7ef79176cd2540e33aec7ea3086b93
g3gg0 <devnull@localhost>
2013-10-17 21:04:10 +0200

    mlv_rec: really fix 600D hacks and 7D detection...

--- modules/mlv_rec/mlv_rec.c
+++ modules/mlv_rec/mlv_rec.c
@@ -83,6 +83,7 @@ static uint32_t cam_5d2 = 0;
 static uint32_t cam_50d = 0;
 static uint32_t cam_5d3 = 0;
 static uint32_t cam_6d = 0;
+static uint32_t cam_600d = 0;
 static uint32_t cam_7d = 0;
 static uint32_t cam_700d = 0;
 
@@ -1546,6 +1547,7 @@ static void hack_liveview(int32_t unhack)
             cam_50d ? 0xffa84e00 :
             cam_5d2 ? 0xffaac640 :
             cam_5d3 ? 0xff4acda4 :
+            cam_600d ? 0xFF37AA18 :
 			cam_6d  ? 0xFF52BE94 :
 			cam_eos_m ? 0xFF539C1C :
 			cam_700d ? 0xFF52B53C :
@@ -3548,6 +3550,8 @@ static unsigned int raw_rec_init()
     cam_50d = streq(camera_model_short, "50D");
     cam_5d3 = streq(camera_model_short, "5D3");
     cam_6d = streq(camera_model_short, "6D");
+    cam_600d = streq(camera_model_short, "600D");
+    cam_7d = streq(camera_model_short, "7D");
     cam_700d = streq(camera_model_short, "700D");
     
     for (struct menu_entry * e = raw_video_menu[0].children; !MENU_IS_EOL(e); e++)
