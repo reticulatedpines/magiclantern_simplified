380adec4beb00fa9b7d082c459db023440d63557
alex <alex@thinkpad>
2013-11-30 21:25:19 +0200

    cr2hdr: fine-tuned autodetection for Adobe DNG Converter

--- modules/dual_iso/adobedng-bridge.c
+++ modules/dual_iso/adobedng-bridge.c
@@ -15,38 +15,56 @@ static const char* search_path[] = {
 
 static int can_exec(const char *file)
 {
+    //~ printf("%s: %d\n", file, !access(file, X_OK));
     return !access(file, X_OK);
 }
 
 static const char* find_adobe_dng_converter()
 {
-    static char adobe_dng_path[1000];
+    static char adobe_dng_path[1000] = "";
+    
+    /* valid path from previous attempt? just return it */
+    if (adobe_dng_path[0])
+        return adobe_dng_path;
     
     /* let's try some usual paths */
     int i;
     for (i = 0; i < COUNT(search_path); i++)
     {
-        snprintf(adobe_dng_path, sizeof(adobe_dng_path), "%s%s", search_path[i], "Adobe DNG Converter");
-        if (can_exec(adobe_dng_path))
-            return adobe_dng_path;
-
-        snprintf(adobe_dng_path, sizeof(adobe_dng_path), "%s%s", search_path[i], "Adobe DNG Converter.exe");
-        if (can_exec(adobe_dng_path))
-            return adobe_dng_path;
-    }
-    
-    /* let's ask Wine to find the path for us */
-    FILE* f = popen("winepath \"C:\\Program Files\\Adobe\\Adobe DNG Converter.exe\"", "r");
-    if (f)
-    {
-        if (fgets(adobe_dng_path, sizeof(adobe_dng_path), f))
+        int is_win_path = (search_path[i][strlen(search_path[i])-1] == '\\');
+        if (is_win_path)
         {
-            strtok(adobe_dng_path, "\n");
+            snprintf(adobe_dng_path, sizeof(adobe_dng_path), "%s%s", search_path[i], "Adobe DNG Converter.exe");
+            if (can_exec(adobe_dng_path))
+                return adobe_dng_path;
+
+            /* let's try Wine */
+            #if !defined(WIN32) && !defined(_WIN32)
+            snprintf(adobe_dng_path, sizeof(adobe_dng_path), "winepath \"%s%s\"", search_path[i], "Adobe DNG Converter.exe");
+            FILE* f = popen(adobe_dng_path, "r");
+            if (f)
+            {
+                int ok = fgets(adobe_dng_path, sizeof(adobe_dng_path), f) != 0;
+                pclose(f);
+                
+                if (ok)
+                {
+                    strtok(adobe_dng_path, "\n");
+                    if (can_exec(adobe_dng_path))
+                        return adobe_dng_path;
+                }
+            }
+            #endif
+        }
+        else
+        {
+            snprintf(adobe_dng_path, sizeof(adobe_dng_path), "%s%s", search_path[i], "Adobe DNG Converter");
             if (can_exec(adobe_dng_path))
                 return adobe_dng_path;
         }
     }
     
+    adobe_dng_path[0] = 0;
     return 0;
 }
 
