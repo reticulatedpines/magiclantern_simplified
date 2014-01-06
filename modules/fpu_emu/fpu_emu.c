75d84b4bfffc97b09b85452da831439d7d3628f8
g3gg0 <devnull@localhost>
2014-01-06 13:56:41 +0100

    fpu_emu: added some comments, implemented uninstall code

--- modules/fpu_emu/fpu_emu.c
+++ modules/fpu_emu/fpu_emu.c
@@ -1,5 +1,5 @@
 /**
- * fpu_emu: 
+ * fpu_emu: Emulate ARM FPA code in software, uses illegal instruction trap
  */
 
 #include <module.h>
@@ -10,25 +10,32 @@
 #include <tasks.h>
 #include <config.h>
 
+/* this file uses FPA11 structure, so include the definition */
 #include <nwfpe/fpa11.h>
 
 #define FPU_EMU_STACK_SIZE 512
 #define FPU_EMU_MAX_TASKS 256
 
+
+/* the stack pointer is defined in assembly code, declare it here */
 extern uint32_t fpu_emu_stack;
 
+/* this buffer stores both stack and the context of the trapping task */
 uint32_t fpu_emu_context_buffer[FPU_EMU_STACK_SIZE + 18 /* context */];
-void (*fpu_emu_orig_undef_handler)(void) = 0;
+void (*fpu_emu_orig_undef_handler)(void) = NULL;
+uint32_t fpu_emu_undef_handler_addr = 0;
 
+/* pointer to the trapping task ctx, will point into fpu_emu_context_buffer */
 uint32_t *fpu_emu_current_ctx = NULL;
 
+/* counters and task information */
 uint32_t fpu_emu_call_count = 0;
 uint32_t fpu_emu_error_count = 0;
 uint32_t fpu_emu_signal_count = 0;
 uint32_t fpu_emu_trap_addr = 0;
 int32_t fpu_emu_trap_task = 0;
 
-
+/* for every task we have a separate FPU register set */
 static FPA11 *fpu_emu_fp_ctx[FPU_EMU_MAX_TASKS];
 
 
@@ -138,34 +145,40 @@ void __attribute__((naked)) fpu_emu_undef_handler()
 
 static uint32_t fpu_emu_install()
 {
-    uint32_t undef_handler_addr = 0;
     uint32_t undef_handler_opcode = *(uint32_t*)0x04;
     
     /* make sure abort handler address is loaded into PC using PC relative LDR */
     if((undef_handler_opcode & 0xFFFFF000) != 0xE59FF000)
     {
         bmp_printf(FONT(FONT_MED, COLOR_RED, COLOR_BLACK), 0, 0, "fpu_emu: Failed to find hook point");
-        return 0;
+        return -1;
     }
     
     /* extract offset from LDR */
-    undef_handler_addr = (undef_handler_opcode & 0x00000FFF) + 0x04 + 0x08;
+    fpu_emu_undef_handler_addr = (undef_handler_opcode & 0x00000FFF) + 0x04 + 0x08;
     
     /* first install stack etc */
     fpu_emu_stack = (uint32_t)&fpu_emu_context_buffer[FPU_EMU_STACK_SIZE + 17];
     fpu_emu_context_buffer[17] = 0xDEADBEEF;
 
     /* then patch handler */
-    fpu_emu_orig_undef_handler = (void*)MEM(undef_handler_addr);
-    MEM(undef_handler_addr) = (uint32_t)&fpu_emu_undef_handler;
+    fpu_emu_orig_undef_handler = (void*)MEM(fpu_emu_undef_handler_addr);
+    MEM(fpu_emu_undef_handler_addr) = (uint32_t)&fpu_emu_undef_handler;
     
-    if(MEM(undef_handler_addr) != (uint32_t)&fpu_emu_undef_handler)
+    if(MEM(fpu_emu_undef_handler_addr) != (uint32_t)&fpu_emu_undef_handler)
     {
         bmp_printf(FONT(FONT_MED, COLOR_RED, COLOR_BLACK), 0, 0, "fpu_emu: Failed to install trap handler");
-        return 0;
+        return -1;
     }
-    
-    bmp_printf(FONT(FONT_MED, COLOR_RED, COLOR_BLACK), 0, 0, "fpu_emu: installed");
+
+    return 0;
+}
+
+static uint32_t fpu_emu_uninstall()
+{
+    /* just remove undefined instruction trap pointer */
+    MEM(fpu_emu_undef_handler_addr) = (uint32_t)fpu_emu_orig_undef_handler;
+
     return 0;
 }
 
@@ -209,6 +222,7 @@ static uint32_t __attribute__((naked)) fpu_emu_test(uint32_t a, uint32_t b)
         MOV PC, LR;\
     ");
 }
+
 static MENU_SELECT_FUNC(fpu_emu_select)
 {
     uint32_t ret = fpu_emu_test(40, 50);
@@ -229,7 +243,8 @@ static struct menu_entry fpu_emu_menu[] =
     {
         .name = "FPU Emu",
         .select = menu_open_submenu,
-        .children =  (struct menu_entry[]) {
+        .children = (struct menu_entry[])
+        {
             {
                 .name = "Test FPU",
                 .update = fpu_emu_update_count,
@@ -266,10 +281,11 @@ static unsigned int fpu_emu_init()
 
 static unsigned int fpu_emu_deinit()
 {
+    fpu_emu_uninstall();
     return 0;
 }
 
-/* accessor functions for NWFPE code */
+/* accessor functions for NWFPE code to read/write tasks CPU registers */
 uint32_t fpu_emu_read_reg(uint32_t reg)
 {
     return fpu_emu_current_ctx[reg];
@@ -280,13 +296,15 @@ void fpu_emu_write_reg(uint32_t reg, uint32_t val)
     fpu_emu_current_ctx[reg] = val;
 }
 
+/* this function must return a pointer to the current tasks FPU state */
 FPA11 *fpu_emu_get_fpa11()
 {
     return fpu_emu_fp_ctx[fpu_emu_trap_task];
 }
 
 
-/* some glue code here */
+/* the libraries were designed for linux with MMU systems where user memory maps differently
+   and is not directly accessible from kernel context. not needed for our MMU-less system */
 void put_user(uint32_t val, void *addr) 
 {
     MEM(addr) = val;
@@ -297,6 +315,7 @@ void get_user(uint32_t *val, void *addr)
     *val = MEM(addr);
 }
 
+/* just count the signals, but dont handle yet. do we need signalling? */
 void float_raise(signed char flags)
 {
     fpu_emu_signal_count++;
@@ -306,25 +325,26 @@ void float_raise(signed char flags)
 void __attribute__((naked)) __ashldi3()
 {
     asm("\
-	subs	r3, r2, #32          ;\
-	rsb	    r12, r2, #32         ;\
-	movmi	r1, r1, lsl r2       ;\
-	movpl	r1, r0, lsl r3       ;\
-	orrmi	r1, r1, r0, lsr r12  ;\
-	mov	    r0, r0, lsl r2       ;\
-	mov     pc, lr               ;\
+        subs    r3, r2, #32          ;\
+        rsb     r12, r2, #32         ;\
+        movmi   r1, r1, lsl r2       ;\
+        movpl   r1, r0, lsl r3       ;\
+        orrmi   r1, r1, r0, lsr r12  ;\
+        mov     r0, r0, lsl r2       ;\
+        mov     pc, lr               ;\
     ");
 }
+
 void __attribute__((naked)) __lshrdi3()
 {
     asm("\
-	subs	r3, r2, #32          ;\
-	rsb	    r12, r2, #32         ;\
-	movmi	r0, r0, lsr r2       ;\
-	movpl	r0, r1, lsr r3       ;\
-	orrmi	r0, r0, r1, lsl r12  ;\
-	mov	    r1, r1, lsr r2       ;\
-	mov     pc, lr               ;\
+        subs    r3, r2, #32          ;\
+        rsb     r12, r2, #32         ;\
+        movmi   r0, r0, lsr r2       ;\
+        movpl   r0, r1, lsr r3       ;\
+        orrmi   r0, r0, r1, lsl r12  ;\
+        mov     r1, r1, lsr r2       ;\
+        mov     pc, lr               ;\
     ");
 }
 
