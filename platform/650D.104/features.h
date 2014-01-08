fbff114a1d4f1b0306a3d87971dd4d0e444b25a9
Mathew Kelly <dkelly11@tampabay.rr.com>
2014-01-08 11:03:50 +0000

    Added 650D audio meters and beep features

--- platform/650D.104/features.h
+++ platform/650D.104/features.h
@@ -6,11 +6,12 @@
 // Disable all audio stuff
 #undef FEATURE_WAV_RECORDING
 #undef FEATURE_FPS_WAV_RECORD
-#undef FEATURE_BEEP
 #undef FEATURE_VOICE_TAGS
 #undef FEATURE_AUDIO_REMOTE_SHOT
-#undef FEATURE_AUDIO_METERS
 
+// Audio features working
+#define FEATURE_AUDIO_METERS // only when recording movie
+#define FEATURE_BEEP
 
 //~ #undef FEATURE_ARROW_SHORTCUTS
 
