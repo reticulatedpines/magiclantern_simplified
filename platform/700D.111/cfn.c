027a616aa17933e98829333e65e3d4819956e7db
Mathew Kelly <dkelly11@tampabay.rr.com>
2014-01-15 13:17:41 +0000

    700d, Fix for ALO in cfn.c

--- platform/700D.111/cfn.c
+++ platform/700D.111/cfn.c
@@ -3,39 +3,44 @@
 
 // look on camera menu or review sites to get custom function numbers
 
-int get_htp() { 
+int get_htp() 
+{ 
 	return GetCFnData(0, 3);
- }
+}
 
-void set_htp(int value) {
+void set_htp(int value) 
+{
 	SetCFnData(0, 3, value); 
 }
 
-int get_mlu() {
+int get_mlu() 
+{
 	return GetCFnData(0, 5); 
 }
 
-void set_mlu(int value) { 
+void set_mlu(int value) 
+{ 
 	SetCFnData(0, 5, value);
 }
 
-int cfn_get_af_button_assignment() {
+int cfn_get_af_button_assignment() 
+{
 	return GetCFnData(0, 6);
 }
 
-void cfn_set_af_button(int value) {
+void cfn_set_af_button(int value) 
+{
 	SetCFnData(0, 6, value);
 }
 
-// on some cameras, ALO is CFn
-//PROP_INT(PROP_ALO, alo);
-int get_alo() { 
-	//return alo; 
-	return 0;
+PROP_INT(PROP_ALO, alo); //ALO has to be set this way since it's in Canon Main Menu
+int get_alo() 
+{ 
+       return alo & 0xFF; 
 }
 
-void set_alo(int value)
-{
-	/*value = COERCE(value, 0, 3);
-	prop_request_change(PROP_ALO, &value, 4);*/
+void set_alo(int value) 
+{  
+    value = COERCE(value, 0, 3);
+    prop_request_change(0x8000003d, value, 0x2f);
 }
