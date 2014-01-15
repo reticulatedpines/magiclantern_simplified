4e9497ab4e9035e86be1b9d88ed37fcd6bbbc02f
Mathew Kelly <dkelly11@tampabay.rr.com>
2014-01-15 08:33:24 +0000

    700D Enable cfn.c stuff

--- platform/700D.111/cfn.c
+++ platform/700D.111/cfn.c
@@ -4,41 +4,41 @@
 // look on camera menu or review sites to get custom function numbers
 
 int get_htp() { 
-	/*return GetCFnData(0, 3);*/
+	return GetCFnData(0, 3);
 	return 0;
  }
 
 void set_htp(int value) {
-	//SetCFnData(0, 3, value); 
+	SetCFnData(0, 3, value); 
 }
 
 int get_mlu() {
-	//return GetCFnData(0, 5); 
+	return GetCFnData(0, 5); 
 	return 0;
 }
 
 void set_mlu(int value) { 
-	//SetCFnData(0, 5, value);
+	SetCFnData(0, 5, value);
 }
 
 int cfn_get_af_button_assignment() {
-	//return GetCFnData(0, 6);
+	return GetCFnData(0, 6);
 	return 0;
 }
 
 void cfn_set_af_button(int value) {
-	//SetCFnData(0, 6, value);
+	SetCFnData(0, 6, value);
 }
 
 // on some cameras, ALO is CFn
-//PROP_INT(PROP_ALO, alo);
+PROP_INT(PROP_ALO, alo);
 int get_alo() { 
-	//return alo; 
+	return alo; 
 	return 0;
 }
 
 void set_alo(int value)
 {
-	/*value = COERCE(value, 0, 3);
-	prop_request_change(PROP_ALO, &value, 4);*/
+	value = COERCE(value, 0, 3);
+	prop_request_change(PROP_ALO, &value, 4);
 }
