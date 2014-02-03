b2556195d394fba85447486038f340999e7cac4d
Marsu42 <devnull@localhost>
2014-02-03 10:34:24 +0100

    dual_iso: change another api name in the core

--- src/lens.c
+++ src/lens.c
@@ -2422,7 +2422,7 @@ static LVINFO_UPDATE_FUNC(tv_update)
     }
 }
 
-static int (*dual_iso_is_enabled)() = MODULE_FUNCTION(dual_iso_is_enabled);
+static int (*dual_iso_is_active)() = MODULE_FUNCTION(dual_iso_is_active);
 static int (*dual_iso_get_recovery_iso)() = MODULE_FUNCTION(dual_iso_get_recovery_iso);
 
 static LVINFO_UPDATE_FUNC(iso_update)
@@ -2437,7 +2437,7 @@ static LVINFO_UPDATE_FUNC(iso_update)
         iso_high = raw2iso(get_effective_hdr_iso_for_display(iso_high));
         snprintf(buffer, sizeof(buffer), SYM_ISO"%d/%d", iso_low, iso_high);
     }
-    else if (dual_iso_is_enabled())
+    else if (dual_iso_is_active())
     {
         snprintf(buffer, sizeof(buffer), SYM_ISO"%d/%d", 
             raw2iso(lens_info.iso_analog_raw),
