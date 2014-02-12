7adb4249f1a816bdffd69bbd3559db8b6c59249b
alex <alex@thinkpad>
2014-02-12 15:44:23 +0200

    Memory backend: use a more pessimistic heuristic for max region

--- src/mem.c
+++ src/mem.c
@@ -542,7 +542,7 @@ static int search_for_allocator(int size, int require_preferred_size, int requir
                         )
                     {
                         /* do we have a large enough contiguous chunk? */
-                        int max_region = allocators[a].get_max_region ? allocators[a].get_max_region() : free_space / 2;
+                        int max_region = allocators[a].get_max_region ? allocators[a].get_max_region() : free_space / 4;
                         //~ console_printf("%s: max rgn %s\n", allocators[a].name, format_memory_size(max_region));
                         if (size < max_region)
                         {
