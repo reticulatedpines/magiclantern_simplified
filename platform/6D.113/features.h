eb2baf7c1ffcf26a01e63b05a82e940b46e0fa94
Giovanni Condello <condellog@gmail.com>
2014-02-15 15:55:48 +0100

    Cleanup and fix 6D build

--- platform/6D.113/features.h
+++ platform/6D.113/features.h
@@ -7,24 +7,11 @@
 #undef FEATURE_QUICK_ZOOM    //Canon Has it
 #undef FEATURE_IMAGE_EFFECTS // Missing Zero Sharpness :( Regs have changed
 #undef FEATURE_IMAGE_REVIEW_PLAY // not needed, one can press Zoom right away
-#undef CONFIG_STRESS_TEST // We don't need this so much.
-#undef CONFIG_MEMPATCH_CHECK // Reports 0 for total.
-
 
 /*Work Relatively well*/
-#define CONFIG_FPS_AGGRESSIVE_UPDATE
 #define FEATURE_FOCUS_PEAK_DISP_FILTER
 #define FEATURE_ZOOM_TRICK_5D3 // Doubleclick to zoom/shortcut
 #define FEATURE_KEN_ROCKWELL_ZOOM_5D3 // Play From Image Review Mode - Did not bring up play
-#define CONFIG_AFMA_EXTENDED
-/* Development */
-
-/* Debugging Stuff */
-#define CONFIG_HEXDUMP
-#define CONFIG_DIGIC_POKE
-//~ #define FEATURE_SHOW_SIGNATURE
-//~ #define CONFIG_DUMPROM Lawyers will kill your camera.. and you.
-
 
 /* Audio Features */
 #define FEATURE_HEADPHONE_MONITORING
@@ -32,4 +19,5 @@
 #define FEATURE_WAV_RECORDING
 #define FEATURE_AUDIO_METERS
 
-//~ #define FEATURE_HEADPHONE_OUTPUT_VOLUME
+/* Not working */
+#undef FEATURE_AF_PATTERNS
--- platform/6D.113/internals.h
+++ platform/6D.113/internals.h
@@ -125,6 +125,8 @@
 
 /** FPS override: change timers from EVF state */
 #define CONFIG_FPS_UPDATE_FROM_EVF_STATE
+/** Aggressive FPS update **/
+#define CONFIG_FPS_AGGRESSIVE_UPDATE
 
 /** Use the new Rec.709 for YUV-RGB conversion (undefine for Rec.601) */
 #define CONFIG_REC709
