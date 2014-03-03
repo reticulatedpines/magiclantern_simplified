bb43aba77b2965b706d23bf80fbee0ac692ba250
alex <alex@thinkpad>
2014-03-03 19:07:10 +0200

    Moved screenshot code to screenshot.c / screenshot.h

--- src/vram.h
+++ src/vram.h
@@ -25,6 +25,7 @@
  */
 
 #include "compiler.h"
+#include "screenshot.h"
 
 /** Write the VRAM to a BMP file named "A:/test.bmp" */
 extern void
@@ -79,26 +80,6 @@ struct vram_info * get_yuv422_vram();
 struct vram_info * get_yuv422_hd_vram();
 void display_filter_get_buffers(uint32_t** src_buf, uint32_t** dst_buf);
 void vram_clear_lv();
-
-/**
- * Take a screenshot of the BMP overlay and (optionally) the YUV overlay
- * and save it as PPM (a very simple image format).
- * 
- * filename can be:
- * - 0 -> screenshot will be VRAM0.PPM to VRAM9999.PPM
- * - a plain file name, including the PPM extension
- * - a file pattern containing a %d or similar (e.g. "screen%02d.png")
- *
- * also_yuv: if true, also save the LiveView overlay wherever the BMP is transparent.
- * 
- * returns 1 on success, 0 on failure.
- */
-int take_screenshot( char* filename, uint32_t mode );
-
-#define SCREENSHOT_FILENAME_AUTO 0  /* pass it instead of filename => VRAM0.PPM - VRAM9999.PPM in root directory of the ML card */
-#define SCREENSHOT_BMP 1            /* mode flag: save BMP overlays */
-#define SCREENSHOT_YUV 2            /* mode flag: save YUV422 overlays (specify both flags to get them merged) */
-
 
 /* Coordinate transformations between different VRAMs */
 /* http://magiclantern.wikia.com/wiki/VRAM/Geometry */
