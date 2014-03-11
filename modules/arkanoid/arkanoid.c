4cf76304249faa4ab64a4280987ee4d30b0d4957
a1ex <devnull@localhost>
2014-03-11 12:26:29 +0200

    Arkanoid: reworked the state machine and removed the global lock (should fix many subtle bugs)

--- modules/arkanoid/arkanoid.c
+++ modules/arkanoid/arkanoid.c
@@ -22,10 +22,8 @@
 #define ELEM_PRESENT 7
 
 
-static int elem_i;
-#define ELEM_LOOP(code) for(elem_i = 0; elem[elem_i].type != ELEM_END; elem_i++) { element *e = &elem[elem_i]; code; };
-static int elem_c;
-#define ELEM_LOOP2(code) for(elem_c = 0; elem[elem_c].type != ELEM_END; elem_c++) { element *e = &elem[elem_c]; code; };
+#define ELEM_LOOP(code) for(int elem_i = 0; elem[elem_i].type != ELEM_END; elem_i++) { element *e = &elem[elem_i]; code; };
+#define ELEM_LOOP2(code) for(int elem_c = 0; elem[elem_c].type != ELEM_END; elem_c++) { element *e = &elem[elem_c]; code; };
 
 #define COERCE_ABS(i, min, max) if(i < min) { i = (min) - (i - (min)); } if(i > max){ i = (max) - (i - (max)); }
 #define EXCLUDE_RANGE(i, min, max) if(i > min && i < max) { if(i > ((max) - (min)) / 2) i = max; else i = min;}
@@ -33,18 +31,13 @@ static int elem_c;
 #define ARK_IDLE 0
 #define ARK_INRO 1
 #define ARK_LOGO 2
-#define ARK_GAME 3
-
-#define ARK_IS_IDLE (arkanoid_state == ARK_IDLE)
-#define ARK_IS_INRO (arkanoid_state == ARK_INRO)
-#define ARK_IS_LOGO (arkanoid_state == ARK_LOGO)
-#define ARK_IS_GAME (arkanoid_state == ARK_GAME)
+#define ARK_NEW_GAME 3
+#define ARK_GAME_PLAYING 4
 
 static bool arkanoid_running = 0;
-static bool arkanoid_is_busy = 0;  //semaphore
 static bool game_quit;  //semaphore
-static bool game_startup;
 static int arkanoid_state = ARK_IDLE;
+static int arkanoid_next_state = ARK_INRO;
 static int cur_elem = 0;
 static int last_key;
 static int brick_count;
@@ -195,8 +188,6 @@ static element* new_ball(){
     
     e->speed = 3 + rand()%300 / 100.0;
     
-    if(ARK_IS_GAME)ball_count++;
-    
     return e;
 }
 
@@ -216,17 +207,10 @@ static void reset_elems() {
     cur_elem = 0;
 }
 
-
-
-
-
-void arkanoid_game(){
-    if(arkanoid_is_busy)return;
-    arkanoid_is_busy = 1;
-    
+/* state transition, to be called only from arkanoid task */
+void arkanoid_game_init(){
     brick_count = ball_count = 0;
     
-    arkanoid_state = ARK_GAME;
     reset_elems();
     
     element *p = new_elem(ELEM_PAD);
@@ -241,20 +225,17 @@ void arkanoid_game(){
     int i = 1;
     while(i++ <= level) {
         element *e = new_ball();
+        ball_count++;
         e->x = (720 / 2) - (level * (e->w + 5) / 2) + (i * (e->w + 5));
         e->y = p->y - e->h;
         e->speed = 0;
         set_direction(e, -90);
     }
     generate_level();
-    
-    game_startup = 1;
-    arkanoid_is_busy = 0;
 }
 
+/* state transition, to be called only from arkanoid task */
 static void arkanoid_game_start() {
-    game_startup = 0;
-    
     ELEM_LOOP (
         if(elem_i == 0) continue;
         if(e->type!=ELEM_BALL)break;
@@ -296,19 +277,15 @@ static void hit_test(element *a){
             e->speed = 5 + rand()%5;
             
             if(--brick_count == 0) {
-                level++;
-                arkanoid_game();
+                level = MIN(level + 1, 10);
+                arkanoid_next_state = ARK_NEW_GAME;
             }
         }
     )
 }
 
+/* state transition, to be called only from arkanoid task */
 void arkanoid_logo() {
-    if(arkanoid_is_busy)return;
-    arkanoid_is_busy = 1;
-    
-    arkanoid_state = ARK_LOGO;
-    
     //hide text or pad
     elem[0].fade_delta = -5;
     
@@ -350,22 +327,15 @@ void arkanoid_logo() {
         closest->c1 = i;
         closest->speed = 0.2;
     }
-    
-    arkanoid_is_busy = 0;
 }
 
+/* state transition, to be called only from arkanoid task */
 void arkanoid_intro(){
-    if(arkanoid_is_busy)return;
-    arkanoid_is_busy = 1;
-
-    arkanoid_state = ARK_INRO;
     reset_elems();
     
     element *e;
     e = new_elem(ELEM_ML);
     fade(e, 2);
-    
-    arkanoid_is_busy = 0;
 }
 
 
@@ -378,7 +348,7 @@ void arkanoid_intro(){
 
 
 static void ml_ef(element* e){
-    if(!ARK_IS_INRO)return;
+    if(arkanoid_state != ARK_INRO)return;
     
     element* b = new_ball();
     fade(b, 2);
@@ -392,13 +362,13 @@ static void ml_ef(element* e){
 }
 
 static void present_ef(element* e){
-    if(!ARK_IS_INRO)return;
+    if(arkanoid_state != ARK_INRO)return;
     
     element* b = new_ball();
     fade(b, 2);
 
     if(e->fade == 100)e->fade_delta*=-1;
-    if(e->fade == -1)arkanoid_logo();
+    if(e->fade == -1) arkanoid_next_state = ARK_LOGO;
 }
 
 static void ball_coerce(element* e){
@@ -413,7 +383,7 @@ static void ball_ef(element* e){
     float plusX;
     float plusY;
     
-    if(ARK_IS_LOGO && e->c1 != -1) {
+    if(arkanoid_state == ARK_LOGO && e->c1 != -1) {
         plusX = logo_arr[e->c1][0] - e->x;
         plusY = logo_arr[e->c1][1] - e->y;
         e->x += plusX * 0.1;
@@ -439,7 +409,7 @@ static void ball_ef(element* e){
         e->y += e->deltaY;
         ball_coerce(e);
         
-        if(ARK_IS_GAME) {        
+        if(arkanoid_state == ARK_GAME_PLAYING) {        
             x = (int)e->x;
             y = (int)e->y;
             if(ABS(x - last_x) > e->w || ABS(y - last_y) > e->h) {
@@ -450,11 +420,11 @@ static void ball_ef(element* e){
         }
     }
     
-    if(ARK_IS_GAME && e->y > 460) {
+    if(arkanoid_state == ARK_GAME_PLAYING && e->y > 460) {
         e->type = ELEM_NULL;
         if(--ball_count == 0) {
             reset_elems();
-            arkanoid_logo();
+            arkanoid_next_state = ARK_LOGO;
         }
     }
 }
@@ -481,15 +451,37 @@ static void arkanoid_task()
     
     TASK_LOOP
     {
-        if(!gui_menu_shown() || game_quit)break;
-        if(arkanoid_is_busy) goto cont;
-        if(ARK_IS_IDLE)arkanoid_intro();
+        if(!gui_menu_shown() || game_quit)
+        {
+            break;
+        }
+        
+        if (arkanoid_next_state != arkanoid_state)
+        {
+            /* change state */
+            switch (arkanoid_next_state)
+            {
+                case ARK_INRO:
+                    arkanoid_intro();
+                    break;
+                case ARK_LOGO:
+                    arkanoid_logo();
+                    break;
+                case ARK_NEW_GAME:
+                    arkanoid_game_init();
+                    break;
+                case ARK_GAME_PLAYING:
+                    arkanoid_game_start();
+            }
+            arkanoid_state = arkanoid_next_state;
+        }
+        
         
         ELEM_LOOP
         (
             handle_fades(e);
             
-            if((ARK_IS_GAME && game_startup) || last_key == MODULE_KEY_TRASH) continue;
+            if((arkanoid_state == ARK_NEW_GAME) || last_key == MODULE_KEY_TRASH) continue;
             
             switch (e->type) {
                 case ELEM_ML: ml_ef(e); break;
@@ -501,7 +493,6 @@ static void arkanoid_task()
         )
         
         arkanoid_redraw();
-        cont:
         msleep(40);
     }
     
@@ -516,7 +507,11 @@ static MENU_SELECT_FUNC(arkanoid_start) {
 static MENU_SELECT_FUNC(level_select) {
     level += delta;
     level = COERCE(level, 1, 10);
-    if(ARK_IS_GAME) arkanoid_logo();
+    
+    if (arkanoid_state != ARK_IDLE)
+    {
+        arkanoid_next_state = ARK_LOGO;
+    }
 }
 
 static struct menu_entry arkanoid_menu[] =
@@ -551,16 +546,39 @@ unsigned int arkanoid_deinit()
 
 unsigned int arkanoid_keypress(unsigned int key)
 {
-    if(arkanoid_running){
+    if(arkanoid_running)
+    {
         last_key = key;
+        int direction_key_pressed = last_delta();
         
-        if(last_key == MODULE_KEY_PRESS_HALFSHUTTER)game_quit = 1;
-        if(arkanoid_is_busy || game_quit)return 0;
+        if(last_key == MODULE_KEY_PRESS_HALFSHUTTER)
+        {
+            game_quit = 1;
+        }
         
-        if(key == MODULE_KEY_Q || key == MODULE_KEY_PICSTYLE)arkanoid_state = ARK_IDLE;
-        if(ARK_IS_INRO && last_delta())arkanoid_logo();
-        else if(ARK_IS_LOGO && last_delta())arkanoid_game();
-        else if(ARK_IS_GAME && last_delta() && game_startup)arkanoid_game_start();
+        
+        if (key == MODULE_KEY_Q || key == MODULE_KEY_PICSTYLE)
+        {
+            arkanoid_next_state = ARK_IDLE;
+        }
+        else if (direction_key_pressed)
+        {
+            switch (arkanoid_state)
+            {
+                case ARK_INRO:
+                    arkanoid_next_state = ARK_LOGO;
+                    break;
+                case ARK_LOGO:
+                    arkanoid_next_state = ARK_NEW_GAME;
+                    break;
+                case ARK_NEW_GAME:
+                    arkanoid_next_state = ARK_GAME_PLAYING;
+                    break;
+                case ARK_GAME_PLAYING:
+                    /* nothing to do, just keep playing */
+                    break;
+            }
+        }
         
         return 0;
     }
