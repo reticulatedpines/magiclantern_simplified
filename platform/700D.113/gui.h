d3db7ade0d800f871b56970c3f8b9400e506c6f4
Mathew Kelly <dkelly11@tampabay.rr.com>
2014-03-18 12:32:57 +0000

    700D gui.h more indent and spacing cleanups

--- platform/700D.113/gui.h
+++ platform/700D.113/gui.h
@@ -1,4 +1,4 @@
- /* Indented = WRONG */
+    /* Indented = WRONG */
 
 #ifndef _cameraspecific_gui_h_
 #define _cameraspecific_gui_h_
@@ -15,8 +15,8 @@
 
         // button codes as received by gui_main_task
         // need to print those on screen
-        #define BGMT_WHEEL_UP 0
-        #define BGMT_WHEEL_DOWN 1
+    #define BGMT_WHEEL_UP 0
+    #define BGMT_WHEEL_DOWN 1
     #define BGMT_WHEEL_LEFT 2
     #define BGMT_WHEEL_RIGHT 3
     #define BGMT_PRESS_SET 4
@@ -30,20 +30,20 @@
     #define BGMT_LV 0x1E
     #define BGMT_Q 0x1D
 
-    #define BGMT_PRESS_UP 0x28          //~ unpress = 0x2b
+    #define BGMT_PRESS_UP 0x28
     #define BGMT_UNPRESS_UP 0x29
-    #define BGMT_PRESS_LEFT 0x26       //~ unpress = 0x27
+    #define BGMT_PRESS_LEFT 0x26
     #define BGMT_UNPRESS_LEFT 0x27
-    #define BGMT_PRESS_RIGHT 0x24      //~ unpress = 0x29
+    #define BGMT_PRESS_RIGHT 0x24
     #define BGMT_UNPRESS_RIGHT 0x25
-    #define BGMT_PRESS_DOWN 0x2a       //~ unpress = 0x2d
+    #define BGMT_PRESS_DOWN 0x2a
     #define BGMT_UNPRESS_DOWN 0x2b
 
     #define BGMT_PRESS_HALFSHUTTER 0x4e
     #define BGMT_UNPRESS_HALFSHUTTER 0x4f
 
-        #define BGMT_PRESS_FULLSHUTTER 0x50 
-        #define BGMT_UNPRESS_FULLSHUTTER 0x51
+    #define BGMT_PRESS_FULLSHUTTER 0x50 
+    #define BGMT_UNPRESS_FULLSHUTTER 0x51
 
     #define GMT_GUICMD_PRESS_BUTTON_SOMETHING 0x52 // unhandled buttons?
 
@@ -53,8 +53,8 @@
     #define GMT_GUICMD_START_AS_CHECK 95
     #define GMT_GUICMD_OPEN_SLOT_COVER 91
     #define GMT_GUICMD_LOCK_OFF 89
-        
-        //~ not implemented yet
+    
+          //~ not implemented yet
         #define GMT_LOCAL_DIALOG_REFRESH_LV 0x36 // event type = 2, gui code = 0x100000C6 in EOS-M
         #define BGMT_FLASH_MOVIE (0)
         #define BGMT_PRESS_FLASH_MOVIE (0)
