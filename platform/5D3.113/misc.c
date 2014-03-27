50d4750238ffd95540e29cc2f5fab6b763f02204
Giovanni Condello <condellog@gmail.com>
2014-03-27 10:43:23 +0100

    5D3.113 : Remove unused my_memcpy

--- platform/5D3.113/misc.c
+++ platform/5D3.113/misc.c
@@ -1,4 +1,4 @@
-// misc functions specific to 60D/109
+// misc functions specific to 5D3/113
 
 #include <dryos.h>
 #include <property.h>
@@ -12,15 +12,3 @@
 int new_LiveViewApp_handler = 0xff123456;
 void bootflag_write_bootblock(){};
 int handle_af_patterns(struct event * event) { return 1; }
-
-// gcc mempcy has odd alignment issues?
-void
-my_memcpy(
-    void *       dest,
-    const void *     src,
-    size_t          len
-)
-{
-    while( len-- > 0 )
-        *(uint8_t*)dest++ = *(const uint8_t*)src++;
-}
