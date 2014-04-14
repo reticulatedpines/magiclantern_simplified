edff6ac93700fb9348e4fcbeb010d48225beeecd
g3gg0 <devnull@localhost>
2014-04-14 17:09:47 +0200

    cf_acc: added raw sector read/write code, refactored

--- modules/cf_acc/cf_acc.c
+++ modules/cf_acc/cf_acc.c
@@ -5,25 +5,33 @@
 #include <menu.h>
 
 
-#define MEM(x) (*(volatile unsigned int *)(x))
-#define CF_REG_B(x) *(volatile unsigned char *)(0xC0620000 | x)
-#define CF_REG_W(x) *(volatile unsigned short *)(0xC0620000 | x)
-#define CF_REG_D(x) *(volatile unsigned int *)(0xC0620000 | x)
+#define CF_REG_B(x) (*(volatile uint8_t *)     (0xC0620000 | x))
+#define CF_REG_W(x) (*(volatile uint16_t *)    (0xC0620000 | x))
+#define CF_REG_D(x) (*(volatile unsigned int *)(0xC0620000 | x))
 
-#define CF_REG_DATA_IN   0x2000
-#define CF_REG_FEATURE   0x2001
-#define CF_REG_SEC_COUNT 0x2002
-#define CF_REG_SEC_NUM   0x2003
-#define CF_REG_CYL_HI    0x2004
-#define CF_REG_CYL_LO    0x2005
-#define CF_REG_CDH       0x2006
-#define CF_REG_COMMAND   0x2007
+/* compact flash spec registers */
+#define CF_REG_DATA_IN       0x2000
+#define CF_REG_FEATURE       0x2001
+#define CF_REG_SEC_COUNT     0x2002
+#define CF_REG_SEC_NUM       0x2003
+#define CF_REG_CYL_HI        0x2004
+#define CF_REG_CYL_LO        0x2005
+#define CF_REG_CDH           0x2006
+#define CF_REG_COMMAND       0x2007
 
-unsigned char *cf_acc_get_string(unsigned char *buf, unsigned short *data, int chars)
+/* bit 1: set to zero to disable card INT */
+#define CF_REG_IREQ          0x200E
+
+/* set to zero to disable controller interrupts */
+#define CF_REG_CTRL_INT_A    0x8040
+#define CF_REG_CTRL_INT_B    0x8044
+
+/* translate a string from uint16_t format */
+uint8_t *cf_acc_get_string(uint8_t *buf, uint16_t *data, int chars)
 {
     for(int pos = 0; pos < chars; pos++)
     {
-        unsigned char character = (data[pos/2] >> (8*(1-(pos&1)))) & 0xFF;
+        uint8_t character = (data[pos/2] >> (8*(1-(pos&1)))) & 0xFF;
         buf[pos] = character;
     }
     buf[chars] = 0;
@@ -31,23 +39,107 @@ unsigned char *cf_acc_get_string(unsigned char *buf, unsigned short *data, int c
     return buf;
 }
 
+/* read a raw sector in LBA mode */
+uint32_t cf_acc_read_sector(uint32_t sector, uint8_t *data)
+{
+    /* disable card interrupt. not sure if needed. */
+    uint8_t control = CF_REG_B(CF_REG_IREQ);
+    CF_REG_B(CF_REG_IREQ) = (control & ~2);
+    
+    /* disable controller interrupts for this command */
+    CF_REG_D(CF_REG_CTRL_INT_B) = 0;
+    CF_REG_D(CF_REG_CTRL_INT_A) = 0;
+    
+    /* issue READ_SECTOR command */
+    CF_REG_B(CF_REG_CDH) = 0xE0 | ((sector >> 24) & 0x0F);
+    CF_REG_B(CF_REG_CYL_HI) = (sector >> 16) & 0xFF;
+    CF_REG_B(CF_REG_CYL_LO) = (sector >> 8) & 0xFF;
+    CF_REG_B(CF_REG_SEC_NUM) = sector & 0xFF;
+    CF_REG_B(CF_REG_SEC_COUNT) = 1;
+    CF_REG_B(CF_REG_COMMAND) = 0x20;
+    
+    /* wait until card is ready again */
+    while((CF_REG_B(CF_REG_COMMAND) ^ 0x40) & 0xC0)
+    {
+        msleep(20);
+    }
+    
+    uint16_t *word_buf = (uint16_t *)data;
+    
+    /* read received data */
+    for(int word_num = 0; word_num < 256; word_num++)
+    {
+        word_buf[word_num] = CF_REG_W(CF_REG_DATA_IN);
+    }
+    
+    /* reenable card interrupts */
+    CF_REG_B(CF_REG_IREQ) = control;
+    
+    return 0;
+}
+
+/* write a raw sector in LBA mode */
+uint32_t cf_acc_write_sector(uint32_t sector, uint8_t *data)
+{
+    /* disable card interrupt. not sure if needed. */
+    uint8_t control = CF_REG_B(CF_REG_IREQ);
+    CF_REG_B(CF_REG_IREQ) = (control & ~2);
+    
+    /* disable controller interrupts for this command */
+    CF_REG_D(CF_REG_CTRL_INT_B) = 0;
+    CF_REG_D(CF_REG_CTRL_INT_A) = 0;
+    
+    /* issue READ_SECTOR command */
+    CF_REG_B(CF_REG_CDH) = 0xE0 | ((sector >> 24) & 0x0F);
+    CF_REG_B(CF_REG_CYL_HI) = (sector >> 16) & 0xFF;
+    CF_REG_B(CF_REG_CYL_LO) = (sector >> 8) & 0xFF;
+    CF_REG_B(CF_REG_SEC_NUM) = sector & 0xFF;
+    CF_REG_B(CF_REG_SEC_COUNT) = 1;
+    CF_REG_B(CF_REG_COMMAND) = 0x30;
+    
+    /* wait until card is ready */
+    while(CF_REG_B(CF_REG_COMMAND) != 0x58)
+    {
+        msleep(20);
+    }
+    
+    /* write data */
+    uint16_t *word_buf = (uint16_t *)data;
+    
+    for(int word_num = 0; word_num < 256; word_num++)
+    {
+        CF_REG_W(CF_REG_DATA_IN) = word_buf[word_num];
+    }
+    
+    /* wait until card is ready again */
+    while((CF_REG_B(CF_REG_COMMAND) ^ 0x40) & 0xC0)
+    {
+        msleep(20);
+    }
+    
+    /* reenable card interrupts */
+    CF_REG_B(CF_REG_IREQ) = control;
+    
+    return 0;
+}
+
 void cf_acc_read_task()
 {
     int y = 1;
-    unsigned char str_buf[64];
-    unsigned short data_buf[256];
+    uint8_t str_buf[64];
+    uint16_t data_buf[256];
     msleep(1000);
     
     canon_gui_disable_front_buffer();
     clrscr();
     
     /* disable card interrupt. not sure if needed. */
-    unsigned char control = CF_REG_B(0x200E);
-    CF_REG_B(0x200E) = (control & ~2);
+    uint8_t control = CF_REG_B(CF_REG_IREQ);
+    CF_REG_B(CF_REG_IREQ) = (control & ~2);
     
     /* disable controller interrupts for this command */
-    CF_REG_D(0x8044) = 0;
-    CF_REG_D(0x8040) = 0;
+    CF_REG_D(CF_REG_CTRL_INT_B) = 0;
+    CF_REG_D(CF_REG_CTRL_INT_A) = 0;
     
     /* issue IDENTIFY_DRIVE command */
     CF_REG_B(CF_REG_CDH) = 0;
@@ -66,7 +158,7 @@ void cf_acc_read_task()
     }
     
     /* reenable card interrupts */
-    CF_REG_B(0x200E) = control;
+    CF_REG_B(CF_REG_IREQ) = control;
     
     bmp_printf(FONT_MED, 0, font_med.height * y++, "Model:     %s", cf_acc_get_string(str_buf, &data_buf[27], 40));
     bmp_printf(FONT_MED, 0, font_med.height * y++, "Firmware:  %s", cf_acc_get_string(str_buf, &data_buf[23], 8));
@@ -82,6 +174,7 @@ void cf_acc_read_task()
     bmp_printf(FONT_MED, 0, font_med.height * y++, "IDECap:    0x%04X  PCMCIACap: 0x%04X", data_buf[163], data_buf[164]);
     bmp_printf(FONT_MED, 0, font_med.height * y++, "Security:  0x%04X  VendorDat: 0x%04X%04X", data_buf[128], data_buf[158], data_buf[159]);
     bmp_printf(FONT_MED, 0, font_med.height * y++, "Power:     0x%04X  PowerReq:  0x%04X", data_buf[91], data_buf[160]);
+    
     call("dispcheck");
     msleep(3000);
     canon_gui_enable_front_buffer(0);
@@ -125,10 +218,4 @@ MODULE_INFO_START()
     MODULE_INIT(cf_acc_init)
     MODULE_DEINIT(cf_acc_deinit)
 MODULE_INFO_END()
-
-MODULE_STRINGS_START()
-    MODULE_STRING("Author", "g3gg0")
-    MODULE_STRING("License", "GPL")
-    MODULE_STRING("Description", "Direct CF access")
-MODULE_STRINGS_END()
 
