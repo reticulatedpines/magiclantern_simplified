637d8af4cc4d2f121834162e516c679eb64841bc
a1ex <devnull@localhost>
2014-06-25 22:12:21 +0300

    FIO backend: removed aj_create_log_file and refactored what was using it

--- src/fio-ml.c
+++ src/fio-ml.c
@@ -625,50 +625,33 @@ int get_numbered_file_name(const char* pattern, int nmax, char* filename, int ma
     return -1;
 }
 
-static FILE * g_aj_logfile = INVALID_PTR;
-static unsigned int aj_create_log_file( char * name)
-{
-   g_aj_logfile = FIO_CreateFile( name );
-   if ( g_aj_logfile == INVALID_PTR )
-   {
-      bmp_printf( FONT_SMALL, 120, 40, "FCreate: Err %s", name );
-      return( 0 );  // FAILURE
-   }
-   return( 1 );  // SUCCESS
-}
-
-static void aj_close_log_file( void )
-{
-   if (g_aj_logfile == INVALID_PTR)
-      return;
-   FIO_CloseFile( g_aj_logfile );
-   g_aj_logfile = INVALID_PTR;
-}
-
 void dump_seg(void* start, uint32_t size, char* filename)
 {
     DEBUG();
-    aj_create_log_file(filename);
-    FIO_WriteFile( g_aj_logfile, start, size );
-    aj_close_log_file();
+    FILE* f = FIO_CreateFile(filename);
+    if (f)
+    {
+        FIO_WriteFile( f, start, size );
+        FIO_CloseFile(f);
+    }
     DEBUG();
 }
 
 void dump_big_seg(int k, char* filename)
 {
     DEBUG();
-    aj_create_log_file(filename);
-    
-    int i;
-    for (i = 0; i < 16; i++)
+    FILE* f = FIO_CreateFile(filename);
+    if (f)
     {
-        DEBUG();
-        uint32_t start = (k << 28 | i << 24);
-        bmp_printf(FONT_LARGE, 50, 50, "DUMP %x %8x ", i, start);
-        FIO_WriteFile( g_aj_logfile, (const void *) start, 0x1000000 );
+        for (int i = 0; i < 16; i++)
+        {
+            DEBUG();
+            uint32_t start = (k << 28 | i << 24);
+            bmp_printf(FONT_LARGE, 50, 50, "Saving %8x...", start);
+            FIO_WriteFile( f, (const void *) start, 0x1000000 );
+        }
+        FIO_CloseFile(f);
     }
-    
-    aj_close_log_file();
     DEBUG();
 }
 
