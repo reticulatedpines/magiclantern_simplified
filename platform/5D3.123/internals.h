9c7a0b1843d7865978386970c431066d7d208bfc
a1ex <devnull@localhost>
2014-08-06 13:38:51 +0300

    5D3-123: changes from cleanup branch

--- platform/5D3.123/internals.h
+++ platform/5D3.123/internals.h
@@ -154,3 +154,6 @@
 
 /** this method bypasses Canon's lv_save_raw and slurps the raw data directly from connection #0 */
 #define CONFIG_EDMAC_RAW_SLURP
+
+/** Hide Canon bottom bar from DebugMsg hook */
+#define CONFIG_LVAPP_HACK_DEBUGMSG
