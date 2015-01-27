d517d8506954e0671b1c25cf467b6cf664f04ae5
a1ex <devnull@localhost>
2015-01-27 22:48:44 +0200

    600D, 1100D: fix toggle button for restore after format, https://bitbucket.org/hudson/magic-lantern/pull-request/575/restore-after-format-refactoring-fix-for/diff#comment-2677043

--- platform/1100D.105/consts.h
+++ platform/1100D.105/consts.h
@@ -76,7 +76,7 @@
 #define DIALOG_MnCardFormatBegin   (0x12994+4) // ret_CreateDialogBox(...DlgMnCardFormatBegin_handler...) is stored there
 #define DIALOG_MnCardFormatExecute (0x1570C+4) // similar
 #define FORMAT_BTN_NAME "[Q]"
-#define FORMAT_BTN BGMT_Q
+#define FORMAT_BTN BGMT_Q_ALT
 #define FORMAT_STR_LOC 11
 
 #define BULB_MIN_EXPOSURE 1000
