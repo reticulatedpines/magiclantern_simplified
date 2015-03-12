e99866bf04978d78676a9ada8405c4dcc33a820f
a1ex <devnull@localhost>
2015-03-12 23:45:05 +0200

    Moved burn-in tests from Debug menu to a module: selftest.mo

--- src/config-defines.h
+++ src/config-defines.h
@@ -27,9 +27,6 @@
     /** It's a good idea to back up ROM contents on the card - just in case **/
     #define CONFIG_AUTOBACKUP_ROM
 
-    /** It's a good idea to run some automated tests **/
-    #define CONFIG_STRESS_TEST
-
     /** You may want to disable this for troubleshooting **/
     #define CONFIG_CONFIG_FILE
     
