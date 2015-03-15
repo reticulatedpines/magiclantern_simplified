6da96c2fd9e0b84ff7b5b425b65a3c4589901253
a1ex <devnull@localhost>
2015-03-15 11:44:42 +0200

    Minor tweak in bootflags.c

--- src/bootflags.c
+++ src/bootflags.c
@@ -130,7 +130,7 @@ bootflag_write_bootblock( void )
     /* dual card slot */
     int ml_on_cf = (get_ml_card()->drive_letter[0] == 'A');
     extern struct cf_device ** cf_device_ptr[];
-    struct cf_device *  dev = (struct cf_device *) (ml_on_cf ? cf_device_ptr[0][4] : sd_device[1]);
+    struct cf_device * const dev = (struct cf_device *) (ml_on_cf ? cf_device_ptr[0][4] : sd_device[1]);
 #else
     struct cf_device * const dev = (struct cf_device *) sd_device[1];
 #endif
