158b319dc1a9a2cc7fd21ea6b7a029e5c89f05fd
David Milligan <david.l.milligan@gmail.com>
2015-04-05 22:48:36 -0400

    lua.mo: display library

--- modules/lua/lua.c
+++ modules/lua/lua.c
@@ -33,6 +33,7 @@
 #include <shoot.h>
 #include <zebra.h>
 #include <focus.h>
+#include <screenshot.h>
 #include "lua/lauxlib.h"
 #include "lua/lua.h"
 #include "lua/lualib.h"
@@ -455,6 +456,125 @@ const luaL_Reg movielib[] =
 {
     {"start", luaCB_movie_start},
     {"stop", luaCB_movie_stop},
+    {NULL, NULL}
+};
+
+static int luaCB_display_on(lua_State * L)
+{
+    display_on();
+    return 0;
+}
+
+static int luaCB_display_off(lua_State * L)
+{
+    display_off();
+    return 0;
+}
+
+static int luaCB_display_screenshot(lua_State * L)
+{
+    LUA_PARAM_STRING_OPTIONAL(filename, 1, SCREENSHOT_FILENAME_AUTO);
+    LUA_PARAM_INT_OPTIONAL(mode, 2, SCREENSHOT_BMP | SCREENSHOT_YUV);
+    take_screenshot((char*)filename, mode);
+    return 0;
+}
+
+static int luaCB_display_clear(lua_State * L)
+{
+    clrscr();
+    return 0;
+}
+
+static int luaCB_display_print(lua_State * L)
+{
+    LUA_PARAM_STRING(str, 1);
+    LUA_PARAM_INT(x, 2);
+    LUA_PARAM_INT(y, 3);
+    LUA_PARAM_INT_OPTIONAL(font, 4, (int)FONT_MED);
+    LUA_PARAM_INT_OPTIONAL(fg, 5, COLOR_WHITE);
+    LUA_PARAM_INT_OPTIONAL(bg, 6, COLOR_BLACK);
+    bmp_printf(FONT(font, fg, bg), x, y, "%s", str);
+    return 0;
+}
+
+static int luaCB_display_pixel(lua_State * L)
+{
+    LUA_PARAM_INT(x, 1);
+    LUA_PARAM_INT(y, 2);
+    LUA_PARAM_INT(color, 3);
+    bmp_putpixel(x, y, (uint8_t)color);
+    return 0;
+}
+
+static int luaCB_display_line(lua_State * L)
+{
+    LUA_PARAM_INT(x1, 1);
+    LUA_PARAM_INT(y1, 2);
+    LUA_PARAM_INT(x2, 3);
+    LUA_PARAM_INT(y2, 4);
+    LUA_PARAM_INT(color, 5);
+    draw_line(x1, y1, x2, y2, (uint8_t)color);
+    return 0;
+}
+
+static int luaCB_display_rect(lua_State * L)
+{
+    LUA_PARAM_INT(x, 1);
+    LUA_PARAM_INT(y, 2);
+    LUA_PARAM_INT(w, 3);
+    LUA_PARAM_INT(h, 4);
+    LUA_PARAM_INT(stroke, 5);
+    LUA_PARAM_INT_OPTIONAL(fill, 6, -1);
+    if(fill >= 0) bmp_fill((uint8_t)fill, x, y, w, h);
+    if(stroke >= 0) bmp_draw_rect((uint8_t)stroke, x, y, w, h);
+    return 0;
+}
+
+static int luaCB_display_circle(lua_State * L)
+{
+    LUA_PARAM_INT(x, 1);
+    LUA_PARAM_INT(y, 2);
+    LUA_PARAM_INT(r, 3);
+    LUA_PARAM_INT(stroke, 4);
+    LUA_PARAM_INT_OPTIONAL(fill, 5, -1);
+    if(fill >= 0) fill_circle(x, y, r, (uint8_t)fill);
+    if(stroke >= 0) draw_circle(x, y, r, (uint8_t)stroke);
+    return 0;
+}
+
+static int luaCB_display_index(lua_State * L)
+{
+    LUA_PARAM_STRING(key, 2);
+    if(!strcmp(key, "idle")) lua_pushboolean(L, display_idle());
+    else lua_rawget(L, 1);
+    return 1;
+}
+
+static int luaCB_display_newindex(lua_State * L)
+{
+    LUA_PARAM_STRING(key, 2);
+    if(!strcmp(key, "idle"))
+    {
+        lua_pushstring(L, "property is readonly!"); lua_error(L);
+    }
+    else
+    {
+        lua_rawset(L, 1);
+    }
+    return 0;
+}
+
+const luaL_Reg displaylib[] =
+{
+    {"on", luaCB_display_on},
+    {"off", luaCB_display_off},
+    {"screenshot", luaCB_display_screenshot},
+    {"clear", luaCB_display_clear},
+    {"print", luaCB_display_print},
+    {"pixel", luaCB_display_pixel},
+    {"line", luaCB_display_line},
+    {"rect", luaCB_display_rect},
+    {"circle", luaCB_display_circle},
     {NULL, NULL}
 };
 
