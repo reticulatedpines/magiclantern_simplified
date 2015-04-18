18ecff05f51c9b9ec1c7cab3f579acb47c654626
David Milligan <david.l.milligan@gmail.com>
2015-04-18 11:41:17 -0400

    lua.mo: make battery stuff weak so will load on cameras w/o it

--- modules/lua/lua_battery.c
+++ modules/lua/lua_battery.c
@@ -13,24 +13,51 @@
 
 #include "lua_common.h"
 
+#define NOT_AVAILABLE "function not available on this camera"
+extern WEAK_FUNC(ret_0) int GetBatteryLevel();
+extern WEAK_FUNC(ret_0) int GetBatteryHist();
+extern WEAK_FUNC(ret_0) int GetBatteryPerformance();
+extern WEAK_FUNC(ret_0) int GetBatteryTimeRemaining();
+extern WEAK_FUNC(ret_0) int GetBatteryDrainRate();
+
 static int luaCB_battery_index(lua_State * L)
 {
     LUA_PARAM_STRING_OPTIONAL(key, 2, "");
     /// Get battery level in percentage (0-100)
     // @tfield integer level
-    if(!strcmp(key, "level")) lua_pushinteger(L, GetBatteryLevel());
+    if(!strcmp(key, "level"))
+    {
+        if((void*)&GetBatteryLevel == (void*)&ret_0) return luaL_error(L, NOT_AVAILABLE);
+        lua_pushinteger(L, GetBatteryLevel());
+    }
     /// Get battery ID, as registered in Canon menu
     // @tfield integer id
-    else if(!strcmp(key, "id")) lua_pushinteger(L, GetBatteryHist());
+    else if(!strcmp(key, "id"))
+    {
+        if((void*)&GetBatteryHist == (void*)&ret_0) return luaL_error(L, NOT_AVAILABLE);
+        lua_pushinteger(L, GetBatteryHist());
+    }
     /// Get how many "green dots" (3 for a new battery, less for a used battery)
     // @tfield integer performance
-    else if(!strcmp(key, "performance")) lua_pushinteger(L, GetBatteryPerformance());
+    else if(!strcmp(key, "performance"))
+    {
+        if((void*)&GetBatteryPerformance == (void*)&ret_0) return luaL_error(L, NOT_AVAILABLE);
+        lua_pushinteger(L, GetBatteryPerformance());
+    }
     /// Get estimated time remaining
     // @tfield integer time
-    else if(!strcmp(key, "time")) lua_pushinteger(L, GetBatteryTimeRemaining());
+    else if(!strcmp(key, "time"))
+    {
+        if((void*)&GetBatteryTimeRemaining == (void*)&ret_0) return luaL_error(L, NOT_AVAILABLE);
+        lua_pushinteger(L, GetBatteryTimeRemaining());
+    }
     /// Get estimated battery drain rate
     // @tfield integer drain_rate
-    else if(!strcmp(key, "drain_rate")) lua_pushinteger(L, GetBatteryDrainRate());
+    else if(!strcmp(key, "drain_rate"))
+    {
+        if((void*)&GetBatteryDrainRate == (void*)&ret_0) return luaL_error(L, NOT_AVAILABLE);
+        lua_pushinteger(L, GetBatteryDrainRate());
+    }
     else lua_rawget(L, 1);
     return 1;
 }
