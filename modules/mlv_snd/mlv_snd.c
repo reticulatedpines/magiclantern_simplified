8b55ec94664b5277cb27860e9d811f45543b198f
Daniel Fort <Dan@digiola.com>
2015-09-01 12:41:26 -0700

    Reverted changes to mlv_snd.c -- wait until after 70D merge to revisit.

--- modules/mlv_snd/mlv_snd.c
+++ modules/mlv_snd/mlv_snd.c
@@ -53,7 +53,6 @@ extern WEAK_FUNC(ret_0) void audio_configure(int);
 extern WEAK_FUNC(ret_0) int SetAudioVolumeOut(uint32_t);
 extern WEAK_FUNC(ret_0) int SoundDevActiveIn(uint32_t);
 extern WEAK_FUNC(ret_0) int SoundDevShutDownIn();
-extern WEAK_FUNC(ret_0) int StopASIFDMAADC();
 extern void SetSamplingRate(int sample_rate, int channels);
 extern uint64_t get_us_clock_value();
 
@@ -235,7 +234,6 @@ static void mlv_snd_stop()
     }
     
     /* some models may need this */
-    StopASIFDMAADC();
     SoundDevShutDownIn();
     audio_configure(1);
     
