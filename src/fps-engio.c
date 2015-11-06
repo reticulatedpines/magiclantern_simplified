71a9b4f8cb3d857ece675e607886d7597bd4f43d
Daniel Fort <Dan@digiola.com>
2015-11-05 18:05:55 -0800

    Changed to a1ex's suggestions.

--- src/fps-engio.c
+++ src/fps-engio.c
@@ -231,9 +231,11 @@ static void fps_read_current_timer_values();
     #define FPS_TIMER_B_MIN (fps_timer_b_orig - (ZOOM ? 44 : MV720 ? 0 : 70)) /* you can push LiveView until 68fps (timer_b_orig - 50), but good luck recording that */
 #elif defined(CONFIG_EOSM)
     #define TG_FREQ_BASE 32000000
-    #define FPS_TIMER_A_MIN (fps_timer_a_orig - (ZOOM ? 10 : MV720 ? 8 : MV1080CROP ? 14 : 8)) 
+    #define FPS_TIMER_A_MIN (ZOOM ? 666 : MV1080CROP ? 532 : 520)
     #undef FPS_TIMER_B_MIN
-    #define FPS_TIMER_B_MIN (fps_timer_b_orig - (ZOOM ? 290 : MV720 ? 57 : MV1080CROP ? 650 : 57)) // values tested and confirmed here: http://www.magiclantern.fm/forum/index.php?topic=14959.new#new
+    #define FPS_TIMER_B_MIN ( \
+    RECORDING_H264 ? (MV1080CROP ? 1750 : MV720 ? 990 : 1970) \
+                   : (ZOOM || MV1080CROP ? 1336 : 1970))
 #elif defined(CONFIG_6D)
     #define TG_FREQ_BASE 25600000
     #define FPS_TIMER_A_MIN (fps_timer_a_orig - (ZOOM ? 22 : MV720 ? 10 : 34) ) //, ZOOM ? 708 : 512)
