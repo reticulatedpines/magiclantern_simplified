dadd860612ec367a4dfcb173dc00ab71c0feb6ec
a1ex <devnull@localhost>
2016-03-12 01:47:21 +0200

    lens.c: forgot to rename APEX10 macros

--- src/lens.c
+++ src/lens.c
@@ -2112,11 +2112,10 @@ int get_max_ae_ev() { return MAX_AE_EV; }
 #ifdef AE_VALUE
 int get_ae_value() { return AE_VALUE; }
 int get_bv() {
-    return
-        RAW2TV(lens_info.raw_shutter)
-        + RAW2AV(lens_info.raw_aperture)
-        - RAW2SV(lens_info.iso_equiv_raw)
-        + RAW2EC(get_ae_value());
+    return APEX10_RAW2TV(lens_info.raw_shutter)
+         + APEX10_RAW2AV(lens_info.raw_aperture)
+         - APEX10_RAW2SV(lens_info.iso_equiv_raw)
+         + APEX10_RAW2EC(get_ae_value());
 }
 #endif
 #ifdef AE_STATE
