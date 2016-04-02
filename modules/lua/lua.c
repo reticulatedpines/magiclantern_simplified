3082ef1bae9b55f7cf2c104dade0a231d1b81d1f
David Milligan <david.l.milligan@gmail.com>
2016-04-02 13:16:07 -0400

    lua scripting: don't load scripts that start with "_"
    fixes issue with hidden files created by OSX Finder #2496

--- modules/lua/lua.c
+++ modules/lua/lua.c
@@ -538,7 +538,7 @@ static void lua_load_task(int unused)
     {
         do
         {
-            if (!(file.mode & ATTR_DIRECTORY) && (string_ends_with(file.name, ".LUA") || string_ends_with(file.name, ".lua")) && file.name[0] != '.')
+            if (!(file.mode & ATTR_DIRECTORY) && (string_ends_with(file.name, ".LUA") || string_ends_with(file.name, ".lua")) && file.name[0] != '.' && file.name[0] != '_')
             {
                 add_script(file.name);
             }
