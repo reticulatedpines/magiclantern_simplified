6ca2659ca810e69ef2db1513ac995eb9bd2dbe12
tecgen < >
2016-04-13 17:16:08 +0200

    generating model specific icons to exchange the Q icon (only 50D and 5D2)

--- icons/make-icons.py
+++ icons/make-icons.py
@@ -1,5 +1,7 @@
 from PIL import Image
 import os
+import copy
+import time
 
 W = 46
 H = 42
@@ -25,10 +27,6 @@ icons = [
     'script.png',
     'Q-forward.png',
     'Q-back.png',
-    'FUNC-back.png',
-    'FUNC-forward.png',
-    'picstyle-back.png',
-    'picstyle-forward.png',
     'forward.png',
     'modules.png',
     'modified.png',
@@ -51,15 +49,32 @@ icons_lowres = [
     'script.png',
     'Q-forward.png',
     'Q-back.png',
-    'FUNC-back.png',
-    'FUNC-forward.png',
-    'picstyle-back.png',
-    'picstyle-forward.png',
     'forward.png',
     'modules.png',
     'modified.png',
     'games.png',
 ]
+
+# the 50D has no Q button, instead the FUNC button has the same functionality
+icons_50D = copy.deepcopy(icons)
+# exchange only 50D model specific icons
+for n,i in enumerate(icons):
+    if i=='Q-forward.png':
+        icons_50D[n]='FUNC-forward.png'
+
+    if i=='Q-back.png':
+        icons_50D[n]='FUNC-back.png'
+
+
+# the 5D Mark II has no Q button, instead the picture style button has the same functionality
+icons_5D2 = copy.deepcopy(icons)
+# exchange only 5D2 model specific icons
+for n,i in enumerate(icons):
+    if i=='Q-forward.png':
+        icons_5D2[n]='picstyle-forward.png'
+
+    if i=='Q-back.png':
+        icons_5D2[n]='picstyle-back.png'
 
 
 def process(icons, outfile):
@@ -80,6 +95,32 @@ def process(icons, outfile):
     os.system("perl mkfont-canon < ico.in > %s -width %d -height %d" % (outfile, Wpadded, Hpadded))
 
 process(icons, "ico.c")
+process(icons_50D, "ico-50D.c")
+process(icons_5D2, "ico-5D2.c")
 process(icons_lowres, "ico-lowres.c")
 
-os.system("diff -D CONFIG_LOW_RESOLUTION_DISPLAY ico.c ico-lowres.c > ../src/ico.c")
+# make a merged #ifdef format output
+# (see also http://www.gnu.org/software/diffutils/manual/html_node/If_002dthen_002delse.html#If_002dthen_002delse )
+
+# merge 50D specific icons
+os.system("diff -D CONFIG_50D ico.c ico-50D.c > ico-50D-merged.c")
+
+# merge 5D Mark II specific icons
+os.system("diff -D CONFIG_5D2 ico-50D-merged.c ico-5D2.c > ico-model-specific-merged.c")
+
+# merge model specific icons with low resolution versions
+# and store it in the source folder
+os.system("diff -D CONFIG_LOW_RESOLUTION_DISPLAY ico-model-specific-merged.c ico-lowres.c > ../src/ico.c")
+
+# clean up, remove all generated class files
+time.sleep(5)
+os.remove("ico.c")
+os.remove("ico.in")
+os.remove("ico-50d.c")
+os.remove("ico-50D-merged.c")
+os.remove("ico-5D2.c")
+os.remove("ico-model-specific-merged.c")
+os.remove("ico-lowres.c")
+
+
+
