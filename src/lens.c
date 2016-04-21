6eb2b29ffccf5158cb800f5d7b7897f23b4b8f32
alex <alex@thinkpad>
2016-04-21 11:18:42 +0300

    Attempt to fix issue #2431 (turning the focus ring doesn't reset powersave timer; Canon bug?)

--- src/lens.c
+++ src/lens.c
@@ -42,6 +42,7 @@
 #include "picstyle.h"
 #include "focus.h"
 #include "lvinfo.h"
+#include "powersave.h"
 
 // for movie logging
 static char* mvr_logfile_buffer = 0;
@@ -1498,6 +1499,15 @@ static void update_stuff()
     iso_components_update();
 }
 
+static void focus_ring_powersave_fix()
+{
+    if (!RECORDING && is_manual_focus())
+    {
+        /* Canon bug? turning the focus ring doesn't reset powersave timer */
+        powersave_prolong();
+    }
+}
+
 #if defined(CONFIG_EOSM)
 PROP_HANDLER( PROP_LV_FOCAL_DISTANCE )
 {
@@ -1507,6 +1517,7 @@ PROP_HANDLER( PROP_LV_FOCAL_DISTANCE )
     
     idle_wakeup_reset_counters(-11);
     lens_display_set_dirty();
+    focus_ring_powersave_fix();
     
 #ifdef FEATURE_LV_ZOOM_SETTINGS
     zoom_focus_ring_trigger();
@@ -1538,6 +1549,7 @@ PROP_HANDLER( PROP_LV_LENS )
         
         idle_wakeup_reset_counters(-11);
         lens_display_set_dirty();
+        focus_ring_powersave_fix();
         
         #ifdef FEATURE_LV_ZOOM_SETTINGS
         zoom_focus_ring_trigger();
