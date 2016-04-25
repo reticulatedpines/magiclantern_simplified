9c6c81de9db015923d096063dfd65840eacd83a9
tecgen <devnull@localhost>
2016-04-25 17:49:34 +0200

    added camera model to generated code comment in order to help the diff command to recognise the differences between multiple files better

--- icons/make-icons.py
+++ icons/make-icons.py
@@ -1,7 +1,6 @@
 from PIL import Image
 import os
 import copy
-import time
 
 W = 46
 H = 42
@@ -33,6 +32,7 @@ icons = [
     'games.png',
 ]
 
+
 # On low-res screens, the audio and focus icons are aliasing
 icons_lowres = copy.deepcopy(icons)
 # exchange only low resolution icons
@@ -42,6 +42,7 @@ for n,i in enumerate(icons):
 
     if i=='focus.png':
         icons_lowres[n]='focus-lowres.png'
+
 
 # the 50D has no Q button, instead the FUNC button has the same functionality
 icons_50D = copy.deepcopy(icons)
@@ -65,8 +66,9 @@ for n,i in enumerate(icons):
         icons_5D2[n]='picstyle-back.png'
 
 
-def process(icons, outfile):
-    tmp = open("ico.in", "w")
+def process(icons, filename, camera_model):
+    # write icons in textual representation to *.in file
+    tmp = open(filename+".in", "w")
     k = 0
     for ico in icons:
         print >> tmp, '%s = ' % chr(ord('a') + k)
@@ -79,28 +81,28 @@ def process(icons, outfile):
         print >> tmp, ""
 
     tmp.close()
+    # convert *.in files to *.c class files
+    os.system("perl mkfont-canon < %s.in > %s.c -width %d -height %d -camera_model %s" % (filename, filename, Wpadded, Hpadded, camera_model))
 
-    os.system("perl mkfont-canon < ico.in > %s -width %d -height %d" % (outfile, Wpadded, Hpadded))
-
-process(icons, "ico.c")
-process(icons_50D, "ico-50D.c")
-process(icons_5D2, "ico-5D2.c")
-process(icons_lowres, "ico-lowres.c")
+# generate class files from the list of PNG icons
+process(icons, "ico", "ico")
+process(icons_50D, "ico-50D", "50D")
+process(icons_5D2, "ico-5D2", "5D2")
+process(icons_lowres, "ico-lowres", "low")
 
 # make a merged #ifdef format output
 # (see also http://www.gnu.org/software/diffutils/manual/html_node/If_002dthen_002delse.html#If_002dthen_002delse )
 
-# merge icons with low resolution versions
-os.system("diff --ifdef=CONFIG_LOW_RESOLUTION_DISPLAY ico.c ico-lowres.c > ico_with_lowres_merged.c")
+# merge default with 50D specific icons
+os.system("diff --ifdef=CONFIG_50D ico.c ico-50D.c > ico-with-50D.c")
 
-# merge 50D specific icons
-os.system("diff --ifdef=CONFIG_50D ico_with_lowres_merged.c ico-50D.c > ../src/ico.c")
+# merge 50D with 5D2 specific icons
+# copy result to class file
+os.system("diff --ifdef=CONFIG_5D2 ico-with-50D.c ico-5D2.c > ico-with-50D-5D2.c")
+
+# merge icons with low resolution versions
+# copy result to a different class file
+os.system("diff --ifdef=CONFIG_LOW_RESOLUTION_DISPLAY ico-with-50D-5D2.c ico-lowres.c > ../src/ico.c")
 
 # clean up, remove all generated class files
-time.sleep(2)
-os.remove("ico.c")
-os.remove("ico.in")
-os.remove("ico-50d.c")
-os.remove("ico-5D2.c")
-os.remove("ico-lowres.c")
-os.system("rm *.c")
+os.system("rm *.in *.c")
