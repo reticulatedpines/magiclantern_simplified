0e5f90eb4b86cf128d4710b64e539ba6ff0e30cc
alex <alex@thinkpad>
2016-05-20 19:02:00 +0300

    QEMU: fix indentation in serial_flash.c

--- contrib/qemu/eos/serial_flash.c
+++ contrib/qemu/eos/serial_flash.c
@@ -25,72 +25,72 @@ static const char * spi_opname(int code)
 
 SerialFlashState * serial_flash_init(const char * filename, size_t size)
 {
-	// Allocate
-	SerialFlashState * sf = (SerialFlashState*) malloc(sizeof(SerialFlashState));
-	if (sf == NULL) {
-		return NULL;
-	}
+    // Allocate
+    SerialFlashState * sf = (SerialFlashState*) malloc(sizeof(SerialFlashState));
+    if (sf == NULL) {
+        return NULL;
+    }
 
-	// Initialize struct
-	memset(sf,0,sizeof(SerialFlashState));
-	sf->data = (uint8_t*) malloc(size);
-	sf->size = size;
-	memcpy(sf->RDID_seq, (uint8_t[3])RDID_MACRONIX, sizeof(sf->RDID_seq));
-	sf->verbose = 1;
-	if (sf->data == NULL) {
-		free(sf);
-	    fprintf(stderr, "Could not allocate %zd (0x%zX) bytes for serial flash\n", size, size);
-		return NULL;
-	}
+    // Initialize struct
+    memset(sf,0,sizeof(SerialFlashState));
+    sf->data = (uint8_t*) malloc(size);
+    sf->size = size;
+    memcpy(sf->RDID_seq, (uint8_t[3])RDID_MACRONIX, sizeof(sf->RDID_seq));
+    sf->verbose = 1;
+    if (sf->data == NULL) {
+        free(sf);
+        fprintf(stderr, "Could not allocate %zd (0x%zX) bytes for serial flash\n", size, size);
+        return NULL;
+    }
 
-	// Initialize data
-	FILE * f = (filename != NULL) ? fopen(filename, "rb") : NULL;
-	if (f != NULL) {
-	    size_t read_size = fread(sf->data, sizeof(uint8_t), size, f);
-	    if (read_size != size) {
-	        fprintf(stderr, "Could not read %zd (0x%zX) bytes from %s (was %zd)\n", size, size, filename, read_size);
-			memset(sf->data,0,size);
-		}
-	    fclose(f);
-	} else {
-		memset(sf->data,0,size);
-	}
-	
-	return sf;
+    // Initialize data
+    FILE * f = (filename != NULL) ? fopen(filename, "rb") : NULL;
+    if (f != NULL) {
+        size_t read_size = fread(sf->data, sizeof(uint8_t), size, f);
+        if (read_size != size) {
+            fprintf(stderr, "Could not read %zd (0x%zX) bytes from %s (was %zd)\n", size, size, filename, read_size);
+            memset(sf->data,0,size);
+        }
+        fclose(f);
+    } else {
+        memset(sf->data,0,size);
+    }
+    
+    return sf;
 }
 
 void serial_flash_free(SerialFlashState * sf)
 {
-	free(sf->data);
-	free(sf);
+    free(sf->data);
+    free(sf);
 }
 
 void serial_flash_set_CS(SerialFlashState * sf, int value)
 {
-	if (value == 1) {
-		if (sf->verbose) {
-		    if (sf->state == 0x03) { // Read array
-		        printf("[EEPROM]: Verbose: Sent %d bytes\n", sf->rw_count);
-		    } else if (sf->state == 0x07) { // Write array
-		        printf("[EEPROM]: Verbose: Received %d bytes\n", sf->rw_count);
-		    }
-		}
-	    sf->data_pointer = 0xFFFFFFFF;
-	    sf->state = 0;
-	    sf->substate = 0;
-	    sf->read_value = 0;
-	    sf->write_poll = 0;
-	}
+    if (value == 1) {
+        if (sf->verbose) {
+            if (sf->state == 0x03) { // Read array
+                printf("[EEPROM]: Verbose: Sent %d bytes\n", sf->rw_count);
+            } else if (sf->state == 0x07) { // Write array
+                printf("[EEPROM]: Verbose: Received %d bytes\n", sf->rw_count);
+            }
+        }
+        sf->data_pointer = 0xFFFFFFFF;
+        sf->state = 0;
+        sf->substate = 0;
+        sf->read_value = 0;
+        sf->write_poll = 0;
+    }
 
-	if (sf->verbose)
-		printf("[EEPROM]: CS = %d\n", value);
+    if (sf->verbose)
+        printf("[EEPROM]: CS = %d\n", value);
 }
 
 uint8_t serial_flash_write_poll(SerialFlashState * sf)
 {
-	uint8_t ret = (sf->write_poll > 0) ? 1 : 0;
-	if (ret) sf->write_poll--;
-	return ret;
+    uint8_t ret = (sf->write_poll > 0) ? 1 : 0;
+    if (ret) sf->write_poll--;
+    return ret;
 }
 
 uint8_t serial_flash_spi_read(SerialFlashState * sf)
@@ -99,18 +99,18 @@ uint8_t serial_flash_spi_read(SerialFlashState * sf)
     switch (sf->state) {
         case 0x6B: // QOFR: Quad Output Fast Read
         case 0x03: // Read array
-			// printf("A: %X\n",sf->read_value);
-			// printf("B: %X\n",sf->data[sf->data_pointer]);
-			// printf("i: %p[0x%X]\n",sf->data,sf->data_pointer);
+            // printf("A: %X\n",sf->read_value);
+            // printf("B: %X\n",sf->data[sf->data_pointer]);
+            // printf("i: %p[0x%X]\n",sf->data,sf->data_pointer);
             sf->data_pointer++;
-			if (sf->data_pointer >= sf->size)
-            	sf->data_pointer -= sf->size;
+            if (sf->data_pointer >= sf->size)
+                sf->data_pointer -= sf->size;
             sf->read_value = sf->data[sf->data_pointer];
             sf->rw_count++;
             sf->write_poll = 10; // TODO parameter
-			// printf("C: %X\n",sf->read_value);
-			// printf("D: %X\n",sf->data[sf->data_pointer]);
-			// printf("j: %p[0x%X]\n",sf->data,sf->data_pointer);
+            // printf("C: %X\n",sf->read_value);
+            // printf("D: %X\n",sf->data[sf->data_pointer]);
+            // printf("j: %p[0x%X]\n",sf->data,sf->data_pointer);
             break;
 
         case 0x05: // Read status
@@ -126,8 +126,8 @@ uint8_t serial_flash_spi_read(SerialFlashState * sf)
                 sf->state = 0;
                 sf->substate = 0;
             }
-			if (sf->verbose)
-            	printf("[EEPROM]: Verbose: READ in RDID = %02Xh\n", ret);
+            if (sf->verbose)
+                printf("[EEPROM]: Verbose: READ in RDID = %02Xh\n", ret);
             break;
 
         default:
@@ -147,42 +147,42 @@ void serial_flash_spi_write(SerialFlashState * sf, uint8_t value)
         switch (value)
         {
             case 0x01: // WRSR: Write Status Register
-				if (sf->verbose)
-					printf("[EEPROM]: Verbose: [SR] << ...\n");
+                if (sf->verbose)
+                    printf("[EEPROM]: Verbose: [SR] << ...\n");
                 sf->state = 0x01;
                 sf->status_register = 0;
                 break;
 
             case 0x05: // RDSR: Read Status Register
-				if (sf->verbose)
-                	printf("[EEPROM]: Verbose: [SR] >> 0x%X\n", sf->status_register);
+                if (sf->verbose)
+                    printf("[EEPROM]: Verbose: [SR] >> 0x%X\n", sf->status_register);
                 sf->read_value = sf->status_register;
                 sf->state = 0x05;
                 break;
 
             case 0x08: // LPWP: Low Power Write Poll
-				if (sf->verbose)
-                	printf("[EEPROM]: Verbose: Write Poll\n");
+                if (sf->verbose)
+                    printf("[EEPROM]: Verbose: Write Poll\n");
                 // Pretend to use some time...
                 sf->read_value = (sf->write_poll > 0) ? 1 : 0;
                 if (sf->read_value) sf->write_poll--;
                 break;
 
             case 0x06: // WREN: Set Write Enable Latch
-				if (sf->verbose)
-                	printf("[EEPROM]: Verbose: Set Write Enable Latch\n");
+                if (sf->verbose)
+                    printf("[EEPROM]: Verbose: Set Write Enable Latch\n");
                 sf->status_register |= (1 << 1); // Set WEL bit
                 break;
 
             case 0x04: // WRDI: Reset Write Enable Latch
-				if (sf->verbose)
-                	printf("[EEPROM]: Verbose: Reset Write Enable Latch\n");
+                if (sf->verbose)
+                    printf("[EEPROM]: Verbose: Reset Write Enable Latch\n");
                 sf->status_register &= ~(1 << 1); // Unset WEL bit
                 break;
 
             case 0x9f: // RDID: Read identification
-				if (sf->verbose)
-                	printf("[EEPROM]: Verbose: Got RDID\n");
+                if (sf->verbose)
+                    printf("[EEPROM]: Verbose: Got RDID\n");
                 sf->read_value = sf->RDID_seq[0];
                 sf->state = 0x9f;
                 sf->substate = 0;
@@ -194,8 +194,8 @@ void serial_flash_spi_write(SerialFlashState * sf, uint8_t value)
             case 0x02: // WRITE: Write byte to Memory
             case 0x07: // WRITE: Write array to Memory
             case 0x6B: // QOFR: Quad Output Fast Read
-				if (sf->verbose)
-                	printf("[EEPROM]: Verbose: Got %s (%02Xh)\n", spi_opname(value), value);
+                if (sf->verbose)
+                    printf("[EEPROM]: Verbose: Got %s (%02Xh)\n", spi_opname(value), value);
                 sf->state = value;
                 sf->substate = 0;
                 sf->data_pointer = 0;
@@ -212,14 +212,14 @@ void serial_flash_spi_write(SerialFlashState * sf, uint8_t value)
 
     // If non-standby state
 
-	// WRSR: Write Status Register
+    // WRSR: Write Status Register
     if (sf->state == 0x01) {
         sf->status_register = value;
         sf->state = 0;
         sf->substate = 0;
-		if (sf->verbose)
-        	printf("[EEPROM]: Verbose: [SR] << 0x%02X\n", value);
-		return;
+        if (sf->verbose)
+            printf("[EEPROM]: Verbose: [SR] << 0x%02X\n", value);
+        return;
     }
 
     // Byte write
@@ -227,19 +227,19 @@ void serial_flash_spi_write(SerialFlashState * sf, uint8_t value)
         sf->data[sf->data_pointer] = value;
         sf->state = 0;
         sf->substate = 0;
-		if (sf->verbose)
-        	printf("[EEPROM]: Verbose: Wrote byte 0x%02X @ 0x%06X\n", value, sf->data_pointer);
-		return;
+        if (sf->verbose)
+            printf("[EEPROM]: Verbose: Wrote byte 0x%02X @ 0x%06X\n", value, sf->data_pointer);
+        return;
     }
 
     // Array write
     if (sf->state == 0x07 && sf->substate == 3) {
         sf->data[sf->data_pointer] = value;
-		if (sf->verbose)
-        	printf("[EEPROM]: Verbose: Wrote array byte 0x%02X @ 0x%06X\n", value, sf->data_pointer);
+        if (sf->verbose)
+            printf("[EEPROM]: Verbose: Wrote array byte 0x%02X @ 0x%06X\n", value, sf->data_pointer);
         sf->data_pointer = (sf->data_pointer+1) % sf->size;
         sf->rw_count++;
-		return;
+        return;
     }
 
     // Address read
@@ -247,14 +247,14 @@ void serial_flash_spi_write(SerialFlashState * sf, uint8_t value)
         sf->data_pointer |= (value << (8*(2 - sf->substate)));
         sf->substate++;
         if (sf->substate == 3) {
-			if (sf->verbose)
-            	printf("[EEPROM]: Verbose: address is now: 0x%06X\n", sf->data_pointer);
+            if (sf->verbose)
+                printf("[EEPROM]: Verbose: address is now: 0x%06X\n", sf->data_pointer);
             if (sf->state == 0x03) {
                 sf->read_value = sf->data[sf->data_pointer];
             }
         }
         sf->write_poll = 10;
-		return;
+        return;
     }
 
     // Otherwise invalid
