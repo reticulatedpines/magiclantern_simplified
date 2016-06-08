83291e4126250b1932b1c26935f6a181f2bd9061
alex <alex@thinkpad>
2016-06-08 11:45:51 +0300

    QEMU: link mpu_send lines with "Complete WaitID" lines (updated 60D and 70D, comments only)

--- contrib/qemu/scripts/extract_init_spells.py
+++ contrib/qemu/scripts/extract_init_spells.py
@@ -21,6 +21,7 @@ num = 1
 num2 = 1
 
 first_send = True
+waitid_prop = None
 
 q_output = ""
 
@@ -39,8 +40,15 @@ for l in f.readlines():
             if first_block: first_block = False
             else: q_output += "        { 0 } } }, {" + "\n"
             
+            if waitid_prop:
+                assert spell.strip().startswith("08 06 00 00 ")
+                q_output += "    %-60s/* spell #%d, Complete WaitID = %s */" % (format_spell(spell) + " {", num, waitid_prop) + "\n"
+                waitid_prop = None
+                continue
+            
             if comment: q_output += "    %-60s/* spell #%d */" % (format_spell(spell) + " {", num) + "\n"
             else:       q_output += "    " + format_spell(spell) + " {" + "\n"
+            continue
 
     m = re.match(".* mpu_recv\(([^()]*)\)", l)
     if m:
@@ -55,6 +63,19 @@ for l in f.readlines():
         if comment: print "        %-56s/* reply #%d.%d */" % (format_spell(spell), num-1, num2)
         else: print "        " + format_spell(spell)
         num2 += 1
+        continue
+    
+    # after a Complete WaitID line, the ICU sends to the MPU a message saying it's ready
+    # so the MPU can then send data for the next property that requires a "Complete WaitID"
+    # (if those are not synced, you will get ERROR TWICE ACK REQUEST)
+    # example:
+    #    PropMgr:ff31ec3c:01:03: Complete WaitID = 0x80020000, 0xFF178514(0)
+    #    PropMgr:00c5c318:00:00: *** mpu_send(08 06 00 00 04 00 00), from 616c
+    # the countdown at the end of the line must be 0
+
+    m = re.match(".*Complete WaitID = ([0-9A-Fx]+), ([0-9A-Fx]+)\(0\)", l)
+    if m:
+        waitid_prop = m.groups()[0]
 
 print "    { 0 } } }"
 print "};"
