83ebf544fcbe6e44b1b5db4b0f20d63e08c84131
alex <alex@thinkpad>
2016-07-06 14:04:52 +0300

    QEMU, MPU spells: recognize button codes from gui.h (comments only)

--- contrib/qemu/eos/mpu_spells/extract_init_spells.py
+++ contrib/qemu/eos/mpu_spells/extract_init_spells.py
@@ -3,6 +3,7 @@
 #
 # Very rough proof of concept, far from complete. Tested on 60D.
 
+from __future__ import print_function
 import os, sys, re
 
 first_mpu_send_only = False
@@ -19,7 +20,7 @@ f = open(log_fullpath, "r")
 [log_path, log_filename] = os.path.split(log_fullpath)
 model = log_filename[:log_filename.index("-")]
 
-print "struct mpu_init_spell mpu_init_spells_%s[] = { {" % model
+print("struct mpu_init_spell mpu_init_spells_%s[] = { {" % model)
 first_block = True
 num = 0
 num2 = 0
@@ -27,7 +28,53 @@ num2 = 0
 first_send = True
 waitid_prop = None
 
-for l in f.readlines():
+lines = f.readlines()
+
+bind_switches = {}
+last_bind_switch = None
+
+for l in lines:
+    # match bindReceiveSwitch with GUI_Control, both from MainCtrl task
+    # note: GUI_Control messages can be sent from other tasks
+    m = re.match(" +MainCtrl:.*bindReceiveSwitch *\(([^()]*)\)", l)
+    if m:
+        args = m.groups()[0].split(",")
+        args = tuple([int(a) for a in args])
+        last_bind_switch = args
+        continue
+    m = re.match(" +MainCtrl:.*GUI_Control:([0-9]+) +0x([0-9])+", l)
+    if m and last_bind_switch is not None:
+        arg1 = int(m.groups()[0])
+        arg2 = int(m.groups()[1],16)
+        if (arg1,arg2) in bind_switches:
+            assert(bind_switches[last_bind_switch] == (arg1,arg2))
+        else:
+            bind_switches[last_bind_switch] = arg1,arg2
+
+def get_switch_names(camera_model):
+    switch_names = {}
+    ml_dir = "../../../../../magic-lantern/platform/"
+    
+    # pick one platform directory for this camera (any firmware version)
+    cam_dir = [d for d in os.listdir(ml_dir) 
+                 if camera_model in d
+                 and os.path.isfile(os.path.join(ml_dir, d, "gui.h"))
+              ][0]
+    
+    gui_h = open(os.path.join(ml_dir, cam_dir, "gui.h")).readlines()
+    for l in gui_h:
+        m = re.match(" *#define +([A-Z0-9_]+) +([0-9a-fA-Fx]+)", l)
+        if m:
+            btn_name = m.groups()[0]
+            btn_code = m.groups()[1]
+            try: btn_code = int(btn_code,16) if btn_code.startswith("0x") else int(btn_code)
+            except: continue
+            switch_names[btn_code] = btn_name
+    return switch_names
+    
+switch_names = get_switch_names(model)
+
+for l in lines:
     m = re.match(".* mpu_send\(([^()]*)\)", l)
     if m:
         spell = m.groups()[0].strip()
@@ -40,34 +87,39 @@ for l in f.readlines():
             num2 = 0
             
             if first_block: first_block = False
-            else: print "        { 0 } } }, {"
+            else: print("        { 0 } } }, {")
             
             if waitid_prop:
                 assert spell.startswith("08 06 00 00 ")
-                print "    %-60s/* spell #%d, Complete WaitID = %s */" % (format_spell(spell) + " {", num, waitid_prop)
+                print("    %-60s/* spell #%d, Complete WaitID = %s */" % (format_spell(spell) + " {", num, waitid_prop))
                 waitid_prop = None
                 continue
             
             if spell.startswith("06 05 04 00 "):
                 arg = int(spell.split(" ")[4], 16)
-                print "    %-60s/* spell #%d, NotifyGUIEvent(%d) */" % (format_spell(spell) + " {", num, arg)
+                print("    %-60s/* spell #%d, NotifyGUIEvent(%d) */" % (format_spell(spell) + " {", num, arg))
                 continue
             
-            print "    %-60s/* spell #%d */" % (format_spell(spell) + " {", num)
+            print("    %-60s/* spell #%d */" % (format_spell(spell) + " {", num))
             continue
 
     m = re.match(".* mpu_recv\(([^()]*)\)", l)
     if m:
         spell = m.groups()[0].strip()
         num2 += 1
-
+        print("        %-56s/* reply #%d.%d" % (format_spell(spell), num, num2), end="")
+        
         if spell.startswith("06 05 06 "):
             args = spell.split(" ")[3:5]
-            args = [int(a,16) for a in args]
-            print "        %-56s/* reply #%d.%d, bindReceiveSwitch(%d, %d) */" % (format_spell(spell), num, num2, args[0], args[1])
-            continue
+            args = tuple([int(a,16) for a in args])
+            if args in bind_switches:
+                btn_code = bind_switches[args][0]
+                if btn_code in switch_names:
+                    print(", %s" % switch_names[btn_code], end="")
+                print(", GUI_Control:%d" % btn_code, end="")
+            print(", bindReceiveSwitch(%d, %d)" % (args[0], args[1]), end="")
 
-        print "        %-56s/* reply #%d.%d */" % (format_spell(spell), num, num2)
+        print(" */")
         continue
     
     # after a Complete WaitID line, the ICU sends to the MPU a message saying it's ready
@@ -82,5 +134,5 @@ for l in f.readlines():
     if m:
         waitid_prop = m.groups()[0]
 
-print "    { 0 } } }"
-print "};"
+print("    { 0 } } }")
+print("};")
