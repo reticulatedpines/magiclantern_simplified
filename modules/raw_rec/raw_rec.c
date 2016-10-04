27cc2e17523e226b01a6d97f3a9529eca5ba4f6e
mlnikfreak@gmail.com <mlnikreak@gmail.com>
2016-10-04 17:10:37 +0200

    fix faulty version numberings

--- modules/raw_rec/raw_rec.c
+++ modules/raw_rec/raw_rec.c
@@ -2119,7 +2119,7 @@ static unsigned int raw_rec_init()
     cam_650d  = is_camera("650D", "1.0.4");
     cam_7d    = is_camera("7D",   "2.0.3");
     cam_700d  = is_camera("700D", "1.1.4");
-    cam_60d   = is_camera("60D",  "1.1.1");
+    cam_60d   = is_camera("60D",  "1.0.1");
     cam_100d  = is_camera("100D", "1.1.1");
     cam_500d  = is_camera("500D", "1.1.1");
     
