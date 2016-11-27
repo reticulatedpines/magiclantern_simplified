b8fa330210abb56abb323adfe60c1a20ea7e7e1f
alex <alex@thinkpad>
2016-11-27 22:53:51 +0200

    QEMU: DCIM test appears to work on 450D as well ... given enough retries :)

--- contrib/qemu/tests/run_tests.sh
+++ contrib/qemu/tests/run_tests.sh
@@ -164,9 +164,10 @@ done
 echo
 echo "Testing file I/O (DCIM directory)..."
 # Most EOS cameras should be able to create the DCIM directory if missing.
-# Currently works only on models that can boot Canon GUI, and also on 100D.
+# Currently works only on models that can boot Canon GUI,
+# and also on 100D and 450D.
 #for CAM in ${EOS_CAMS[*]}; do
-for CAM in ${GUI_CAMS[*]} 100D; do
+for CAM in ${GUI_CAMS[*]} 100D 450D; do
     printf "%5s: " $CAM
     
     mkdir -p tests/$CAM/
