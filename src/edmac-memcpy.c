cb00acd0f4db9a8adff221227c423d4cf560a9f9
Daniel Fort <Dan@digiola.com>
2016-12-11 12:09:15 -0800

    700D: enable CONFIG_EDMAC_RAW_SLURP

--- src/edmac-memcpy.c
+++ src/edmac-memcpy.c
@@ -351,6 +351,10 @@ uint32_t raw_write_chan = 0x4;  /* 0x12 gives corrupted frames on 1.2.3, http://
 uint32_t raw_write_chan = 0x12; /* 60D and newer, including all DIGIC V */
 #endif
 
+#ifdef CONFIG_700D
+uint32_t raw_write_chan = 0x12;
+#endif
+
 #ifdef CONFIG_EOSM
 uint32_t raw_write_chan = 0x12;
 #endif
