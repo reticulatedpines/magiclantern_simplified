0aef7e27b995ed3c00e540650720b02d45b0e81e
bouncyball <devnull@localhost>
2017-01-10 12:18:45 +0400

    camera_id.h: fallback to unique/default name in case of the model w/o localized name

--- modules/mlv_rec/camera_id.h
+++ modules/mlv_rec/camera_id.h
@@ -29,34 +29,34 @@ typedef struct {
 } camera_id_t;
 
 static const camera_id_t camera_id[] = {
-	{ 0x80000213, {"Canon EOS 5D", "", ""} },
-	{ 0x80000218, {"Canon EOS 5D Mark II", "", ""} },
-	{ 0x80000250, {"Canon EOS 7D", "", ""} },
+	{ 0x80000213, {"Canon EOS 5D", NULL, NULL} },
+	{ 0x80000218, {"Canon EOS 5D Mark II", NULL, NULL} },
+	{ 0x80000250, {"Canon EOS 7D", NULL, NULL} },
 	{ 0x80000252, {"Canon EOS 500D", "Canon EOS Rebel T1i", "Canon EOS Kiss X3"} },
 	{ 0x80000254, {"Canon EOS 1000D", "Canon EOS Rebel XS", "Canon EOS Kiss F"} },
-	{ 0x80000261, {"Canon EOS 50D", "", ""} },
+	{ 0x80000261, {"Canon EOS 50D", NULL, NULL} },
 	{ 0x80000270, {"Canon EOS 550D", "Canon EOS Rebel T2i", "Canon EOS Kiss X4"} },
-	{ 0x80000285, {"Canon EOS 5D Mark III", "", ""} },
+	{ 0x80000285, {"Canon EOS 5D Mark III", NULL, NULL} },
 	{ 0x80000286, {"Canon EOS 600D", "Canon EOS Rebel T3i", "Canon EOS Kiss X5"} },
-	{ 0x80000287, {"Canon EOS 60D", "", ""} },
+	{ 0x80000287, {"Canon EOS 60D", NULL, NULL} },
 	{ 0x80000288, {"Canon EOS 1100D", "Canon EOS Rebel T3", "Canon EOS Kiss X50"} },
-	{ 0x80000289, {"Canon EOS 7D Mark II", "", ""} },
+	{ 0x80000289, {"Canon EOS 7D Mark II", NULL, NULL} },
 	{ 0x80000301, {"Canon EOS 650D", "Canon EOS Rebel T4i", "Canon EOS Kiss X6i"} },
-	{ 0x80000302, {"Canon EOS 6D", "", ""} },
-	{ 0x80000325, {"Canon EOS 70D", "", ""} },
+	{ 0x80000302, {"Canon EOS 6D", NULL, NULL} },
+	{ 0x80000325, {"Canon EOS 70D", NULL, NULL} },
 	{ 0x80000326, {"Canon EOS 700D", "Canon EOS Rebel T5i", "Canon EOS Kiss X7i"} },
 	{ 0x80000327, {"Canon EOS 1200D", "Canon EOS Rebel T5", "Canon EOS Kiss X70"} },
-	{ 0x80000331, {"Canon EOS M", "", ""} },
+	{ 0x80000331, {"Canon EOS M", NULL, NULL} },
 	{ 0x80000346, {"Canon EOS 100D", "Canon EOS Rebel SL1", "Canon EOS Kiss X7"} },
 	{ 0x80000347, {"Canon EOS 760D", "Canon EOS Rebel T6s", "Canon EOS 8000D"} },
-	{ 0x80000349, {"Canon EOS 5D Mark IV", "", ""} },
-	{ 0x80000350, {"Canon EOS 80D", "", ""} },
-	{ 0x80000355, {"Canon EOS M2", "", ""} },
-	{ 0x80000382, {"Canon EOS 5DS", "", ""} },
+	{ 0x80000349, {"Canon EOS 5D Mark IV", NULL, NULL} },
+	{ 0x80000350, {"Canon EOS 80D", NULL, NULL} },
+	{ 0x80000355, {"Canon EOS M2", NULL, NULL} },
+	{ 0x80000382, {"Canon EOS 5DS", NULL, NULL} },
 	{ 0x80000393, {"Canon EOS 750D", "Canon EOS Rebel T6i", "Canon EOS Kiss X8i"} },
-	{ 0x80000401, {"Canon EOS 5DS R", "", ""} },
+	{ 0x80000401, {"Canon EOS 5DS R", NULL, NULL} },
 	{ 0x80000404, {"Canon EOS 1300D", "Canon EOS Rebel T6", "Canon EOS Kiss X80"} },
-	{ 0x00000000, {"", "", ""} }
+	{ 0x00000000, {NULL, NULL, NULL} }
 };
 
 /* Get camera name by model ID and name type (UNIQ, LOC1, LOC2) */ 
@@ -69,7 +69,10 @@ static const char * get_camera_name_by_id(unsigned long model_id, int name_type)
 	{
 		if (camera_id[i].cameraModel == model_id)
 		{
-			camName = camera_id[i].cameraName[name_type];
+			if(!(camName = camera_id[i].cameraName[name_type]))
+			{
+				camName = camera_id[i].cameraName[UNIQ];
+			}
 		}
 		i++;
 	}
