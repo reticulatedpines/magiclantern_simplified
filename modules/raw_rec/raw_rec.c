82956578a698e753634781c149b3715c5dce0b4c
Daniel Fort <Dan@digiola.com>
2017-02-07 17:32:44 -0800

    update to firmware revision 700D.115

--- modules/raw_rec/raw_rec.c
+++ modules/raw_rec/raw_rec.c
@@ -1113,7 +1113,7 @@ static void hack_liveview(int unhack)
             cam_650d ? 0xFF527E38 :
             cam_6d   ? 0xFF52C684 :
             cam_eos_m ? 0xFF539C1C :
-            cam_700d ? 0xFF52BB60 :
+            cam_700d ? 0xFF6CF064 :
             cam_7d  ? 0xFF345788 :
             cam_60d ? 0xff36fa3c :
             cam_500d ? 0xFF2ABEF8 :
@@ -2109,7 +2109,7 @@ static unsigned int raw_rec_init()
     cam_600d  = is_camera("600D", "1.0.2");
     cam_650d  = is_camera("650D", "1.0.4");
     cam_7d    = is_camera("7D",   "2.0.3");
-    cam_700d  = is_camera("700D", "1.1.4");
+    cam_700d  = is_camera("700D", "1.1.5");
     cam_60d   = is_camera("60D",  "1.1.1");
     cam_500d  = is_camera("500D", "1.1.1");
 
