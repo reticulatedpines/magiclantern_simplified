d94a16742c7d39a84329dfca3be45b3c8b2479b8
alex <alex@thinkpad>
2017-03-11 23:47:27 +0200

    lua_key: re-enabled yielding during key.wait() using the same workaround as task.yield()

--- modules/lua/lua_key.c
+++ modules/lua/lua_key.c
@@ -31,7 +31,7 @@ static int luaCB_key_press(lua_State * L)
  Wait for a key to be pressed.
  
  FIXME: while waiting for a key to be pressed,
- other tasks or event handlers from the same script cannot run.
+ a task.yield() is performed, with identical limitations.
  
  @tparam[opt] constants.KEY key
  @tparam[opt] int timeout
@@ -51,7 +51,7 @@ static int luaCB_key_wait(lua_State * L)
         return luaL_error(L, "FIXME: cannot use task.yield() or key.wait() from two tasks");
     }
 
-    //lua_give_semaphore(L, NULL);
+    lua_give_semaphore(L, NULL);
 
     //TODO: probably better to use a semaphore
     while((key && last_keypress != key) || (!key && !last_keypress))
@@ -59,12 +59,12 @@ static int luaCB_key_wait(lua_State * L)
         msleep(100);
         if(timeout && time++ > timeout)
         {
-            //lua_take_semaphore(L, 0, NULL);
+            lua_take_semaphore(L, 0, NULL);
             lua_pushinteger(L, 0);
             return 1;
         }
     }
-    //lua_take_semaphore(L, 0, NULL);
+    lua_take_semaphore(L, 0, NULL);
     lua_pushinteger(L, last_keypress);
     return 1;
 }
