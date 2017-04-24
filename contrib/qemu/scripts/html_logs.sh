8df70e53f5430b6911bfd0d9c83e73577b1fad56
alex <alex@thinkpad>
2017-04-24 20:21:18 +0300

    QEMU: save HTML logs to subdirectory

--- contrib/qemu/scripts/html_logs.sh
+++ contrib/qemu/scripts/html_logs.sh
@@ -4,7 +4,12 @@
 # Usage: ./html_logs.sh *.html
 
 for f in $*; do
-  echo "$f -> $f.html"
-  python ansi_cleanup.py $f | ansi2html > $f.html;
-  perl -i -pe 'BEGIN{undef $/;} s/<style.*style>/<link href="ansi.css" rel="stylesheet" type="text\/css"\/>/smg' $f.html
+  html=html/$f.html
+  html_dir=`dirname $html`
+  css=`realpath --relative-to=$html_dir html/ansi.css`
+  echo "$f -> $html"
+  mkdir -p $html_dir
+  python ansi_cleanup.py $f | ansi2html > $html
+  perl -i -pe 'BEGIN{undef $/;} s/<style.*style>/<link href="'"$css"'" rel="stylesheet" type="text\/css"\/>/smg' $html
 done
+cp ansi.css html/
