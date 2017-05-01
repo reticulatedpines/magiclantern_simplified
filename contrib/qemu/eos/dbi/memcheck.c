9f5e8829ab36eaa6e254b8468a3b67e9811d6274
alex <alex@thinkpad>
2017-05-01 15:01:37 +0300

    QEMU memchk: moved stubs in a data structure, for easier porting

--- contrib/qemu/eos/dbi/memcheck.c
+++ contrib/qemu/eos/dbi/memcheck.c
@@ -11,6 +11,39 @@
 #include "../model_list.h"
 #include "memcheck.h"
 
+/* hardcoded; find it with:
+ * readelf magiclantern -a | grep " memcpy$" */
+static const int ml_memcpy = 0x0974c0;
+static const int ml_memcpy_size = 0x96B50 - 0x969EC;
+
+/* our own read-after-free, in __mem_free */
+/* if wrong: lots of warning messages containing f12eeeed during ML module loading */
+/* not needed if MEMCHECK_CHECK is undefined */
+static const int ml_double_free_check = 0x500b0;
+
+struct memcheck_stubs
+{
+    /* any malloc/free-like routines */
+    uint32_t malloc[4];
+    uint32_t free[4];
+
+    /* any memcpy-like routines */
+    uint32_t memcpy[4];
+    uint32_t memcpy_end[4];
+
+    /* routines for initializing a memory heap */
+    uint32_t init_heap;
+    uint32_t checked_heaps[4];
+
+    /* routine for allocating from a heap */
+    uint32_t heap_alloc;
+    uint32_t heap_free;
+
+    /* allow these routines to use-after-free */
+    uint32_t heap_routines[8][2];
+};
+
+static struct memcheck_stubs stubs;
 
 /* 2 bits for each address */
 /* ull because it's shifted and used against a 64-bit value */
@@ -102,46 +135,34 @@ static void copy_mem_status(uint32_t src, uint32_t dst, uint32_t size)
 static int is_heap_routine(EOSState *s, uint32_t pc)
 {
     /* malloc/free are allowed to use after free */
-    /* todo: identify stubs in a way that does not require hardcoding? */
 
-    if (strcmp(s->model->name, "500D") == 0)
+    for (int i = 0; i < COUNT(stubs.heap_routines); i++)
     {
-        switch (pc)
+        assert(stubs.heap_routines[i][0] <= stubs.heap_routines[i][1]);
+
+        if (pc >= stubs.heap_routines[i][0] &&
+            pc <= stubs.heap_routines[i][1])
         {
-            case 0xff06a4f4 ... 0xFF06B5CC: /* AllocateMemory */
-            case 0xFF018F70 ... 0xFF019110: /* malloc */
-
-            /* not sure whether these are false warnings or not */
-            case 0xff06c1c0:    /* PackMem? */
-            case 0xff06c15c:    /* PackMem get size? */
-            case 0xff06c184:    /* PackMem get size? */
-            case 0xFF06CC4C:    /* GetNextMemoryChunk?! why? */
-            case 0xff06beec:    /* DeleteMemorySuite?! why? */
-
-            /* our own read-after-free, in __mem_free: */
-            case 0x500b0:
-
-                /* all these are allowed to read/write
-                 * unallocated or uninitialized memory */
-                return 1;
+            return 1;
         }
+
     }
-    else
+
+    if (pc == ml_double_free_check)
     {
-        fprintf(stderr, "FIXME: no heap stubs for %s.\n", s->model->name);
+        return 1;
     }
 
     return 0;
 }
 
-/* hardcoded; find it with:
- * readelf magiclantern -a | grep " memcpy$" */
-static const int ml_memcpy = 0x0974c0;
-static const int ml_memcpy_size = 0x96B50 - 0x969EC;
-
 static int is_memcpy(EOSState *s, uint32_t pc)
 {
-    if (pc >= ml_memcpy && pc <= ml_memcpy + ml_memcpy_size)
+    if ((pc >= ml_memcpy && pc <= ml_memcpy + ml_memcpy_size) ||
+        (pc >= stubs.memcpy[0] && pc <= stubs.memcpy_end[0])  ||
+        (pc >= stubs.memcpy[1] && pc <= stubs.memcpy_end[1])  ||
+        (pc >= stubs.memcpy[2] && pc <= stubs.memcpy_end[2])  ||
+        (pc >= stubs.memcpy[3] && pc <= stubs.memcpy_end[3]))
     {
         return 1;
     }
@@ -168,7 +189,7 @@ void eos_memcheck_log_mem(EOSState *s, hwaddr addr, uint64_t value, uint32_t siz
         );
     }
 
-    if (0 && addr < 0x1000 && CURRENT_CPU->env.regs[15] > 0x1000)
+    if (0 && addr < 0x1000 && pc > 0x1000)
     {
         printf(KLRED"[%s:%x:%x] %x %s TCM (%x)"KRESET"\n",
             eos_get_current_task_name(s), pc, lr,
@@ -228,6 +249,9 @@ void eos_memcheck_log_exec(EOSState *s, uint32_t pc)
     if (prev_pc == pc) return;
     prev_pc = pc;
 
+    /* our uninitialized stubs are 0 - don't log this address */
+    if (!pc) return;
+
     /* allow a few multi-tasked calls */
     int id = (malloc_lr[0] == 0) ? 0 :
              (malloc_lr[1] == 0) ? 1 :
@@ -236,8 +260,7 @@ void eos_memcheck_log_exec(EOSState *s, uint32_t pc)
                                   -1 ;
     assert(id >= 0);
 
-    //~ if (pc == 0xFF0C8178 || pc == 0x99A0)
-    if (pc == 0xFF018F70 || pc == 0xFF06ACB4)
+    if (pc == stubs.malloc[0] || pc == stubs.malloc[1] || pc == stubs.malloc[2] || pc == stubs.malloc[3])
     {
         assert(malloc_lr[id] == 0);
         malloc_lr[id] = CURRENT_CPU->env.regs[14];
@@ -269,8 +292,7 @@ void eos_memcheck_log_exec(EOSState *s, uint32_t pc)
         }
     }
 
-    //~ if (pc == 0xFF0C8144 || pc == 0x9D3C)
-    if (pc == 0xFF019044 || pc == 0xFF06B044)
+    if (pc == stubs.free[0] || pc == stubs.free[1] || pc == stubs.free[2] || pc == stubs.free[3])
     {
         int free_ptr = CURRENT_CPU->env.regs[0];
         qemu_log_mask(EOS_LOG_VERBOSE, "free %x ", free_ptr);
@@ -289,7 +311,7 @@ void eos_memcheck_log_exec(EOSState *s, uint32_t pc)
         }
     }
 
-    if (pc == ml_memcpy || pc == 0xFF3EBBC4) /* memcpy */
+    if (pc == stubs.memcpy[0] || pc == stubs.memcpy[1] || pc == stubs.memcpy[2] || pc == stubs.memcpy[3])
     {
         int dst = CURRENT_CPU->env.regs[0];
         int src = CURRENT_CPU->env.regs[1];
@@ -303,15 +325,18 @@ void eos_memcheck_log_exec(EOSState *s, uint32_t pc)
         copy_mem_status(src, dst, num);
     }
 
-    if (pc == 0xFF06A4CC)   /* init_heap */
+    if (pc == stubs.init_heap)   /* init_heap */
     {
         int start = CURRENT_CPU->env.regs[0];
         int size  = CURRENT_CPU->env.regs[1];
         fprintf(stderr, "init_heap %x %x\n", start, start+size);
 
-        if (start == 0x300000 && start+size == 0xd00000)
+        if (start == stubs.checked_heaps[0] ||
+            start == stubs.checked_heaps[1] ||
+            start == stubs.checked_heaps[2] ||
+            start == stubs.checked_heaps[3])
         {
-            fprintf(stderr, "AllocMem heap\n");
+            fprintf(stderr, "Checking this heap.\n");
             mem_set_status(start, start+size, MS_FREED | MS_NOINIT);
         }
     }
@@ -322,4 +347,30 @@ void eos_memcheck_init(EOSState *s)
     printf("Marking all memory as uninitialized...\n");
     mem_set_status(0, s->model->ram_size, MS_NOINIT);
     /* fixme: also check both TCMs */
+
+    /* todo: identify stubs in a way that does not require hardcoding? */
+    if (strcmp(s->model->name, "500D") == 0)
+    {
+        stubs = (struct memcheck_stubs) {
+            .malloc         = { 0xFF018F70, 0xFF06ACB4 },
+            .free           = { 0xFF019044, 0xFF06B044 },
+            .memcpy         = { 0xFF3EBBC4 },
+            .memcpy_end     = { 0xFF3EBC2C },
+            .init_heap      =   0xFF06A4CC,
+            .checked_heaps  = { 0x300000 },
+
+            .heap_routines  = {
+                                { 0xff06a4f4, 0xFF06B5CC }, /* AllocateMemory */
+                                { 0xFF018F70, 0xFF019110 }, /* malloc */
+                                { 0xff06c1c0, 0xFF06C1EC }, /* PackMem? */
+                                { 0xff06c15c, 0xFF06C188 }, /* PackMem get size? */
+                                { 0xFF06CC4C, 0xFF06CCA8 }, /* GetNextMemoryChunk?! why? */
+                                { 0xff06beec, 0xFF06BF98 }, /* DeleteMemorySuite?! why? */
+                              }, 
+        };
+    }
+    else
+    {
+        fprintf(stderr, "FIXME: no memcheck stubs for %s.\n", s->model->name);
+    }
 }
