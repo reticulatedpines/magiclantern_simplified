79dbe6f90910d14b8c94ee2a4d4ca36a5627bc62
alex <alex@thinkpad>
2017-05-01 11:01:30 +0300

    QEMU: compile with code coverage (gcov); helper script for lcov

--- contrib/qemu/scripts/configure_eos.sh
+++ contrib/qemu/scripts/configure_eos.sh
@@ -4,6 +4,7 @@
 # gcc 4 uses gnu90 by default, but we use C99 code
 # passing C99 to g++ gives warning, but QEMU treats all warnings as errors
 # QEMU_CPPFLAGS is derived from QEMU_CFLAGS in ./configure
+# gcov is enabled by default (no obvious difference in emulation speed)
 CC="gcc --std=gnu99" \
     ./configure --target-list=arm-softmmu --disable-docs --enable-sdl \
-    --extra-cflags="-Wno-error=deprecated-declarations" $*
+    --extra-cflags="-Wno-error=deprecated-declarations --coverage" $*
