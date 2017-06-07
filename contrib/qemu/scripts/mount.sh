23fafd8a474b76819fae7fdc7c1ce00961e823b8
Daniel Fort <Dan@digiola.com>
2017-06-06 22:12:35 -0700

    Use hdiutil on OS X systems.

--- contrib/qemu/scripts/mount.sh
+++ contrib/qemu/scripts/mount.sh
@@ -1,11 +1,20 @@
+#!/usr/bin/env bash
 
-echo "This will mount sd.img and cf.img as loopback, using kpartx."
+echo "This will mount sd.img and cf.img as a loopback device."
 echo "Please enter your password (of course, after reviewing what this script does)."
-sudo kpartx -av sd.img
-sudo kpartx -av cf.img
 
-
-echo "Done."
-echo "To remove the device mappings, run:"
-echo "   sudo kpartx -dv sd.img"
-echo "   sudo kpartx -dv cf.img"
+if [ $(uname -s) == "Darwin" ]; then
+    sudo hdiutil attach sd.img
+    sudo hdiutil attach cf.img
+    echo "Done."
+    echo "To remove the device mappings, run:"
+    echo '   sudo hdiutil detach "/Volumes/EOS_DIGITAL"'
+    echo '   sudo hdiutil detach "/Volumes/EOS_DIGITAL 1"'
+else
+    sudo kpartx -av sd.img
+    sudo kpartx -av cf.img
+    echo "Done."
+    echo "To remove the device mappings, run:"
+    echo "   sudo kpartx -dv sd.img"
+    echo "   sudo kpartx -dv cf.img"
+fi
