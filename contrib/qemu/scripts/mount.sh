408b66185dafe572665a94ea12a2ac547d7cf511
Daniel Fort <Dan@digiola.com>
2017-06-10 15:58:44 -0700

    OS X doesn’t require root permissions to mount disk image.

--- contrib/qemu/scripts/mount.sh
+++ contrib/qemu/scripts/mount.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 echo "This will mount sd.img and cf.img as a loopback device."
-echo "Please enter your password (of course, after reviewing what this script does)."
 
 if [ $(uname -s) == "Darwin" ]; then
-    sudo hdiutil attach sd.img
-    sudo hdiutil attach cf.img
+    hdiutil attach sd.img
+    hdiutil attach cf.img
     echo "Done."
     echo "To remove the device mappings, run:"
-    echo '   sudo hdiutil detach "/Volumes/EOS_DIGITAL"'
-    echo '   sudo hdiutil detach "/Volumes/EOS_DIGITAL 1"'
+    echo '   hdiutil detach "/Volumes/EOS_DIGITAL"'
+    echo '   hdiutil detach "/Volumes/EOS_DIGITAL 1"'
 else
+    echo "Please enter your password (of course, after reviewing what this script does)."
     sudo kpartx -av sd.img
     sudo kpartx -av cf.img
     echo "Done."
