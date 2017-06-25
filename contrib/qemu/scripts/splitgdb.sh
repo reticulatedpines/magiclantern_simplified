e17c2231dfc1fc8329ff23766f83b64c6955dacf
alex <alex@thinkpad>
2017-06-25 19:51:20 +0300

    QEMU: updated splitgdb.sh as discussed on EOS M2 thread http://www.magiclantern.fm/forum/index.php?topic=15895.msg186245#msg186245

--- contrib/qemu/scripts/splitgdb.sh
+++ contrib/qemu/scripts/splitgdb.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-tmux new-session -d "./run_canon_fw.sh $1 -S -s"
-tmux split-window -h "arm-none-eabi-gdb -x gdbopts -x $1/debugmsg.gdb"
-tmux attach-session -d 
+CAM=${1//,*/}
+if [ ! "$CAM" ]; then
+    echo usage: same arguments as with run_canon_fw.sh
+    echo example: ./splitgdb.sh 60D,firmware="boot=1" -d callstack
+    exit
+fi
 
+tmux new-session -d "./run_canon_fw.sh $* -s -S"
+tmux split-window -h "cgdb -d arm-none-eabi-gdb -x $CAM/debugmsg.gdb"
+tmux attach-session -d
