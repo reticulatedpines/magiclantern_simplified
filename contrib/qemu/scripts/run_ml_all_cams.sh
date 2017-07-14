8f13c3c0166cf9593ac3fd7a25527c5e015081f6
alex <alex@thinkpad>
2017-07-14 15:08:13 +0300

    run_ml_all_cams: fix copying zip to SD/CF image

--- contrib/qemu/scripts/run_ml_all_cams.sh
+++ contrib/qemu/scripts/run_ml_all_cams.sh
@@ -86,8 +86,10 @@ for CAM_DIR in $ML_PLATFORMS; do
             mkdir qemu-tmp
             unzip -q $CAM_DIR/magiclantern-qemu.zip -d qemu-tmp
             cd ../../qemu/
-            mcopy -o -s -i $MSD ../magic-lantern/platform/qemu-tmp/ ::
-            mcopy -o -s -i $MCF ../magic-lantern/platform/qemu-tmp/ ::
+            mcopy -o -i $MSD ../magic-lantern/platform/qemu-tmp/* ::
+            mcopy -o -i $MCF ../magic-lantern/platform/qemu-tmp/* ::
+            mcopy -o -s -i $MSD ../magic-lantern/platform/qemu-tmp/ML/ ::
+            mcopy -o -s -i $MCF ../magic-lantern/platform/qemu-tmp/ML/ ::
             rm -rf qemu-tmp/
         fi
 
