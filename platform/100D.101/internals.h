87367eb2d5929d4afefd0cccc351ad5695cd311f
alex <alex@thinkpad>
2017-07-24 14:00:07 +0300

    100D: enabled CONFIG_EDMAC_RAW_SLURP

--- platform/100D.101/internals.h
+++ platform/100D.101/internals.h
@@ -126,6 +126,6 @@
 #define CONFIG_LVAPP_HACK_DEBUGMSG
 
 /** this method bypasses Canon's lv_save_raw and slurps the raw data directly from connection #0 */
-//~ #define CONFIG_EDMAC_RAW_SLURP
+#define CONFIG_EDMAC_RAW_SLURP
 
 #define CONFIG_MENU_TIMEOUT_FIX
