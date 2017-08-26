7d7435fe92e72bfd9e8ae4148de6f8cd4a90c7e8
alex <alex@thinkpad>
2017-08-26 11:04:43 +0300

    cropmarks: minor boundary fix

--- src/cropmarks.c
+++ src/cropmarks.c
@@ -565,9 +565,9 @@ static void FAST default_movie_cropmarks()
 void set_movie_cropmarks(int x, int y, int w, int h)
 {
     x = COERCE(x, os.x0+1, os.x_max-1);
-    y = COERCE(y, os.y0+1, os.y_max-1);
-    w = COERCE(w, 0, os.x_max-1 - x);
-    h = COERCE(h, 0, os.y_max-1 - y);
+    y = COERCE(y, os.y0+2, os.y_max-2);
+    w = COERCE(w, 0, os.x_max-2 - x);
+    h = COERCE(h, 0, os.y_max-4 - y);
     cropmarks_x = (x << 16) | (x + w);
     cropmarks_y = (y << 16) | (y + h);
 }
