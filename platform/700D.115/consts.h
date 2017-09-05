f5471519ceacf889b6b9c5db6f40f6a37c47b265
Daniel Fort <Dan@digiola.com>
2017-09-04 22:33:49 -0700

    Found SRM_BUFFER_SIZE for EOSM and 700D

--- platform/700D.115/consts.h
+++ platform/700D.115/consts.h
@@ -234,6 +234,7 @@
     // see "Malloc Information"
 #define MALLOC_STRUCT 0x4b908
 #define MALLOC_FREE_MEMORY (MEM(MALLOC_STRUCT + 8) - MEM(MALLOC_STRUCT + 0x1C)) // "Total Size" - "Allocated Size"
+#define SRM_BUFFER_SIZE 0x1F24000   /* print it from srm_malloc_cbr */
 
 //TODO: Check if this hack works again or not :(
 #define UNAVI_BASE (0x41948)
--- platform/EOSM.202/consts.h
+++ platform/EOSM.202/consts.h
@@ -190,6 +190,7 @@
 // see "Malloc Information"
 #define MALLOC_STRUCT 0x668C8
 #define MALLOC_FREE_MEMORY (MEM(MALLOC_STRUCT + 8) - MEM(MALLOC_STRUCT + 0x1C)) // "Total Size" - "Allocated Size"
+#define SRM_BUFFER_SIZE 0x1F24000   /* print it from srm_malloc_cbr */
 
 #define UNAVI (MEM(0x5D408) == 2) // Find with Mem Browser // dec CancelUnaviFeedBackTimer
 #define SCROLLHACK (MEM(0x5D43C) != 0) //-450
