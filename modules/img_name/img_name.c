a6b613080ca006cc0bcfb2b591374df100c4bca7
alex <alex@thinkpad>
2017-11-27 17:38:52 +0200

    img_name: save the user-set prefix in config file

--- modules/img_name/img_name.c
+++ modules/img_name/img_name.c
@@ -6,12 +6,16 @@
 #include <bmp.h>
 #include <menu.h>
 #include <fileprefix.h>
+#include <config.h>
 
 #include "../ime_base/ime_base.h"
 
 static char default_prefix[8];
 static char prefix[8];
 static int key = -1;
+
+/* prefix has 4 characters - encode it as 32-bit integer */
+static CONFIG_INT("image.prefix", prefix_enc, 0);
 
 static IME_DONE_FUNC(file_prefix_enter_done)
 {
@@ -36,6 +40,14 @@ static IME_DONE_FUNC(file_prefix_enter_done)
         if (!streq(prefix, default_prefix))
         {
             key = file_prefix_set(prefix);
+
+            /* update encoded prefix (this goes into config file) */
+            prefix_enc = *(int*)prefix;
+        }
+        else
+        {
+            /* disable user-set prefix */
+            prefix_enc = 0;
         }
     }
     return IME_OK;
@@ -95,14 +107,28 @@ static struct menu_entry img_name_menu[] =
         .update     = file_prefix_upd,
         .icon_type  = IT_SUBMENU,
         .help       = "Custom image file prefix (e.g. IMG_1234.JPG -> ABCD1234.JPG).",
-        .help2      = "Setting not persistent. Might conflict with Dual ISO prefixes.",
+        .help2      = "Might conflict with Dual ISO prefixes (to be tested).",
     }
 };
 
 static unsigned int img_name_init()
 {
+    /* get default prefix at startup */
+    /* fixme: what if user changes it from Canon menu? */
     snprintf(default_prefix, sizeof(default_prefix), "%s", get_file_prefix());
+
+    /* set image prefix from config file, if any saved */
+    if (prefix_enc)
+    {
+        /* note: CONFIG_INT's are of type int, which is int32_t on our target */
+        /* fixme: compile time assert to check sizeof(int) */
+        *(int*)prefix = prefix_enc; prefix[4] = 0;
+        key = file_prefix_set(prefix);
+    }
+
+    /* setup menus */
     menu_add("Shoot", img_name_menu, COUNT(img_name_menu));
+
     return 0;
 }
 
@@ -115,3 +141,7 @@ MODULE_INFO_START()
     MODULE_INIT(img_name_init)
     MODULE_DEINIT(img_name_deinit)
 MODULE_INFO_END()
+
+MODULE_CONFIGS_START()
+    MODULE_CONFIG(prefix_enc)
+MODULE_CONFIGS_END()
