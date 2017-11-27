4adb3ed4ddfbf0ae164200f7a9e0567d7e213fed
alex <alex@thinkpad>
2017-11-27 17:09:41 +0200

    img_name: misc small fixes - no strcpy - warn if IME modules are not loaded - fix changing the fourth character - fill with _ if user enters less than 4 characters - moved to Shoot menu (it only applies to still pictures) - menu appearance, help update - no symbols exported - fix warnings

--- modules/img_name/img_name.c
+++ modules/img_name/img_name.c
@@ -12,27 +12,43 @@
 static char prefix[8];
 static int key = -1;
 
-IME_DONE_FUNC(file_prefix_enter_done)
+static IME_DONE_FUNC(file_prefix_enter_done)
 {
-    if(status == IME_OK) {
-        if(key != -1 && key != 0) file_prefix_reset(key);
+    if (status == IME_OK)
+    {
+        /* if user entered less than 4 characters, fill with _ */
+        prefix[4] = 0;
+        int len = strlen(prefix);
+        for (int i = len; i < 4; i++)
+        {
+            prefix[i] = '_';
+        }
+
+        /* any previously set prefix? */
+        if (key != -1 && key != 0)
+        {
+            file_prefix_reset(key);
+        }
+
+        /* set the new prefix */
         key = file_prefix_set(prefix);
     }
     return IME_OK;
 }
 
-IME_UPDATE_FUNC(file_prefix_enter_upd)
+static IME_UPDATE_FUNC(file_prefix_enter_upd)
 {
     return IME_OK;
 }
 
 static MENU_SELECT_FUNC(file_prefix_enter)
 {
-    strcpy(prefix, get_file_prefix());
+    snprintf(prefix, sizeof(prefix), "%s", get_file_prefix());
+
     ime_base_start(
-        (unsigned char *)"Enter file prefix",
-        (unsigned char *)prefix,
-        4,
+        "Enter file prefix",
+        prefix,
+        5,
         IME_UTF8,
         IME_CHARSET_FILENAME,
         file_prefix_enter_upd,
@@ -45,28 +61,40 @@ static MENU_UPDATE_FUNC(file_prefix_upd)
 {
     MENU_SET_VALUE("%s", get_file_prefix());
     
-    if(key == 0)MENU_SET_RINFO("FAILED");
-    else MENU_SET_RINFO("");
+    if (key == 0)
+    {
+        MENU_SET_RINFO("FAILED");
+    }
+    else if (key != -1)
+    {
+        MENU_SET_ENABLED(1);
+    }
+
+    if ((void *) &ime_base_start == (void *) &ret_0)
+    {
+        MENU_SET_WARNING(MENU_WARN_NOT_WORKING, "Please load the IME modules (ime_base.mo and one ime_*.mo)");
+    }
 }
 
-static struct menu_entry ime_base_menu[] =
+static struct menu_entry img_name_menu[] =
 {
     {
-        .name = "Image file prefix",
-        .select = file_prefix_enter,
-        .update = file_prefix_upd,
-        .help = "Custom image file prefix (e.g. IMG_1234.JPG -> ABCD1234.JPG).",
-        .help2 = "There can be some conflict with Dual ISO prefixes.",
+        .name       = "Image file prefix",
+        .select     = file_prefix_enter,
+        .update     = file_prefix_upd,
+        .icon_type  = IT_SUBMENU,
+        .help       = "Custom image file prefix (e.g. IMG_1234.JPG -> ABCD1234.JPG).",
+        .help2      = "Setting not persistent. Might conflict with Dual ISO prefixes.",
     }
 };
 
-unsigned int img_name_init()
+static unsigned int img_name_init()
 {
-    menu_add("Prefs", ime_base_menu, COUNT(ime_base_menu));
+    menu_add("Shoot", img_name_menu, COUNT(img_name_menu));
     return 0;
 }
 
-unsigned int img_name_deinit()
+static unsigned int img_name_deinit()
 {
     return 0;
 }
