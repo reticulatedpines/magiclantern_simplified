9d50dc6a0837c474f8e6eaec8ef0e4ecfedf998a
g3gg0 <devnull@localhost>
2017-12-19 18:59:36 +0100

    mlv_rec: properly fill lensSerial with a hex representation of the serial number in lens_info struct

--- modules/mlv_rec/mlv.c
+++ modules/mlv_rec/mlv.c
@@ -55,8 +55,11 @@ void mlv_fill_lens(mlv_lens_hdr_t *hdr, uint64_t start_timestamp)
     hdr->autofocusMode = af_mode;
     hdr->flags = 0;
 
+    char buf[33];
+    snprintf(buf, sizeof(buf), "%X%08X", (uint32_t) (lens_info.lens_serial >> 32), (uint32_t)(lens_info.lens_serial & 0xFFFFFFFF));
+    
     strncpy((char *)hdr->lensName, lens_info.name, 32);
-    strncpy((char *)hdr->lensSerial, "", 32);
+    strncpy((char *)hdr->lensSerial, buf, 32);
 }
 
 void mlv_fill_wbal(mlv_wbal_hdr_t *hdr, uint64_t start_timestamp)
