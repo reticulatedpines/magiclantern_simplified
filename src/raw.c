0709c5a7036e1465860cd5968d43d16c78aafa9f
alex <alex@thinkpad>
2018-02-02 12:22:41 +0100

    Raw backend: DEFAULT_RAW_BUFFER_SIZE for 700D and EOSM (thanks dfort)

--- src/raw.c
+++ src/raw.c
@@ -122,10 +122,12 @@ static int (*dual_iso_get_dr_improvement)() = MODULE_FUNCTION(dual_iso_get_dr_im
 
 #ifdef CONFIG_700D
 #define DEFAULT_RAW_BUFFER MEM(0x25B0C + 0x3C)
+#define DEFAULT_RAW_BUFFER_SIZE (0x47F00000 - 0x46798080)
 #endif
 
 #ifdef CONFIG_EOSM
 #define DEFAULT_RAW_BUFFER MEM(0x404E4 + 0x44)
+#define DEFAULT_RAW_BUFFER_SIZE (0x47F00000 - 0x46798080)
 #endif
 
 #ifdef CONFIG_6D
