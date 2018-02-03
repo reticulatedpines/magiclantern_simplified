c6643059814836c528b29031f5d8812ddb28e2b7
alex <alex@thinkpad>
2018-02-03 13:32:57 +0100

    Menu: some rough support for sumbenus where the initial field enables the entire group - the initial field should have the same .priv field as the master entry, and .select = menu_open_submenu - prevent graying out the field used for enabling, even if it gives the truth value for the master menu entry - do not print value by default for submenus (we already draw a forward sign)

--- src/menu.c
+++ src/menu.c
@@ -2316,9 +2316,10 @@ static int check_default_warnings(struct menu_entry * entry, char* warning)
     /* all submenu entries depend on the master entry, if any */
     if (IS_SUBMENU(entry->parent_menu))
     {
-        if (entry->parent && IS_ML_PTR(entry->parent->priv))
+        if (entry->parent && IS_ML_PTR(entry->parent->priv) &&  /* does it have a parent with a valid priv field? */
+            entry->parent->priv != entry->priv)         /* priv different from our own? (cannot depend on itself) */
         {
-            if (!MENU_INT(entry->parent))
+            if (!MENU_INT(entry->parent))   /* is the master menu entry disabled? if so, gray out the entire submenu */
             {
                 int is_plural = entry->parent->name[strlen(entry->parent->name)-1] == 's';
                 snprintf(warning, MENU_MAX_WARNING_LEN, "%s %s disabled.", entry->parent->name, is_plural ? "are" : "is");
@@ -2459,7 +2460,9 @@ entry_default_display_info(
         STR_APPEND(value, "%s", entry->choices[SELECTED_INDEX(entry)]);
     }
 
-    else if (IS_ML_PTR(entry->priv) && entry->select != run_in_separate_task)
+    else if (IS_ML_PTR(entry->priv) &&
+            entry->icon_type != IT_ACTION &&    /* no default value for actions */
+            entry->icon_type != IT_SUBMENU)     /* no default value for submenus */
     {
         if (entry->min == 0 && entry->max == 1)
         {
