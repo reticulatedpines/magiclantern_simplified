e2d275ae3613403d76aaf6a0266ebaa551c6833a
alex <alex@thinkpad>
2018-02-17 13:38:07 +0100

    MPU spells: script to annotate logs with mpu_send/recv events (known MPU messages moved to known_spells.py)

---
+++ contrib/qemu/eos/mpu_spells/annotate_mpu_log.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python2
+
+import os, sys, re
+from known_spells import known_spells
+
+if len(sys.argv) <= 2:
+    print "This script looks for mpu_send/mpu_recv entries in a log file, and adds comments"
+    print "MPU messages are read from known_spells.py; button codes are parsed from button_codes.h"
+    print "Similar script for ADTG/CMOS/ENGIO registers:"
+    print "https://bitbucket.org/hudson/magic-lantern/src/iso-research/modules/adtg_gui/annotate_log.py" 
+    print
+    print "Usage: python annotate_mpu_log.py 5D3 input.log > output.log"
+    raise SystemExit
+
+# identify button codes for the requested camera model
+buttons = {}
+lines = open("button_codes.h").readlines()
+first = next(i for i,l in enumerate(lines) if "_" + sys.argv[1] + "[" in l) + 1
+last  = next(i for i,l in enumerate(lines[first:]) if "END_OF_LIST" in l) + first
+print >> sys.stderr, lines[first-1].strip()
+lines = lines[first:last]
+
+# parse button codes and names
+for l in lines:
+    m = re.match(".*\[(.*)\] += 0x(....)", l)
+    assert m
+    print >> sys.stderr, "  ", m.groups()[1], m.groups()[0]
+    buttons[m.groups()[1].upper()] = m.groups()[0]
+print >> sys.stderr, "}"
+
+# input log file
+lines = open(sys.argv[2]).readlines()
+lines = [l.strip("\n") for l in lines]
+
+# annotate MPU events
+for l in lines:
+    comments = []
+
+    m = re.match(".* mpu_(recv|send)\(([^()]*)\)", l)   
+    if m:
+        spell = m.groups()[1].strip()
+        if spell[6:11] in known_spells:
+            metadata = known_spells[spell[6:11]]
+            description = metadata[1]
+            if description == "Complete WaitID":
+                if spell[12:17] in known_spells:
+                    description += " = " + known_spells[spell[12:17]][1]
+            comments.append(description)
+
+        if spell[6:8] == "06":
+            btn_code = spell[9:14].replace(" ", "").upper()
+            if btn_code in buttons:
+                comments.append(buttons[btn_code])
+
+    if comments:
+        out = "%-100s ; %s" % (l, "; ".join(comments))
+        print out
+        print >> sys.stderr, out
+    else:
+        print l
