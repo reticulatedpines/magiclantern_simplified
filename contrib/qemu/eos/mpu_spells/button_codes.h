329511398c25107a2f0e5fc17ac16be1d01328a1
alex <alex@thinkpad>
2018-05-03 23:31:24 +0200

    QEMU: fix Q button code for 1200D
    - https://www.magiclantern.fm/forum/index.php?topic=12627.msg200889#msg200889
    - assuming the same applies to 1300D; unsure about 600D/1100D

--- contrib/qemu/eos/mpu_spells/button_codes.h
+++ contrib/qemu/eos/mpu_spells/button_codes.h
@@ -67,6 +67,41 @@ static int button_codes_1100D[] = {
     [BGMT_END_OF_LIST]                  = 0x0000
 };
 
+static int button_codes_1200D[] = {
+    [BGMT_INFO]                         = 0x0101,
+    [BGMT_ISO]                          = 0x0B01,
+    [BGMT_LV]                           = 0x2101,
+    [BGMT_MENU]                         = 0x0001,
+    [BGMT_PLAY]                         = 0x0301,
+    [BGMT_PRESS_AV]                     = 0x1C01,
+    [BGMT_PRESS_DISP]                   = 0x2501,
+    [BGMT_PRESS_DOWN]                   = 0x1901,
+    [BGMT_PRESS_LEFT]                   = 0x1B01,
+    [BGMT_PRESS_RIGHT]                  = 0x1A01,
+    [BGMT_PRESS_SET]                    = 0x0C01,
+    [BGMT_PRESS_UP]                     = 0x1801,
+    [BGMT_PRESS_ZOOM_IN]                = 0x0901,
+    [BGMT_PRESS_ZOOM_OUT]               = 0x0A01,
+    [BGMT_Q]                            = 0x2001,
+    [BGMT_UNPRESS_AV]                   = 0x1C00,
+    [BGMT_UNPRESS_DISP]                 = 0x2500,
+    [BGMT_UNPRESS_DOWN]                 = 0x1900,
+    [BGMT_UNPRESS_LEFT]                 = 0x1B00,
+    [BGMT_UNPRESS_RIGHT]                = 0x1A00,
+    [BGMT_UNPRESS_SET]                  = 0x0C00,
+    [BGMT_UNPRESS_UP]                   = 0x1800,
+    [BGMT_UNPRESS_ZOOM_IN]              = 0x0900,
+    [BGMT_UNPRESS_ZOOM_OUT]             = 0x0A00,
+    [BGMT_WHEEL_DOWN]                   = 0x0E01,
+    [BGMT_WHEEL_LEFT]                   = 0x0DFF,
+    [BGMT_WHEEL_RIGHT]                  = 0x0D01,
+    [BGMT_WHEEL_UP]                     = 0x0EFF,
+    [GMT_GUICMD_OPEN_BATT_COVER]        = 0x1301,
+    [GMT_GUICMD_OPEN_SLOT_COVER]        = 0x1201,
+    [GMT_GUICMD_START_AS_CHECK]         = 0x1100,
+    [BGMT_END_OF_LIST]                  = 0x0000
+};
+
 static int button_codes_450D[] = {
     [BGMT_INFO]                         = 0x0101,
     [BGMT_JUMP]                         = 0x0201,
