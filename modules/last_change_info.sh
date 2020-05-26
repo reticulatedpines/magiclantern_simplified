3163d41cee35fb54b4308e1f32318ff87de53645
stephen-e <sje@example.com>
2020-05-26 03:56:47 +0100

    Remove dependency on hg for building

--- modules/last_change_info.sh
+++ modules/last_change_info.sh
@@ -4,6 +4,6 @@
 # also useful by itself to look at a formatted report
 # of the extracted version info
 
-LC_TIME=EN \
-hg log . -r 'reverse(ancestors(.))' -l 1 --template \
-'{date|hgdate}\n{node|short}\n{author|user}\n{desc|strip|firstline}'
+#LC_TIME=EN \
+#hg log . -r 'reverse(ancestors(.))' -l 1 --template \
+#'{date|hgdate}\n{node|short}\n{author|user}\n{desc|strip|firstline}'
