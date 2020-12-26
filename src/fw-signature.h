8f6f1c534dc79384c74bca82ff9202319a99010e
coon <coon@mailbox.org>
2020-12-26 20:12:56 +0100

    fw-signature.h: add SIG_EOSRP_160

--- src/fw-signature.h
+++ src/fw-signature.h
@@ -27,6 +27,7 @@
 #define SIG_200D_101 0xf72c729a // from E0040000
 #define SIG_M50_102  0x3b70901c // from E0040000
 #define SIG_R_120    0x8C19E5DA // from E0040000
+#define SIG_EOSRP_160 0xECDDA78C // from E0040000
 #define SIG_5D4_112  0xf3316d96 // from FE0A0000
 #define SIG_5DS_111  0x6f867e6a // from FE0A0000
 #define SIG_5DSR_112 0xc60c4679 // from FE0A0000
