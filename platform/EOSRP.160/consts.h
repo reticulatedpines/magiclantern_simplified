0238afb700b0a22c075ac4cede01a02365246949
coon <coon@mailbox.org>
2020-12-28 18:27:22 +0100

    EOSRP.160: consts.h: add AF_LED_ADDRESS + verified LEDs

--- platform/EOSRP.160/consts.h
+++ platform/EOSRP.160/consts.h
@@ -2,9 +2,10 @@
  *  RP 1.6.0 consts
  */
 
-#define CARD_LED_ADDRESS               0xD01300D4 // TODO: verify!
-#define LEDON                          0x004D0002 // TODO: verify!
-#define LEDOFF                         0x004C0003 // TODO: verify!
+#define CARD_LED_ADDRESS               0xD01300D4
+#define AF_LED_ADDRESS                 0xD01300D8
+#define LEDON                          0x004D0002
+#define LEDOFF                         0x004C0003
 
 #define HIJACK_FIXBR_DCACHE_CLN_1      0xE0040068   /* first call to dcache_clean, before cstart */
 #define HIJACK_FIXBR_ICACHE_INV_1      0xE0040072   /* first call to icache_invalidate, before cstart */
