bc0c1ebce1c3e8fab5735947e755af0c5ef9fd9b
kitor <github@kitor.pl>
2021-01-16 22:42:51 +0100

    Stubs update. Not working yet.

---
+++ platform/R.180/consts.h
@@ -0,0 +1,17 @@
+/*
+ *  EOS R 1.8.0 consts
+ */
+
+#define CARD_LED_ADDRESS            0xD01300D4   /* maybe also 0xD01300D8/DC? */
+#define LEDON                       0xD0002
+#define LEDOFF                      0xC0003
+
+#define HIJACK_FIXBR_DCACHE_CLN_1   0xE0040068   /* first call to dcache_clean, before cstart */
+#define HIJACK_FIXBR_ICACHE_INV_1   0xE0040072   /* first call to icache_invalidate, before cstart */
+#define HIJACK_FIXBR_DCACHE_CLN_2   0xe00400A0   /* second call to dcache_clean, before cstart */
+#define HIJACK_FIXBR_ICACHE_INV_2   0xE00400AA   /* second call to icache_invalidate, before cstart */
+#define HIJACK_INSTR_BL_CSTART      0xE00400C6   /* easier to fix up here, rather than at E0040034 */
+#define HIJACK_INSTR_HEAP_SIZE      0xE00401D8   /* easier to patch the size; start address is computed */
+#define HIJACK_FIXBR_BZERO32        0xe0040152   /* called from cstart */
+#define HIJACK_FIXBR_CREATE_ITASK   0xe00401b4   /* called from cstart */
+#define HIJACK_INSTR_MY_ITASK       0xe00401e4   /* address of init_task passed to create_init_task */
--- platform/R.180/internals.h
+++ platform/R.180/internals.h
@@ -1,5 +1,5 @@
 /**
- * Camera internals for EOS R 1.2.0
+ * Camera internals for EOS R 1.8.0
  */
 
 /** This camera has a DIGIC VIII chip */
