f620d72ad48cab1f668ec03b8072253441b34de2
kitor <github@kitor.pl>
2021-01-17 09:26:16 +0100

    Fix CSTART and bmp_vram_info, now hello_wolrd works

--- platform/R.180/consts.h
+++ platform/R.180/consts.h
@@ -10,7 +10,7 @@
 #define HIJACK_FIXBR_ICACHE_INV_1   0xE0040072   /* first call to icache_invalidate, before cstart */
 #define HIJACK_FIXBR_DCACHE_CLN_2   0xe00400A0   /* second call to dcache_clean, before cstart */
 #define HIJACK_FIXBR_ICACHE_INV_2   0xE00400AA   /* second call to icache_invalidate, before cstart */
-#define HIJACK_INSTR_BL_CSTART      0xE00400C6   /* easier to fix up here, rather than at E0040034 */
+#define HIJACK_INSTR_BL_CSTART      0xE00400C0   /* easier to fix up here, rather than at E0040034 */
 #define HIJACK_INSTR_HEAP_SIZE      0xE00401D8   /* easier to patch the size; start address is computed */
 #define HIJACK_FIXBR_BZERO32        0xe0040152   /* called from cstart */
 #define HIJACK_FIXBR_CREATE_ITASK   0xe00401b4   /* called from cstart */
