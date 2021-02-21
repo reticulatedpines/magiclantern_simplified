a5f8e689f46068a0cd3fdbf7632b380c43200c25
br <brian@recchia.name>
2021-02-20 22:44:23 -0500

    ubuntu 18.04 uses python2 for python-*

--- contrib/toolchain/prepare.ubuntu.sh
+++ contrib/toolchain/prepare.ubuntu.sh
@@ -2,6 +2,6 @@
 # Development
 ML_PKGS=("flex" "bison" "libgmp3-dev" "libmpfr-dev" "libncurses5-dev" "libmpc-dev" "autoconf" "texinfo" "build-essential" "git-core" "mercurial" "zlibc" "zlib1g-dev" "gcc-arm-none-eabi" ) # "gdb-multiarch" ) #this gdb is broken
 # Documentation
-ML_PKGS+=("python2-matplotlib" "python2-matplotlib-data" "python2-matplotlib-doc" "python2-matplotlib-dbg" "texlive-luatex" "python2-docutils" "texlive-latex-base" "texlive-latex-recommended" "texlive-fonts-extra" "dvi2ps" "texlive-science" "texlive-science-doc" "pandoc" "texlive-latex-extra" "ruby-full" "poppler-utils" "imagemagick" )
+ML_PKGS+=("python-matplotlib" "python-matplotlib-data" "python-matplotlib-doc" "python-matplotlib-dbg" "texlive-luatex" "python-docutils" "texlive-latex-base" "texlive-latex-recommended" "texlive-fonts-extra" "dvi2ps" "texlive-science" "texlive-science-doc" "pandoc" "texlive-latex-extra" "ruby-full" "poppler-utils" "imagemagick" )
 
 sudo apt-get install ${ML_PKGS[@]}
