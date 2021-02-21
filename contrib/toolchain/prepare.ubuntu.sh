339dfa9a535fde17a6f555515fc401e80db41e56
br <brian@recchia.name>
2021-02-20 22:36:00 -0500

    use bash arrays to reduce apt-get calls

--- contrib/toolchain/prepare.ubuntu.sh
+++ contrib/toolchain/prepare.ubuntu.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 # Development
-sudo apt-get install flex bison libgmp3-dev libmpfr-dev libncurses5-dev libmpc-dev autoconf texinfo build-essential git-core mercurial zlibc zlib1g-dev gcc-arm-none-eabi
+ML_PKGS=("flex" "bison" "libgmp3-dev" "libmpfr-dev" "libncurses5-dev" "libmpc-dev" "autoconf" "texinfo" "build-essential" "git-core" "mercurial" "zlibc" "zlib1g-dev" "gcc-arm-none-eabi" ) # "gdb-multiarch" ) #this gdb is broken
 # Documentation
-sudo apt-get install python-matplotlib python-matplotlib-data python-matplotlib-doc python-matplotlib-dbg texlive-luatex python-docutils texlive-latex-base texlive-latex-recommended texlive-fonts-extra dvi2ps texlive-science texlive-science-doc pandoc texlive-latex-extra ruby-full poppler-utils imagemagick
+ML_PKGS+=("python2-matplotlib" "python2-matplotlib-data" "python2-matplotlib-doc" "python2-matplotlib-dbg" "texlive-luatex" "python2-docutils" "texlive-latex-base" "texlive-latex-recommended" "texlive-fonts-extra" "dvi2ps" "texlive-science" "texlive-science-doc" "pandoc" "texlive-latex-extra" "ruby-full" "poppler-utils" "imagemagick" )
+
+sudo apt-get install ${ML_PKGS[@]}
