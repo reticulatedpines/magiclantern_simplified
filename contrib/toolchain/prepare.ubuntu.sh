670b9aa9b27848d6412d75e4e0e5834794091eb2
br <brian@recchia.name>
2021-02-20 22:41:24 -0500

    Oh, bash arrays are a bashism. Use bash.

--- contrib/toolchain/prepare.ubuntu.sh
+++ contrib/toolchain/prepare.ubuntu.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 # Development
 ML_PKGS=("flex" "bison" "libgmp3-dev" "libmpfr-dev" "libncurses5-dev" "libmpc-dev" "autoconf" "texinfo" "build-essential" "git-core" "mercurial" "zlibc" "zlib1g-dev" "gcc-arm-none-eabi" ) # "gdb-multiarch" ) #this gdb is broken
 # Documentation
