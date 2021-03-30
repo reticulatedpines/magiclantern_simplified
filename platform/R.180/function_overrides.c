f9f65b732d257d356a1ebcb3d70b4ccaa2985a19
kitor <github@kitor.pl>
2021-03-30 18:55:52 +0200

    Borrow LoadCalendarFromRTC fix from SJE

--- platform/R.180/function_overrides.c
+++ platform/R.180/function_overrides.c
@@ -38,6 +38,11 @@ void GUI_Control(int bgmt_code, int obj, int arg, int unknown){
     gui_enqueue_message(0, bgmt_code, obj, arg);
 }
 
+void LoadCalendarFromRTC(struct tm *tm)
+{
+    _LoadCalendarFromRTC(tm, 0, 0, 16);
+}
+
 /** File I/O **/
 
 /**
--- src/dryos.h
+++ src/dryos.h
@@ -115,7 +115,7 @@ struct tm {
         char    *tm_zone;       /* timezone abbreviation */
 };
 
-#if defined CONFIG_200D
+#if defined(CONFIG_DIGIC_78)
 void LoadCalendarFromRTC(struct tm *tm);
 extern void _LoadCalendarFromRTC(struct tm *tm, uint32_t a, uint32_t b, uint32_t c);
 #else
