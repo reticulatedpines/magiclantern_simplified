738e21a179d13c972226756c61f40319813223b1
stephen-e <sje@example.com>
2021-04-13 04:47:51 +0100

    Some stubs improved.  Audit pass complete.

--- platform/200D.101/features.h
+++ platform/200D.101/features.h
@@ -2,6 +2,7 @@
 
 #define FEATURE_VRAM_RGBA
 
+
 // partially working
 //#define FEATURE_SHOW_SHUTTER_COUNT
 //#define FEATURE_SHOW_TASKS
@@ -10,6 +11,7 @@
 // This does not yet work (assumes ARM, not Thumb).  Alex recommends
 // a good looking fix:
 // http://www.mcternan.me.uk/ArmStackUnwinding/
+#undef CONFIG_ADDITIONAL_VERSION
 #undef CONFIG_CRASH_LOG
 #undef CONFIG_TSKMON
 #undef CONFIG_PROP_REQUEST_CHANGE
