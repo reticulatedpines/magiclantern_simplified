84f6c48d4ee783d9382fe6ab1341de98091c9465
coon <coon@mailbox.org>
2021-06-13 19:29:31 +0200

    EOSRP: gui.h add button codes

--- platform/EOSRP.160/gui.h
+++ platform/EOSRP.160/gui.h
@@ -1,29 +1,29 @@
 #ifndef _cameraspecific_gui_h_
 #define _cameraspecific_gui_h_
 
-/* Codes found for R180 */
+/* Codes found for RP160 */
 
-#define BGMT_PRESS_UP                0x31
-#define BGMT_PRESS_LEFT              0x2F
-#define BGMT_PRESS_RIGHT             0x2D
-#define BGMT_PRESS_DOWN              0x33
+#define BGMT_PRESS_UP                0x3B
+#define BGMT_PRESS_LEFT              0x39
+#define BGMT_PRESS_RIGHT             0x37
+#define BGMT_PRESS_DOWN              0x3D
 
 /* Q/Set is one button on R */
-#define BGMT_PRESS_SET               0x04
-#define BGMT_UNPRESS_SET             0x05
+#define BGMT_PRESS_SET               0x06
+#define BGMT_UNPRESS_SET             0x07
 
-#define BGMT_TRASH                   0x0E
-#define BGMT_MENU                    0x06
-#define BGMT_INFO                    0x08
+#define BGMT_TRASH                   0x10
+#define BGMT_MENU                    0x08
+#define BGMT_INFO                    0x0A
 
-#define BGMT_PLAY                    0x0C
+#define BGMT_PLAY                    0x0E
 
-#define BGMT_WHEEL_LEFT              0x02
-#define BGMT_WHEEL_RIGHT             0x03
+#define BGMT_WHEEL_LEFT              0x00 // main dial
+#define BGMT_WHEEL_RIGHT             0x01 // main dial
 
-#define BGMT_PRESS_HALFSHUTTER       0x7D
+#define BGMT_PRESS_HALFSHUTTER       0xFF // unknown
 
-/* WRONG: DNE in R */
+/* WRONG: DNE in RP */
 #define BGMT_PRESS_UP_RIGHT          0xF0
 #define BGMT_PRESS_UP_LEFT           0xF1
 #define BGMT_PRESS_DOWN_RIGHT        0xF2
@@ -37,25 +37,25 @@
  *        Thus skipping for now.
  *        Same applies for RF lenses ring btw.
  */
-#define BGMT_WHEEL_UP                0xF5
-#define BGMT_WHEEL_DOWN              0xF6
-#define BGMT_LV                      0xF7
+#define BGMT_WHEEL_UP                0xF5 // does not exist on RP
+#define BGMT_WHEEL_DOWN              0xF6 // does not exist on RP
+#define BGMT_LV                      0xF7 // does not exist on RP (mirrorless)
 /* what is that ?! */
-#define BGMT_UNPRESS_UDLR            0xF8
+#define BGMT_UNPRESS_UDLR            0xF8 // does probably not exist on RP
 
-#define BGMT_PRESS_ZOOM_IN           0xF9
-#define BGMT_UNPRESS_ZOOM_IN         0xE0
-#define BGMT_PRESS_ZOOM_OUT          0xE1
-#define BGMT_UNPRESS_ZOOM_OUT        0xE2
-#define BGMT_PICSTYLE                0xE3
+#define BGMT_PRESS_ZOOM_IN           0x17 
+#define BGMT_UNPRESS_ZOOM_IN         0x18
+#define BGMT_PRESS_ZOOM_OUT          0xE1 // does not exist on RP
+#define BGMT_UNPRESS_ZOOM_OUT        0xE2 // does not exist on RP
+#define BGMT_PICSTYLE                0xE3 // does not exist on RP
 
-#define BGMT_FLASH_MOVIE             0xE4
-#define BGMT_PRESS_FLASH_MOVIE       0xE5
-#define BGMT_UNPRESS_FLASH_MOVIE     0xE6
+#define BGMT_FLASH_MOVIE             0xE4 // does not exist on RP
+#define BGMT_PRESS_FLASH_MOVIE       0xE5 // does not exist on RP
+#define BGMT_UNPRESS_FLASH_MOVIE     0xE6 // does not exist on RP
 
-#define BGMT_ISO_MOVIE               0xE7
-#define BGMT_PRESS_ISO_MOVIE         0xE8
-#define BGMT_UNPRESS_ISO_MOVIE       0xE9
+#define BGMT_ISO_MOVIE               0xE7 // does not exist on RP
+#define BGMT_PRESS_ISO_MOVIE         0xE8 // does not exist on RP
+#define BGMT_UNPRESS_ISO_MOVIE       0xE9 // does not exist on RP
 /* kitor: Defs not used by ML
  * MODE button: 0x35 PRESS, 0x36 UNPRESS
  * Backlight:   0x3D PRESS, 0x3E UNPRESS
