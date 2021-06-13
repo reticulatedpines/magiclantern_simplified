7ee126621daaf1eba473c231255aad8b46ee4427
coon <coon@mailbox.org>
2021-06-13 13:55:47 +0200

    raw.c: add color matrix table for RP

--- src/raw.c
+++ src/raw.c
@@ -348,6 +348,15 @@ static int (*dual_iso_get_dr_improvement)() = MODULE_FUNCTION(dual_iso_get_dr_im
     -1199, 10000,    2769, 10000,   6108, 10000
 #endif
 
+#ifdef CONFIG_EOSRP // from https://github.com/LibRaw/LibRaw src/tables/colordata.cpp
+    // { LIBRAW_CAMERAMAKER_Canon, "EOS EOSRP", 0, 0,
+    //  { 8608,-2097,-1178,-5425,13265,2383,-1149,2238,5680 } },
+    #define CAM_COLORMATRIX1 \
+     8608, 10000,   -2097, 10000,  -1178, 10000, \
+    -5425, 10000,   13265, 10000,   2383, 10000, \
+    -1149, 10000,   2238,  10000,   5680, 10000
+#endif
+
 #ifdef CONFIG_M50 // from https://github.com/LibRaw/LibRaw src/tables/colordata.cpp
     // { LIBRAW_CAMERAMAKER_Canon, "EOS M50", 0, 0,
     //   { 8532,-701,-1167,-4095,11879,2508,-797,2424,7010 } },
