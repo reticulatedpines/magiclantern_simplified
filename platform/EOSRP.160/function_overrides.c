56b9958a65c12a251295782225e6ac3b4e2ed3e7
coon <coon@mailbox.org>
2021-06-13 15:25:05 +0200

    add missing files

---
+++ platform/EOSRP.160/function_overrides.c
@@ -0,0 +1,135 @@
+/** \file
+ * Function overrides needed for RP 1.6.0
+ */
+/*
+ * Copyright (C) 2021 Magic Lantern Team
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA.
+ */
+
+ #include <dryos.h>
+ #include <property.h>
+ #include <bmp.h>
+ #include <config.h>
+ #include <consts.h>
+ #include <lens.h>
+
+/** GUI **/
+//see comments in stub.S
+void gui_init_end(void){ }
+
+//Not sure if all args are uint.
+extern void gui_enqueue_message(uint32_t, uint32_t, uint32_t, uint32_t);
+void GUI_Control(int bgmt_code, int obj, int arg, int unknown){
+    gui_enqueue_message(0, bgmt_code, obj, arg);
+}
+
+void LoadCalendarFromRTC(struct tm *tm)
+{
+    _LoadCalendarFromRTC(tm, 0, 0, 16);
+}
+
+/** File I/O **/
+
+/**
+ * _FIO_GetFileSize returns now 64bit size in form of struct.
+ * This probably should be integrated into fio-ml for CONFIG_DIGIC_VIII
+ */
+extern int _FIO_GetFileSize64(const char *, void *);
+int _FIO_GetFileSize(const char * filename, uint32_t * size){
+    uint32_t size64[2];
+    int code = _FIO_GetFileSize64(filename, &size64);
+    *size = size64[0]; //return "lower" part
+    return code;
+}
+
+/** WRONG: temporary overrides to get CONFIG_HELLO_WORLD working **/
+
+void SetEDmac(unsigned int channel, void *address, struct edmac_info *ptr, int flags)
+{
+    return;
+}
+
+void ConnectWriteEDmac(unsigned int channel, unsigned int where)
+{
+    return;
+}
+
+void ConnectReadEDmac(unsigned int channel, unsigned int where)
+{
+    return;
+}
+
+void StartEDmac(unsigned int channel, int flags)
+{
+    return;
+}
+
+void AbortEDmac(unsigned int channel)
+{
+    return;
+}
+
+void RegisterEDmacCompleteCBR(int channel, void (*cbr)(void*), void* cbr_ctx)
+{
+    return;
+}
+
+void UnregisterEDmacCompleteCBR(int channel)
+{
+    return;
+}
+
+void RegisterEDmacAbortCBR(int channel, void (*cbr)(void*), void* cbr_ctx)
+{
+    return;
+}
+
+void UnregisterEDmacAbortCBR(int channel)
+{
+    return;
+}
+
+void RegisterEDmacPopCBR(int channel, void (*cbr)(void*), void* cbr_ctx)
+{
+    return;
+}
+
+void UnregisterEDmacPopCBR(int channel)
+{
+    return;
+}
+
+void _EngDrvOut(uint32_t reg, uint32_t value)
+{
+    return;
+}
+
+uint32_t shamem_read(uint32_t addr)
+{
+    return 0;
+}
+
+void _engio_write(uint32_t* reg_list)
+{
+    return;
+}
+
+unsigned int UnLockEngineResources(struct LockEntry *lockEntry)
+{
+    return 0;
+}
