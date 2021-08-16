55755e124f8f8fd71a46e7c5b9c02dc4fa38ad03
stephen-e <sje@example.com>
2021-08-16 07:02:32 +0100

    EOSRP: fix build for mem stealing changes, fix get_task_info_by_id stub
    The change to early mem stealing broke D678 cams that
    don't have the required PTR_ consts.  These require a small
    amount of reversing to find.
    
    Update name is_taskid_valid -> get_task_info_by_id, fix address,
    create wrapper due to changed API

--- platform/EOSRP.160/consts.h
+++ platform/EOSRP.160/consts.h
@@ -12,10 +12,45 @@
 #define HIJACK_FIXBR_DCACHE_CLN_2      0xE00400A0   /* second call to dcache_clean, before cstart */
 #define HIJACK_FIXBR_ICACHE_INV_2      0xE00400AA   /* second call to icache_invalidate, before cstart */
 #define HIJACK_INSTR_BL_CSTART         0xE00400C0   /* easier to fix up here, rather than at E0040034 */
-#define HIJACK_INSTR_HEAP_SIZE         0xE00401D8   /* easier to patch the size; start address is computed */
 #define HIJACK_FIXBR_BZERO32           0xE0040152   /* called from cstart */
 #define HIJACK_FIXBR_CREATE_ITASK      0xE00401B4   /* called from cstart */
 #define HIJACK_INSTR_MY_ITASK          0xE00401E4   /* address of init_task passed to create_init_task */
+
+#define PTR_USER_MEM_SIZE              0xE00401D8   /* easier to patch the size; start address is computed */
+#define PTR_SYS_OFFSET                 0xe00401d0   // offset from DryOS base to sys_mem start
+#define PTR_SYS_OBJS_OFFSET            0xe00401dc   // offset from DryOS base to sys_obj start
+#define PTR_DRYOS_BASE                 0xe00401bc
+#define ML_MAX_USER_MEM_STOLEN 0x40000 // True max differs per cam, 0x40000 has been tested on
+                                       // the widest range of D678 cams with no observed problems,
+                                       // but not all cams have been tested!
+
+#define ML_MAX_SYS_MEM_INCREASE 0x40000 // More may be VERY unsafe!  Increasing this pushes sys_mem
+                                        // higher in memory, at some point that must cause Bad Things,
+                                        // consequences unknown.  0x40000 has been tested, a little...
+
+#define ML_RESERVED_MEM 0x66000 // Can be lower than ML_MAX_USER_MEM_STOLEN + ML_MAX_SYS_MEM_INCREASE,
+                                // but must not be higher; sys_objs would get overwritten by ML code.
+                                // Must be larger than MemSiz reported by build for magiclantern.bin
+/*
+Before patching:
+DryOS base    user_start                       sys_objs_start    sys_start
+    |-------------|--------------------------------|---------------|--------------------->
+                   <-------  user_mem_size ------->                 <---- sys_len ------->
+    ---------------- sys_objs_offset ------------->
+    ---------------- sys_mem_offset ------------------------------>
+
+After patching, user mem reduced and sys mem moved up
+DryOS base    user_start                                 sys_objs_start    sys_start
+    |-------------|-------------------|<-- ml_reserved_mem -->|---------------|--------------------->
+                   <- user_mem_size ->                                         <---- sys_len ------->
+    ---------------- sys_objs_offset ------------------------>
+    ---------------- sys_mem_offset ----------------------------------------->
+*/
+
+#if ML_RESERVED_MEM > ML_MAX_USER_MEM_STOLEN + ML_MAX_SYS_MEM_INCREASE
+#error "ML_RESERVED_MEM too big to fit!"
+#endif
+
 
 /* PROPABLY WRONG: Some hacks for early porting */
 #define DISPLAY_IS_ON               1
