bae1c85d185e1850ad795caa7c5f10fdf106b4b8
kitor <github@kitor.pl>
2021-10-16 18:46:46 +0200

    Digic 6: disable semaphore use in FEATURE_VRAM_RGBA

--- src/bmp.c
+++ src/bmp.c
@@ -209,10 +209,14 @@ void refresh_yuv_from_rgb(void)
         b++;
     }
 
-    take_semaphore(winsys_sem, 0);
     // trigger Ximr to render to OSD from RGB buffer
+#ifdef CONFIG_DIGIC_VI
+    XimrExe((void *)XIMR_CONTEXT);
+#else
+    take_semaphore(winsys_sem, 0);
     XimrExe((void *)XIMR_CONTEXT);
     give_semaphore(winsys_sem);
+#endif
     ml_refresh_display_needed = 0;
 }
 
