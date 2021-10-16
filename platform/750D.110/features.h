e1a081f5caeed9eeb2cc3ce3da46bd304dbf92ce
kitor <github@kitor.pl>
2021-10-16 19:00:02 +0200

    750D: Full build with some features enabled

--- platform/750D.110/features.h
+++ platform/750D.110/features.h
@@ -1,1 +1,24 @@
 #define FEATURE_VRAM_RGBA
+
+// Don't Click Me menu looks to be intended as a place
+// for devs to put custom code in debug.c run_test(),
+// and allowing triggering from a menu context.
+#define FEATURE_DONT_CLICK_ME
+
+#define FEATURE_SHOW_SHUTTER_COUNT
+
+// working but incomplete, some allocators don't report
+// anything yet as they're faked / not yet found
+#define FEATURE_SHOW_FREE_MEMORY
+
+#define CONFIG_ADDITIONAL_VERSION
+#define FEATURE_SCREENSHOT
+
+#define CONFIG_TSKMON
+#define FEATURE_SHOW_TASKS
+#define FEATURE_SHOW_CPU_USAGE
+#define FEATURE_SHOW_GUI_EVENTS
+
+#undef CONFIG_CRASH_LOG
+#undef CONFIG_PROP_REQUEST_CHANGE
+#undef CONFIG_AUTOBACKUP_ROM
--- platform/750D.110/internals.h
+++ platform/750D.110/internals.h
@@ -4,3 +4,10 @@
 
 /** This camera has a DIGIC VI chip */
 #define CONFIG_DIGIC_VI
+
+/** Digic 6 does not have bitmap font in ROM, try to load it from card **/
+#define CONFIG_NO_BFNT
+
+/** disable SRM for early port
+ */
+//#define CONFIG_MEMORY_SRM_NOT_WORKING
