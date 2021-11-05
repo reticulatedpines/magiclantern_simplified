f94a03590dbf4570d76e91d8c24b5500bf1cde7b
kitor <github@kitor.pl>
2021-11-05 20:06:27 +0100

    750D: C.fn definitions

--- platform/750D.110/cfn.c
+++ platform/750D.110/cfn.c
@@ -1,23 +1,22 @@
 #include <dryos.h>
 #include <property.h>
+#include <cfn-generic.h>
 
-// look on camera menu or review sites to get custom function numbers
+// on 750D these are not CFn, but in main menus
+GENERIC_GET_ALO
 
-/* kitor: nerfed everything for now, just in case */
-int get_htp() { return 0; }
-void set_htp(int value) {  return; }
+// GUI shows groups I-IV. But pages are numbered 1-13
+// GetCFnData(0, <number>) returns proper value
 
-int get_alo() { return 0; }
-void set_alo(int value) { return; }
+// Changing settings disabled for now.
 
-/* kitor: mirrorless, no such option */
-int get_mlu() { return 0; }
+// 2-3
+int get_htp() { return GetCFnData(0, 3); }
+
+// 3-9
+int get_mlu() { return GetCFnData(0, 9); }
 void set_mlu(int value) { return; }
 
-/* kitor: not sure what to do? 4-1 opens menu for all button assigments */
-int cfn_get_af_button_assignment() { return 0; }
+// 4-10
+int cfn_get_af_button_assignment() { return GetCFnData(0, 10); }
 void cfn_set_af_button(int value) { return; }
-
-/* kitor: same as above? */
-int get_af_star_swap() { return 0; }
-void set_af_star_swap(int value) { return; }
