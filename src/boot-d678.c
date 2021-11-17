5cc1c32f4646f27ba3269d32bcbbf3e8403efe32
stephen-e <sje@example.com>
2021-11-17 03:06:07 +0000

    D6: boot code, fix broken BLX encoder
    Wow, what a tricky bug.  The old code in patch_thumb_branch()
    was wrong in the BLX case, producing BFCSEL.  Qemu 2 didn't handle
    this instruction, and somehow made it work (interpreted it as
    some different branch type?).  Problems were seen on physical 750D.
    
    Qemu 4 handles BFCSEL and repro'd the 750D problems.

--- src/boot-d678.c
+++ src/boot-d678.c
@@ -41,25 +41,40 @@ static void patch_thumb_branch(uint32_t pc, uint32_t dest)
     // Type of branch to patch in is detected from dest,
     // Thumb targets should be specified with LSb set.
 
-    uint32_t opcode = 0xc000f000; // BLX, swap to ARM
-    if (dest % 2) // Thumb target
-        opcode = 0xd000f000; // BL, stay in Thumb
-
     pc = reloc_addr(pc);
     qprint("[BOOT] fixing up branch at "); qprintn(pc);
     qprint(" (ROM: "); qprintn(pc); qprint(") to "); qprintn(dest); qprint("\n");
-    /* thanks atonal */
-    //uint32_t offset = dest - ((pc + 4) & ~3); /* according to datasheets, this should be the correct calculation -> ALIGN(PC, 4) */
-    uint32_t offset = dest - (pc + 4);  /* this one works for BX */
-    uint32_t s = (offset >> 24) & 1;
-    uint32_t i1 = (offset >> 23) & 1;
-    uint32_t i2 = (offset >> 22) & 1;
-    uint32_t imm10 = (offset >> 12) & 0x3ff;
-    uint32_t imm11 = (offset >> 1) & 0x7ff;
-    uint32_t j1 = (!(i1 ^ s)) & 0x1;
-    uint32_t j2 = (!(i2 ^ s)) & 0x1;
 
-    *(uint32_t *)pc = opcode | (s << 10) | imm10 | (j1 << 29) | (j2 << 27) | (imm11 << 16);
+    // See ARMv7-A / ARMv7-R Ref Manual for encodings
+
+    if (dest % 2) // Thumb target, use BL, firmware is Thumb
+    {
+        uint32_t opcode = 0xd000f000; // BL fixed bits
+        uint32_t offset = dest - (pc + 4);
+        uint32_t s = (offset >> 24) & 1;
+        uint32_t i1 = (offset >> 23) & 1;
+        uint32_t i2 = (offset >> 22) & 1;
+        uint32_t imm10 = (offset >> 12) & 0x3ff;
+        uint32_t imm11 = (offset >> 1) & 0x7ff;
+        uint32_t j1 = (!(i1 ^ s)) & 0x1;
+        uint32_t j2 = (!(i2 ^ s)) & 0x1;
+
+        *(uint32_t *)pc = opcode | (s << 10) | imm10 | (j1 << 29) | (j2 << 27) | (imm11 << 16);
+    }
+    else // ARM target, use BLX
+    {
+        uint32_t opcode = 0xc000f000; // BLX fixed bits
+        uint32_t offset = dest - (pc + 4);
+        uint32_t s = (offset >> 24) & 1;
+        uint32_t i1 = (offset >> 23) & 1;
+        uint32_t i2 = (offset >> 22) & 1;
+        uint32_t imm10H = (offset >> 12) & 0x3ff;
+        uint32_t imm10L = (offset >> 2) & 0x3ff;
+        uint32_t j1 = (!(i1 ^ s)) & 0x1;
+        uint32_t j2 = (!(i2 ^ s)) & 0x1;
+
+        *(uint32_t *)pc = opcode | (s << 10) | imm10H | (j1 << 29) | (j2 << 27) | (imm10L << 17);
+    }
 }
 
 static void my_bzero32(void *buf, size_t len)
