e9fbea90f11dbc753417251d297194491f41c11e
coon <coon@mailbox.org>
2021-12-26 16:15:24 +0100

    RP.160: enable FEATURE_GLOBAL_DRAW + CONFIG_LIVEVIEW + CONFIG_STATE_OBJECT_HOOKS

--- platform/EOSRP.160/internals.h
+++ platform/EOSRP.160/internals.h
@@ -13,3 +13,9 @@
  * even if just one buffer is requested.
  */
 #define CONFIG_MEMORY_SRM_NOT_WORKING
+
+/* has LV */
+#define CONFIG_LIVEVIEW
+
+/* enable state objects hooks */
+#define CONFIG_STATE_OBJECT_HOOKS
