0ccd67b3edcc6ea98a1c80e82f2d0e9e0885959d
reticulatedpines <33672591+reticulatedpines@users.noreply.github.com>
2022-07-19 18:36:57 +0100

    Move cache_hacks funcs to ML, so modules can check deps
    cache_hacks.h held code directly, and modules were including
    that header.  Those functions weren't visible in ML, so checkdeps.py
    was blind to them.  The functions had Digic 4, 5 specific assumptions
    and couldn't work on D678X.
    
    Moving the code into cache_hacks.o puts the symbols in ML and
    module build fails as expected for D678X, rather than succeeding
    inappropriately.

--- src/cache_hacks.h
+++ src/cache_hacks.h
@@ -1,9 +1,6 @@
 #ifndef _CACHE_HACKS_H_
 #define _CACHE_HACKS_H_
 
-#ifdef __ARM__
-
-#ifdef CONFIG_DIGIC_45
 /*
  * Canon cameras prior to Digic 6 appear to use the ARMv5 946E.
  * (Confirmed on: 550D, ... )
@@ -38,8 +35,6 @@
  */
 
 /*
- * We don't use this file for D678X.
- *
  * Digic 6 cams use ARMv7-R, Digic 7, 8 and X ARMv7-A.
  * These cpus - at least in Qemu - do not support cache lockdown.
  * Some physical cams have been tested and attempts to use cache lockdown
@@ -101,67 +96,9 @@
 #define CACHE_SEGMENT_ADDRMASK(t)   (CACHE_SEGMENT_BITMASK(t)<<CACHE_SEGMENT_TAGOFFSET(t))
 
 /* return cache size in bits (13 -> 2^13 -> 8192 -> 8KiB) */
-static uint32_t cache_get_size(uint32_t type)
-{
-    uint32_t cache_info = 0;
-    
-    /* get cache type register
-     * On 550D: 0x0F112112.  8KB I/D Cache. 4 way set associative.
-     * On 5D3:  0x0F192192. 32KB I/D Cache. 4 way set associative.
-     */
-    asm volatile ("\
-       MRC p15, 0, %0, c0, c0, 1\r\n\
-       " : "=r"(cache_info));
+static uint32_t cache_get_size(uint32_t type);
 
-    /* dcache is described at bit pos 12 */
-    if(type == TYPE_DCACHE)
-    {
-        cache_info >>= 12;
-    }
-    
-    /* check if size is invalid, or absent flag is set */
-    uint32_t size = (cache_info >> 6) & 0x0F;
-    uint32_t absent = (cache_info >> 2) & 0x01;
-    
-    if((size < 3) || absent)
-    {
-        return 0;
-    }
-    
-    /* return as 2^x */
-    return size + 9;
-}
-
-static uint32_t cache_patch_single_word(uint32_t address, uint32_t data, uint32_t type)
-{
-    uint32_t cache_seg_index_word = (address & (CACHE_INDEX_ADDRMASK(type) | CACHE_WORD_ADDRMASK(type)));
-    uint32_t cache_tag_index = (address & (CACHE_TAG_ADDRMASK(type) | CACHE_INDEX_ADDRMASK(type))) | 0x10;
-
-    if(type == TYPE_ICACHE)
-    {
-        asm volatile ("\
-           /* write index for address to write */\
-           MCR p15, 3, %0, c15, c0, 0\r\n\
-           /* set TAG at given index */\
-           MCR p15, 3, %1, c15, c1, 0\r\n\
-           /* write instruction */\
-           MCR p15, 3, %2, c15, c3, 0\r\n\
-           " : : "r"(cache_seg_index_word), "r"(cache_tag_index), "r"(data));
-    }
-    else
-    {
-        asm volatile ("\
-           /* write index for address to write */\
-           MCR p15, 3, %0, c15, c0, 0\r\n\
-           /* set TAG at given index */\
-           MCR p15, 3, %1, c15, c2, 0\r\n\
-           /* write data */\
-           MCR p15, 3, %2, c15, c4, 0\r\n\
-           " : : "r"(cache_seg_index_word), "r"(cache_tag_index), "r"(data));
-    }
-    
-    return 1;
-}
+static uint32_t cache_patch_single_word(uint32_t address, uint32_t data, uint32_t type);
 
 /* fetch all the instructions in that temp_cacheline the given address is in.
    this is *required* before patching a single address.
@@ -172,353 +109,30 @@ static uint32_t cache_patch_single_word(uint32_t address, uint32_t data, uint32_
 
    same applies to dcache routines
  */
-static void cache_fetch_line(uint32_t address, uint32_t type)
-{
-    uint32_t base = (address & ~0x1F);
-    uint32_t temp_cacheline[8];
-    
-    /* our ARM946 has 0x20 byte temp_cachelines. fetch the current line
-       thanks to unified memories, we can do LDR on instructions.
-    */
-    for(uint32_t pos = 0; pos < 8; pos++)
-    {
-        temp_cacheline[pos] = ((uint32_t *)base)[pos];
-    }
-
-    /* and nail it into locked cache */
-    for(uint32_t pos = 0; pos < 8; pos++)
-    {
-        cache_patch_single_word(base + pos * 4, temp_cacheline[pos], type);
-    }
-}
+static void cache_fetch_line(uint32_t address, uint32_t type);
 
 /* return the tag and content at given index (segment+index+word) */
-static void cache_get_content(uint32_t segment, uint32_t index, uint32_t word, uint32_t type, uint32_t *tag, uint32_t *data)
-{
-    uint32_t cache_seg_index_word = 0;
-    uint32_t stored_tag_index = 0;
-    uint32_t stored_data = 0;
-
-    cache_seg_index_word |= ((segment & CACHE_SEGMENT_BITMASK(type)) << CACHE_SEGMENT_TAGOFFSET(type));
-    cache_seg_index_word |= ((index & CACHE_INDEX_BITMASK(type)) << CACHE_INDEX_TAGOFFSET(type));
-    cache_seg_index_word |= ((word & CACHE_WORD_BITMASK(type)) << CACHE_WORD_TAGOFFSET(type));
-
-    if(type == TYPE_ICACHE)
-    {
-        asm volatile ("\
-           /* write index for address to write */\
-           MCR p15, 3, %2, c15, c0, 0\r\n\
-           /* get TAG at given index */\
-           MRC p15, 3, %0, c15, c1, 0\r\n\
-           /* get DATA at given index */\
-           MRC p15, 3, %1, c15, c3, 0\r\n\
-           " : "=r"(stored_tag_index), "=r"(stored_data) : "r"(cache_seg_index_word));
-    }
-    else
-    {
-        asm volatile ("\
-           /* write index for address to write */\
-           MCR p15, 3, %2, c15, c0, 0\r\n\
-           /* get TAG at given index */\
-           MRC p15, 3, %0, c15, c2, 0\r\n\
-           /* get DATA at given index */\
-           MRC p15, 3, %1, c15, c4, 0\r\n\
-           " : "=r"(stored_tag_index), "=r"(stored_data) : "r"(cache_seg_index_word));
-    }
-
-    *tag = stored_tag_index;
-    *data = stored_data;
-}
+static void cache_get_content(uint32_t segment, uint32_t index, uint32_t word, uint32_t type, uint32_t *tag, uint32_t *data);
 
 /* check if given address is already used or if it is usable for patching */
 /* optional: get current cached value */
-static uint32_t cache_is_patchable(uint32_t address, uint32_t type, uint32_t* current_value)
-{
-    uint32_t stored_tag_index = 0;
-    uint32_t stored_data = 0;
-    
-    if (current_value)
-    {
-        *current_value = 0xFFFFFFFF;
-    }
-    
-    cache_get_content(0, (address & CACHE_INDEX_ADDRMASK(type))>>CACHE_INDEX_TAGOFFSET(type), (address & CACHE_WORD_ADDRMASK(type))>>CACHE_WORD_TAGOFFSET(type), type, &stored_tag_index, &stored_data);
-    
-    /* this line is free, so can be used for patching */
-    if((stored_tag_index & 0x10) == 0)
-    {
-        return 1;
-    }
-
-    if (current_value)
-    {
-        *current_value = stored_data;
-    }
-    
-    /* now check if the TAG RAM content matches with what we expect and valid bit is set */
-    uint32_t tag_index_mask = CACHE_TAG_ADDRMASK(type) | CACHE_INDEX_ADDRMASK(type);
-    uint32_t cache_tag_index = address & tag_index_mask;
-
-    if((stored_tag_index & tag_index_mask) == cache_tag_index)
-    {
-        /* that line is used by the right address, now check data */
-        if(stored_data == *(uint32_t*)address)
-        {
-            /* data is original, so it is patchable */
-            return 1;
-        }
-        
-        /* its already patched. so return 2 */
-        return 2;
-    }
-    
-    /* oh, its already used by some other patch. sorry. */
-    return 0;
-}
+uint32_t cache_is_patchable(uint32_t address, uint32_t type, uint32_t* current_value);
 
 /* check if given address is already in cache */
-static uint32_t cache_get_cached(uint32_t address, uint32_t type)
-{
-    uint32_t cache_seg_index_word = (address & (CACHE_INDEX_ADDRMASK(type) | CACHE_WORD_ADDRMASK(type)));
-    uint32_t stored_tag_index = 0;
+static uint32_t cache_get_cached(uint32_t address, uint32_t type);
 
-    if(type == TYPE_ICACHE)
-    {
-        asm volatile ("\
-           /* write index for address to write */\
-           MCR p15, 3, %1, c15, c0, 0\r\n\
-           /* get TAG at given index */\
-           MRC p15, 3, %0, c15, c1, 0\r\n\
-           " : "=r"(stored_tag_index) : "r"(cache_seg_index_word));
-    }
-    else
-    {
-        asm volatile ("\
-           /* write index for address to write */\
-           MCR p15, 3, %1, c15, c0, 0\r\n\
-           /* get TAG at given index */\
-           MRC p15, 3, %0, c15, c2, 0\r\n\
-           " : "=r"(stored_tag_index) : "r"(cache_seg_index_word));
-    }
-
-    /* now check if the TAG RAM content matches with what we expect and valid bit is set */
-    uint32_t tag_index_valid_mask = CACHE_TAG_ADDRMASK(type) | CACHE_INDEX_ADDRMASK(type) | 0x10;
-    uint32_t cache_tag_index = (address & tag_index_valid_mask) | 0x10;
-
-    if((stored_tag_index & tag_index_valid_mask) == cache_tag_index)
-    {
-        return 1;
-    }
-
-    return 0;
-}
-
-static void icache_unlock()
-{
-    uint32_t old_int = cli();
-
-    /* make sure all entries are set to invalid */
-    for(uint32_t index = 0; index < (1U<<CACHE_INDEX_BITS(TYPE_ICACHE)); index++)
-    {
-        asm volatile ("\
-           /* write index for address to write */\
-           MOV R0, %0, LSL #5\r\n\
-           MCR p15, 3, R0, c15, c0, 0\r\n\
-           /* set TAG at given index */\
-           MCR p15, 3, R0, c15, c1, 0\r\n\
-           " : : "r"(index) : "r0");
-    }
-
-    /* disable cache lockdown */
-    asm volatile ("\
-       MOV R0, #0\r\n\
-       MCR p15, 0, R0, c9, c0, 1\r\n\
-       " : : : "r0");
-
-    /* and flush cache again to make sure its consistent */
-    asm volatile ("\
-        MOV R0, #0\r\n\
-        MCR p15, 0, R0, c7, c5, 0\r\n\
-        MCR p15, 0, R0, c7, c10, 4\r\n\
-        " : : : "r0"
-    );
-    sei(old_int);
-}
-
-static void dcache_unlock()
-{
-    uint32_t old_int = cli();
-
-    /* first clean and flush dcache entries */
-    for(uint32_t segment = 0; segment < (1U<<CACHE_SEGMENT_BITS(TYPE_DCACHE)); segment++ )
-    {
-        for(uint32_t index = 0; index < (1U<<CACHE_INDEX_BITS(TYPE_DCACHE)); index++)
-        {
-            uint32_t seg_index = (segment << CACHE_SEGMENT_TAGOFFSET(TYPE_DCACHE)) | (index << CACHE_INDEX_TAGOFFSET(TYPE_DCACHE));
-            asm volatile ("\
-                mcr p15, 0, %0, c7, c14, 2\r\n\
-                " : : "r"(seg_index)
-            );
-        }
-    }
-    
-    /* make sure all entries are set to invalid */
-    for(uint32_t index = 0; index < (1U<<CACHE_INDEX_BITS(TYPE_ICACHE)); index++)
-    {
-        asm volatile ("\
-           /* write index for address to write */\
-           MOV R0, %0, LSL #5\r\n\
-           MCR p15, 3, R0, c15, c0, 0\r\n\
-           /* set TAG at given index */\
-           MCR p15, 3, R0, c15, c2, 0\r\n\
-           " : : "r"(index) : "r0");
-    }
-
-    /* disable cache lockdown */
-    asm volatile ("\
-       MOV R0, #0\r\n\
-       MCR p15, 0, R0, c9, c0, 0\r\n\
-       " : : : "r0");
-
-    /* and flush cache again to make sure its consistent */
-    asm volatile ("\
-        MOV R0, #0\r\n\
-        MCR p15, 0, R0, c7, c6, 0\r\n\
-        MCR p15, 0, R0, c7, c10, 4\r\n\
-        " : : : "r0"
-    );
-    
-    sei(old_int);
-}
-
-static void icache_lock()
-{
-    uint32_t old_int = cli();
-
-    /* no need to clean entries, directly flush and lock cache */
-    asm volatile ("\
-       /* flush cache pages */\
-       MCR p15, 0, R0, c7, c5, 0\r\n\
-       \
-       /* enable cache lockdown for segment 0 (of 4) */\
-       MOV R0, #0x80000000\r\n\
-       MCR p15, 0, R0, c9, c0, 1\r\n\
-       \
-       /* finalize lockdown */\
-       MOV R0, #1\r\n\
-       MCR p15, 0, R0, c9, c0, 1\r\n\
-       " : : : "r0");
-
-    /* make sure all entries are set to invalid */
-    for(uint32_t index = 0; index < (1U<<CACHE_INDEX_BITS(TYPE_ICACHE)); index++)
-    {
-        asm volatile ("\
-           /* write index for address to write */\
-           MOV R0, %0, LSL #5\r\n\
-           MCR p15, 3, R0, c15, c0, 0\r\n\
-           /* set TAG at given index */\
-           MCR p15, 3, R0, c15, c1, 0\r\n\
-           " : : "r"(index) : "r0");
-    }
-    sei(old_int);
-}
-
-static void dcache_lock()
-{
-    uint32_t old_int = cli();
-
-    /* first clean and flush dcache entries */
-    for(uint32_t segment = 0; segment < (1U<<CACHE_SEGMENT_BITS(TYPE_DCACHE)); segment++ )
-    {
-        for(uint32_t index = 0; index < (1U<<CACHE_INDEX_BITS(TYPE_DCACHE)); index++)
-        {
-            uint32_t seg_index = (segment << CACHE_SEGMENT_TAGOFFSET(TYPE_DCACHE)) | (index << CACHE_INDEX_TAGOFFSET(TYPE_DCACHE));
-            asm volatile ("\
-                mcr p15, 0, %0, c7, c14, 2\r\n\
-                " : : "r"(seg_index)
-            );
-        }
-    }
-
-    /* then lockdown data cache */
-    asm volatile ("\
-       /* enable cache lockdown for segment 0 (of 4) */\
-       MOV R0, #0x80000000\r\n\
-       MCR p15, 0, R0, c9, c0, 0\r\n\
-       \
-       /* finalize lockdown */\
-       MOV R0, #1\r\n\
-       MCR p15, 0, R0, c9, c0, 0\r\n\
-       " : : : "r0");
-
-    /* make sure all entries are set to invalid */
-    for(uint32_t index = 0; index < (1U<<CACHE_INDEX_BITS(TYPE_DCACHE)); index++)
-    {
-        asm volatile ("\
-           /* write index for address to write */\
-           MOV R0, %0, LSL #5\r\n\
-           MCR p15, 3, R0, c15, c0, 0\r\n\
-           /* set TAG at given index */\
-           MCR p15, 3, R0, c15, c2, 0\r\n\
-           " : : "r"(index) : "r0");
-    }
-    sei(old_int);
-}
+void icache_unlock();
+void dcache_unlock();
+static void icache_lock();
+static void dcache_lock();
 
 /* these are the "public" functions. please use only these if you are not sure what the others are for */
 
-static uint32_t cache_locked()
-{
-    uint32_t status = 0;
-    asm volatile ("\
-       /* get lockdown status */\
-       MRC p15, 0, %0, c9, c0, 1\r\n\
-       " : "=r"(status) : : "r0");
-    
-    return status;
-}
+uint32_t cache_locked();
 
-static void cache_lock()
-{
-#if !defined(CONFIG_QEMU)
-    icache_lock();
-    dcache_lock();
-#endif
-}
+void cache_lock();
+void cache_unlock();
 
-static void cache_unlock()
-{
-#if !defined(CONFIG_QEMU)
-    icache_unlock();
-    dcache_unlock();
-#endif
-}
+uint32_t cache_fake(uint32_t address, uint32_t data, uint32_t type);
 
-static uint32_t cache_fake(uint32_t address, uint32_t data, uint32_t type)
-{
-#if defined(CONFIG_QEMU)
-    /* QEMU doesn't seem to model the CPU cache, but we have configured the ROM area as RAM, so it's trivial to patch things */
-    MEM(address) = data;
-    return 0;
-#else    
-    /* that word is already patched? return failure */
-    /*
-    if(!cache_is_patchable(address, type))
-    {
-        return 0;
-    }
-    */
-    /* is that line not in cache yet? */
-    if(!cache_get_cached(address, type))
-    {
-        /* no, then fetch it */
-        cache_fetch_line(address, type);
-    }
-
-    return cache_patch_single_word(address, data, type);
-#endif
-}
-
-#endif // CONFIG_DIGIC_45
-#endif /* __ARM__ */
 #endif
