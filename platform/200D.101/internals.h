ba7fcb27833a9a65c70125c0dba56e803305149f
reticulatedpines <33672591+reticulatedpines@users.noreply.github.com>
2022-09-12 00:00:53 +0100

    200d: stubs for MMU, enable support in internals.h
    Not yet used, but these are the stubs we'll want later.
    CONFIG_MMU gets defined, but on its own this does nothing,
    you need to opt into CONFIG_MMU_EARLY_REMAP (boot loader context),
    or CONFIG_MMU_REMAP (normal context).

--- platform/200D.101/internals.h
+++ platform/200D.101/internals.h
@@ -5,6 +5,10 @@
 /** This camera has a DIGIC VII chip */
 #define CONFIG_DIGIC_VII
 
+// Cam has MMU (by itself, does nothing, see CONFIG_MMU_REMAP
+// and CONFIG_MMU_EARLY_REMAP)
+#define CONFIG_MMU
+
 /** Digic 7 does not have bitmap font in ROM, try to load it from card **/
 #define CONFIG_NO_BFNT
 
