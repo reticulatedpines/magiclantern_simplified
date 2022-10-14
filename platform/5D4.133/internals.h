2984d6208ea846f6f0d5d86179da596522e0548c
reticulatedpines <33672591+reticulatedpines@users.noreply.github.com>
2022-10-14 14:25:14 +0100

    5D4: fix misclassification of this cam as Dual Digic

--- platform/5D4.133/internals.h
+++ platform/5D4.133/internals.h
@@ -5,7 +5,7 @@
 /** This camera has a DIGIC VI chip */
 #define CONFIG_DIGIC_VI
 
-#define CONFIG_DUAL_DIGIC
+//#define CONFIG_DUAL_DIGIC
 
 /** Digic 6 does not have bitmap font in ROM, try to load it from card **/
 #define CONFIG_NO_BFNT
