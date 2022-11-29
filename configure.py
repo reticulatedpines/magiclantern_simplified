653c6d10d48a9ca4e0346045aa5dd53a12d1a505
Daniel <brikbusters@gmail.com>
2022-11-29 14:19:43 -0500

    fix nitpicks

--- configure.py
+++ configure.py
@@ -1,11 +1,11 @@
 import os, platform, subprocess
 
-def checkCommand(shell):
+def check_command(shell):
     return subprocess.check_call(["which", shell])
 
-def checkAll():    
+def check_all():    
     print("Checking for rst2html")
-    if checkCommand("rst2html"):
+    if check_command("rst2html"):
         print("rst2html not found")
         print("Install with: pip3 install rst2html")
         exit(1)
@@ -13,22 +13,22 @@ def checkAll():
         print("rst2html checked")
 
     print("Checking for python2")
-    if checkCommand("python2"):
+    if check_command("python2"):
         print("python2 not found. ML still relies on it.")
         exit(1)
     else:
         print("python2 works")
 
     print("Checking for host GCC")
-    if checkCommand("gcc"):
+    if check_command("gcc"):
         print("gcc not found")
         exit(1)
     else:
         print("gcc checked")
 
     print("Checking for ARM GCC")
-    if checkCommand("arm-none-eabi-gcc"):
+    if check_command("arm-none-eabi-gcc"):
         print("Could not find arm-none-eabi-gcc")
         exit(1)
 
-checkAll()
+check_all()
