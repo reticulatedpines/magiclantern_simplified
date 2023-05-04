0107f1ba0ada0a6656d52d67950e603d86235ce2
kitor <github@kitor.pl>
2023-05-04 13:47:47 +0200

    Remove pointless 760D platform

--- platform/760D.100/consts.h
+++
@@ -1,8 +0,0 @@
-/*
- *  760D 1.0.0 consts
- */
-
-/* LED ON/OFF values appear to be inverted */
-#define CARD_LED_ADDRESS            0xD20B0A24
-#define LEDON                       0x4C0003
-#define LEDOFF                      0x4D0002
--- platform/760D.100/internals.h
+++
@@ -1,6 +0,0 @@
-/**
- * Camera internals for 760D 1.0.0
- */
-
-/** This camera has a DIGIC VI chip */
-#define CONFIG_DIGIC_VI
