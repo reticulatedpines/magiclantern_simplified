530e1f6cfb632e1d015927b1a4dab247bea8ce8e
reticulatedpines <33672591+reticulatedpines@users.noreply.github.com>
2023-06-05 14:43:37 +0100

    5d4: new task structs

--- platform/5D4.133/internals.h
+++ platform/5D4.133/internals.h
@@ -10,3 +10,7 @@
 
 // Digic 6 does not have bitmap font in ROM, try to load it from card
 #define CONFIG_NO_BFNT
+
+#define CONFIG_NEW_TASK_STRUCTS
+#define CONFIG_TASK_STRUCT_V2
+#define CONFIG_TASK_ATTR_STRUCT_V4
