bb06f05a3195a54ce517fd78024eba5f708f787a
reticulatedpines <33672591+reticulatedpines@users.noreply.github.com>
2023-07-18 01:11:49 +0100

    Merge fixes: 100D needed SRM_BUFFER_SIZE to build

--- platform/100D.101/consts.h
+++ platform/100D.101/consts.h
@@ -252,6 +252,7 @@
 
 #define MALLOC_STRUCT 0x883B8
 #define MALLOC_FREE_MEMORY (MEM(MALLOC_STRUCT + 8) - MEM(MALLOC_STRUCT + 0x1C)) // "Total Size" - "Allocated Size"
+#define SRM_BUFFER_SIZE 0x1F24000   /* print it from srm_malloc_cbr */
 
 #define UNAVI_BASE (0x7B8EC)
 #define UNAVI (MEM(UNAVI_BASE + 0x24))
