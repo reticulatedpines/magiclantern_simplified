9374cc3508d09909643b1fea8d45b9ef21753ec2
stephen-e <33672591+reticulatedpines@users.noreply.github.com>
2023-09-01 23:46:39 +0100

    warnings: convert #warning FIXME to plain comment
    Nobody has ever looked at these compiler warnings,
    if somebody has the time they can grep for FIXME.
    This lets me fail builds if there are warnings.

--- src/tweaks.c
+++ src/tweaks.c
@@ -432,7 +432,7 @@ static void print_set_maindial_hint(int set)
         {
             info_led_off();
             #ifdef CONFIG_TOUCHSCREEN
-            #warning FIXME: dialog_redraw breaks touchscreen functionality in PLAY mode, why?! (issue #2901)
+            //FIXME: dialog_redraw breaks touchscreen functionality in PLAY mode, why?! (issue #2901)
             bmp_idle_copy(1, 0);
             #else
             redraw();
