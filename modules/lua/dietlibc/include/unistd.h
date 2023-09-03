c3b3e76b55646500db318cfdf4adda0a8f6cec25
stephen-e <33672591+reticulatedpines@users.noreply.github.com>
2023-09-03 15:17:17 +0100

    gcc12: fix mismatched params warning
    Manual backport of:
    https://github.com/ensc/dietlibc/commit/96452c84dd231247fac30ee2e07b8f8028f53a3d

--- modules/lua/dietlibc/include/unistd.h
+++ modules/lua/dietlibc/include/unistd.h
@@ -69,8 +69,8 @@ int execve(const char *filename, char *const argv [], char *const envp[]) __THRO
 int execlp(const char *file, const char *arg, ...) __THROW;
 int execv(const char *path, char *const argv[]) __THROW;
 int execvp(const char *file, char *const argv[]) __THROW;
-int execl(const char *path, ...) __THROW;
-int execle(const char *path, ...) __THROW;
+int execl(const char *path, const char *arg, ...) __THROW;
+int execle(const char *path, const char *arg, ...) __THROW;
 
 pid_t getpid(void) __THROW __pure;
 
