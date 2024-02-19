db9bd2905c5c3dfdcf8e7afbdc43ee3f0fa2b3fd
kitor <github@kitor.pl>
2024-02-19 17:28:46 +0100

    R test_features: display temperature sensors data

--- platform/R.180/test_features.c
+++ platform/R.180/test_features.c
@@ -26,7 +26,86 @@
 
 #ifndef CONFIG_HELLO_WORLD
 #include <dryos.h>
+#include <property.h>
+#include <bmp.h>
 
+// random props I found
+#define PROP_BATTERY_CHECK 0x8030013
+
+/*
+ * Temperature monitoring.
+ * See props registerd in TempSvc task (e01ae332). Names come from function
+ * at e01ae6f8.
+ * PROP_EFIC_TEMP seems not to exists anymore. ID is referenced just once,
+ * for prop slave (no masters registered)
+ */
+
+// handled by PropTemperatureMaster. Not sure what those do.
+#define PROP_R_TEMP_X1   0x8004000f
+#define PROP_R_TEMP_X2   0x80030080
+
+// handled by PropTempStatusMaster
+#define PROP_R_TEMP_SH   0x80030062
+#define PROP_R_TEMP_MAIN 0x80030035
+#define PROP_R_TEMP_A    0x8003009E
+#define PROP_R_TEMP_WM   0x80030073
+#define PROP_R_TEMP_BACK 0x80030081
+
+
+uint32_t eosr_temperatures[5];
+
+uint32_t rawTempToDegrees(uint raw)
+{
+    // Data comes in form of fixed point value: 4 LSB for decimal, rest for deg
+    // In fn e01ae228 it is converted into a different value for prop delivery
+    // Unfortunately this conversion eats up decimal points, so I just skip them
+    return ((((raw - 0x80) << 0x14) / 0x10000) - 8) >> 4;
+}
+
+void printTemp(char * msg, uint32_t raw)
+{
+    uint32_t tmp = rawTempToDegrees(raw);
+}
+
+PROP_HANDLER(PROP_R_TEMP_SH)
+{
+    eosr_temperatures[0] = rawTempToDegrees(buf[0]);
+}
+
+PROP_HANDLER(PROP_R_TEMP_MAIN)
+{
+    eosr_temperatures[1] = rawTempToDegrees(buf[0]);
+}
+
+PROP_HANDLER(PROP_R_TEMP_A)
+{
+    eosr_temperatures[2] = rawTempToDegrees(buf[0]);
+}
+
+PROP_HANDLER(PROP_R_TEMP_WM)
+{
+    eosr_temperatures[3] = rawTempToDegrees(buf[0]);
+}
+
+PROP_HANDLER(PROP_R_TEMP_BACK)
+{
+    eosr_temperatures[4] = rawTempToDegrees(buf[0]);
+}
+
+static MENU_UPDATE_FUNC(temp_display)
+{
+    int sensor_id = (int) entry->priv;
+    if (sensor_id >= sizeof(eosr_temperatures))
+      return;
+
+    MENU_SET_VALUE("%d"SYM_DEGREE"C", eosr_temperatures[sensor_id]);
+}
+
+/*
+ * Display overexpo overlay in LV. Temporary solution using Canon functions.
+ * See https://wiki.magiclantern.fm/digic8:registers for registers description
+ * that should contribute to existing ML over/underexpo feature.
+ */
 extern void DISP_SetHighLight(int);
 int highlight_flag = 0;
 
@@ -43,12 +122,48 @@ static struct menu_entry test_features_debug_menu[] = {
         .name   = "EOS R Test features",
         .select = menu_open_submenu,
         .help   = "Temporary features, not integrated into main codebase yet.",
+        .submenu_width = 710,
         .children =  (struct menu_entry[]) {
             {
                 .name   = "Overexposure warning in LiveView",
                 .priv   = overexpo_toggle,
                 .select = run_in_separate_task,
                 .help   = "You may need to toggle twice each time you enter LV."
+            },
+            {
+                .name   = "Temp SH",
+                .priv   = (int*)0,
+                .update = temp_display,
+                .help   = "Used by PhotoOperator",
+                .icon_type = IT_ALWAYS_ON
+            },
+            {
+                .name   = "Temp MAIN",
+                .priv   = (int*)1,
+                .update = temp_display,
+                .help   = "Maybe sensor? Used by lv_set_atemp_new",
+                .icon_type = IT_ALWAYS_ON
+            },
+            {
+                .name   = "Temp A",
+                .priv   = (int*)2,
+                .update = temp_display,
+                .help   = "Used by PhotoOperator and GmtState",
+                .icon_type = IT_ALWAYS_ON
+            },
+            {
+                .name   = "Temp WM",
+                .priv   = (int*)3,
+                .update = temp_display,
+                .help   = "Unknown location, not used in code",
+                .icon_type = IT_ALWAYS_ON
+            },
+            {
+                .name   = "Temp BACK",
+                .priv   = (int*)4,
+                .update = temp_display,
+                .help   = "Probably located near LCD",
+                .icon_type = IT_ALWAYS_ON
             },
             MENU_EOL,
         },
