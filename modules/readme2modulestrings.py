04001c05bc039614b643480b16a4d6cec62198df
anibarro <eduardo.anibarro@gmail.com>
2025-06-28 08:49:31 +0200

    Fix deprecation warning
    Replace the deprecated distutils calll (https://mail.python.org/archives/list/python-dev@python.org/thread/TXU6TVOMBLQU3SV57DMMOA5Y2E67AW7P/) by shutil (https://docs.python.org/3/library/shutil.html#shutil.which)

--- modules/readme2modulestrings.py
+++ modules/readme2modulestrings.py
@@ -61,9 +61,8 @@ def declare_string_section():
     
 def is_command_available(name):
     """Check if command `name` is on PATH."""
-    import distutils.spawn
-    from distutils.spawn import find_executable
-    return find_executable(name) is not None
+    import shutil
+    return shutil.which(name) is not None
 
 # return the first command of the list that can be found on the OS
 def get_command_of(commands):
