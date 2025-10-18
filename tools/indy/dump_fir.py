7a0db7bf77ecb3c0a513f817afd8f66ef876acf0
Laurent Clévy <lclevy@free.fr>
2025-10-18 12:34:03 +0200

    Refactor dump_fir.py for Python 3 compatibility
    Updated to Python 3 compatibility and changed version to 0.6.

--- tools/indy/dump_fir.py
+++ tools/indy/dump_fir.py
@@ -1,13 +1,15 @@
 #dump decrypted thing
+#2025: Lorenzo3324@discord, python3 compatible
 
-from struct import unpack
+from struct import unpack, Struct
 import os
 import sys
 import array
 import ctypes
 from optparse import OptionParser
 
-version_id = "0.5 (22dec2012)"
+#2025
+version_id = "0.6 (oct2025)"
 
 def getLongLE(d, a):
    return unpack('<L',(d)[a:a+4])[0]
@@ -16,7 +18,7 @@ def writePatch(m, filename, patch, n):
   name = filename+'_'+'{0:02x}'.format(n)+'.bin'
   s = patch[n][1]
   e = patch[n][1] + patch[n][2]
-  print 'writing %s [0x%x-0x%x]. size=0x%x/%d' % (name, s, e, patch[n][2], patch[n][2])
+  print ('writing %s [0x%x-0x%x]. size=0x%x/%d' % (name, s, e, patch[n][2], patch[n][2]))
   f = open(name, 'wb')
   f.write( m[ s: e ] )
   f.close()
@@ -30,61 +32,56 @@ parser.add_option("-p", "--prefix", type="string", dest="filename", default="fir
 parser.add_option("-s", "--skip", type="int", dest="skip", default=0, help="skip n bytes")
 (options, args) = parser.parse_args()
 
-f = open(args[0], 'rb')
-f.seek(options.skip)
-m = f.read()
-size = f.tell()-options.skip
-f.close()
+with open(args[0], 'rb') as f:
+  f.seek(options.skip)
+  m = f.read()
+  fsize = f.tell()-options.skip
 
-print 'Dump_fir %s\n' % version_id
+print ('Dump_fir %s\n' % version_id)
 if options.verbose:
-  print 'fileLen = 0x%x' % size
+  print ('fileLen = 0x%x' % fsize)
 
-#elength = getLongLE( m, 8 ) # encrypted length
-
-fchk = getLongLE( m, 0) # embedded checksum to match
+fchk, z1, two, z2 = Struct('<LLLL').unpack_from(m, 0)
 if options.verbose:
-  print '0x000: checksum = 0x%08x' % fchk
-  print '0x004: 0x%08x' % getLongLE( m, 4)
-  print '0x008: 0x%08x' % getLongLE( m, 8)
-  print '0x00c: 0x%08x' % getLongLE( m, 12)
+  print ('0x000: checksum = 0x%08x' % fchk)
+  print ('0x004: 0x%08x' % z1)
+  print ('0x008: 0x%08x' % two)
+  print ('0x00c: 0x%08x' % z2)
+
+#compute checksum
 cchk = sum( array.array('B', m[ 4: ]) )
 if fchk != ctypes.c_uint32(~cchk).value:
-  print "checksum error"
+  print ("checksum error")
   sys.exit()
 
-nb_record = getLongLE( m, 0x10)
-table_offset = getLongLE( m, 0x14)
-record_size = getLongLE( m, 0x18)
-size_after = getLongLE( m, 0x1c)
-#if (size_after+4+table_offset+(nb_record*record_size)) != size:
-#  print 'error: (size_after+4+table_offset+(nb_record*record_size)) != size:'
+nb_record, table_offset, record_size, size_after = Struct('<LLLL').unpack_from(m, 0x10)
+
+'''if (size_after+4+table_offset+(nb_record*record_size)) != fsize:
+  print ('error: (size_after+4+table_offset+(nb_record*record_size)) != fsize:')'''
+
 if options.verbose:
-  print '0x010: nb_record = 0x%x' % nb_record
-  print '0x014: table_offset = 0x%x' % table_offset
-  print '0x018: record_size = 0x%x' % record_size
-  print '0x01c: size_after = 0x%x' % size_after
-  print '0x%03x: ---patches table---' % table_offset
-  print '      + tag  + foffset  +   size   + moffset  +    ?'
-  print   ' --------------------------------------------------------'
+  print ('0x010: nb_record = 0x%x' % nb_record)
+  print ('0x014: table_offset = 0x%x' % table_offset)
+  print ('0x018: record_size = 0x%x' % record_size)
+  print ('0x01c: size_after = 0x%x' % size_after)
+  print ('0x%03x: ---patches table---' % table_offset)
+  print ('      + tag  + foffset  +   size   + moffset  +    ?')
+  print (' --------------------------------------------------------')
+  
 patch = dict()
 n = 0
 size = 0
 for i in range(nb_record):
-   k = getLongLE( m, table_offset + record_size*i)
-   patch[ k ] = ( getLongLE( m, table_offset + record_size*i + 4 ),\
-   getLongLE( m, table_offset + record_size*i + 8 ),\
-   getLongLE( m, table_offset + record_size*i + 12 ),\
-   getLongLE( m, table_offset + record_size*i + 16 ),\
-   getLongLE( m, table_offset + record_size*i + 20 )  )
+   k, t, fo, s, mo, u = Struct('<LLLLLL').unpack_from(m, table_offset + record_size*i)
+   patch[ k ] = (t, fo, s, mo, u)
    if options.verbose:
-     print ' 0x%02x: 0x%04x 0x%08x 0x%08x 0x%08x 0x%08x' % ( k, patch[k][0], patch[k][1], patch[k][2], patch[k][3], patch[k][4] ) 
-   n = n + patch[k][4]
-   size = size + patch[k][2]
+     print (' 0x%02x: 0x%04x 0x%08x 0x%08x 0x%08x 0x%08x' % ( k, t, fo, s, mo, u ) )
+   #n = n + u
+   #size = size + s
 if options.verbose:
-  print '0x%03x: ---patch#1---' % (table_offset + nb_record*record_size)
-  print '                         0x%08x            0x%08x/%d' % (size, n, n)
-  print '%d' % (size/n)
+  print (' 0x%03x: ---patch#1---' % (table_offset + nb_record*record_size))
+  #print ('                         0x%08x            0x%08x/%d' % (size, n, n))
+
 if options.extract != -1:
   if options.extract == 0:
     for i in range(nb_record):
