5a63dfd9f01f886909fb5ce2dae9a3704cdd8d6e
jed2nd <jedi.xi@gmail.com>
2026-06-17 06:59:09 -0600

    R: whitelist PROP_REMOTE_SW1/SW2 (note: writes accepted but don't register a half-press on R)

--- platform/R.180/property_whitelist.h
+++ platform/R.180/property_whitelist.h
@@ -36,6 +36,9 @@ const uint32_t prop_handler_deny[] =
 // allow writes / allow prop_request_change() for these:
 const uint32_t prop_write_allow[] =
 {
+    // remote shutter (half/full press) for ML-triggered AF capture
+    PROP_REMOTE_SW1,
+    PROP_REMOTE_SW2,
     PROP_PICTURE_STYLE,
     PROP_PICSTYLE_SETTINGS_STANDARD,
     PROP_PICSTYLE_SETTINGS_PORTRAIT,
